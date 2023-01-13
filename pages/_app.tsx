4863786a85ba424653f2abca7418f0fa72cba474
Brady Stroud <bradystroud@ssw.com.au>
2023-01-13 16:18:08 +1000

    💫  Only run animations once

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,15 @@
-import { useEffect } from "react";
-import { DefaultSeo } from "next-seo";
-import { NEXT_SEO_DEFAULT } from "../next-seo.config";
+import { useEffect } from 'react';
+import { DefaultSeo } from 'next-seo';
+import { NEXT_SEO_DEFAULT } from '../next-seo.config';
 
 import "../styles.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 const App = ({ Component, pageProps }) => {
-  useEffect(() => AOS.init({duration: 1200}));
-  
+  // Initialize AOS (Animate on Scroll Library) see https://michalsnik.github.io/aos/ 
+  useEffect(() => AOS.init({ duration: 1200, once: true }));
+
   return (
     <>
       <DefaultSeo {...NEXT_SEO_DEFAULT} />
