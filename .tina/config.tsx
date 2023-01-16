6f4cb086306bd596b4c92a5faabd0d15997bd7d2
Jim Zheng <JimZheng@ssw.com.au>
2023-01-16 23:40:17 +0800

    Change technology card field to reference type and update page content

--- .tina/config.tsx
+++ .tina/config.tsx
@@ -291,8 +291,8 @@ const config = defineStaticConfig({
                 type: "string",
                 label: "subTitle",
                 name: "subTitle",
-              }
-            ]
+              },
+            ],
           },
           {
             type: "object",
@@ -336,9 +336,9 @@ const config = defineStaticConfig({
                     type: "string",
                     label: "URL",
                     name: "url",
-                  }
+                  },
                 ],
-              }
+              },
             ],
           },
           {
@@ -348,28 +348,15 @@ const config = defineStaticConfig({
           },
           {
             type: "object",
-            list: true,
-            label: "Technology Cards",
-            name: "technologyCards",
-            fields: [
-              {
-                type: "string",
-                label: "Name",
-                name: "name"
-              }
-            ]
-          },
-          {
-            type: "object",
             label: "Solution",
             name: "solution",
             fields: [
               {
                 type: "string",
                 label: "Project",
-                name: "project"
-              }
-            ]
+                name: "project",
+              },
+            ],
           },
           {
             type: "string",
@@ -382,6 +369,25 @@ const config = defineStaticConfig({
             name: "_body",
             templates: [...Schemas.pageBlocks],
             isBody: true,
+          },
+          {
+            type: "object",
+            label: "Technology Cards",
+            name: "technologyCards",
+            ui: {
+              itemProps: (item) => ({
+                label: item?.technologyCard,
+              }),
+            },
+            list: true,
+            fields: [
+              {
+                type: "reference",
+                label: "Technology Card",
+                name: "technologyCard",
+                collections: ["technologies"],
+              },
+            ],
           },
         ],
       },
@@ -396,30 +402,30 @@ const config = defineStaticConfig({
           },
         },
         fields: [
-        {
-          type: "string",
-          label: "Name",
-          name: "name",
-          required: true,
-        },
-        {
-          type: "image",
-          label: "Avatar",
-          name: "avatar",
-          required: true,
-        },
-        {
-          type: "string",
-          label: "Company",
-          name: "company",
-          required: true,
-        },
-        {
-          type: "rich-text",
-          label: "Body",
-          name: "body",
-          isBody: true,
-        },
+          {
+            type: "string",
+            label: "Name",
+            name: "name",
+            required: true,
+          },
+          {
+            type: "image",
+            label: "Avatar",
+            name: "avatar",
+            required: true,
+          },
+          {
+            type: "string",
+            label: "Company",
+            name: "company",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            label: "Body",
+            name: "body",
+            isBody: true,
+          },
         ],
       },
       {
@@ -451,13 +457,13 @@ const config = defineStaticConfig({
             name: "thumbnail",
           },
           {
-            type: 'rich-text',
-            label: 'Body',
-            name: 'body',
+            type: "rich-text",
+            label: "Body",
+            name: "body",
             isBody: true,
           },
-        ]
-      }
+        ],
+      },
     ],
   },
 });
