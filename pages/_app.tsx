6bfd4bae53ab61f92b6d11b5309c161dab8640bf
Tylah Kapa <TylahKapa@gmail.com>
2023-01-18 15:55:04 +1000

    Add Google Analytics and env variables

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
+import analytics from "../analytics";
 import { DefaultSeo } from "next-seo";
+import { useEffect } from "react";
 import { NEXT_SEO_DEFAULT } from "../next-seo.config";
 import "../styles.css";
 
 const App = ({ Component, pageProps }) => {
+  useEffect(() =>{
+    analytics.page();
+  }, [])
+
   return (
     <>  
       <DefaultSeo {...NEXT_SEO_DEFAULT} />
