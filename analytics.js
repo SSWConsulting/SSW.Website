f7fc68896a4abd17853a9e977667e8e72225bf5c
Tylah Kapa <TylahKapa@gmail.com>
2023-01-18 16:50:16 +1000

    Add Google Analytics

--- analytics.js
+++ analytics.js
@@ -4,16 +4,16 @@ import googleAnalytics from "@analytics/google-analytics"
 
 const analytics = Analytics({
     app: "ssw-website",
-    debug: process.env.NODE_ENV === 'development',
+    debug: process.env.NODE_ENV === "development",
     plugins:[
         googleTagManager({
             containerId: process.env.NEXT_PUBLIC_GOOGLE_GTM_ID,
-            enabled: true,
+            enabled: process.env.NEXT_PUBLIC_GOOGLE_GTM_ID !== "",
         }),
-        // googleAnalytics({
-        //     measurementIds:[process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS],
-        //     enabled: true,
-        // })
+        googleAnalytics({
+            measurementIds:[process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS],
+            enabled: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS !== "",
+        })
     ]
 });
 
