7724f034ef42ac41cf30e1fbac810845bf0845e9
Tylah Kapa <TylahKapa@gmail.com>
2023-01-19 11:53:00 +1000

    Implement Zendesk

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,31 @@ import { useEffect } from "react";
 import { NEXT_SEO_DEFAULT } from "../next-seo.config";
 import "../styles.css";
 import { hotjar } from "react-hotjar";
+import Script from "next/script";
+
+const zendeskKey = process.env.NEXT_PUBLIC_ZENDESK_CHAT_KEY;
+const isDev = process.env.NODE_ENV === "development";
 
 const App = ({ Component, pageProps }) => {
-  useEffect(() =>{
+  useEffect(() => {
     analytics.page();
 
-    if(process.env.NODE_ENV !== "development"){
-      hotjar.initialize(parseInt(process.env.NEXT_PUBLIC_HOTJAR_ID), parseInt(process.env.NEXT_PUBLIC_HOTJAR_SV))
+    if (!isDev) {
+      hotjar.initialize(
+        parseInt(process.env.NEXT_PUBLIC_HOTJAR_ID),
+        parseInt(process.env.NEXT_PUBLIC_HOTJAR_SV)
+      );
     }
-  }, [])
+  }, []);
 
   return (
-    <>  
+    <>
+      {!isDev && 
+        <Script
+          id="ze-snippet"
+          src={`https://static.zdassets.com/ekr/snippet.js?key=${zendeskKey}`}
+        />
+      }
       <DefaultSeo {...NEXT_SEO_DEFAULT} />
       <Component {...pageProps} />
     </>
