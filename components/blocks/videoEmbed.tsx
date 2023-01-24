da3b5de4f28edf377f9763f9d495d53b3b17dfe5
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2023-01-24 11:29:08 +1000

    added video embed component

---
+++ components/blocks/videoEmbed.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactPlayer from "react-player";
+
+export const VideoEmbed = ({ data }) => {
+  return (
+    <div className="relative m-8 mx-auto aspect-video w-3/4">
+      <ReactPlayer
+        className="absolute top-0 left-0"
+        url={data.url}
+        width={"100%"}
+        height={"100%"}
+      />
+    </div>
+  );
+}
+
+export const videoEmbedBlockSchema: Template = {
+  name: "VideoEmbed",
+  label: "Video Embed",
+  fields: [
+    {
+      type: "string",
+      label: "Video URL",
+      name: "url",
+      required: true,
+    },
+  ]
+}
