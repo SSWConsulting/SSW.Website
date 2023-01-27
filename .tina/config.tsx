32e6ea7c1dabeff70a0a7655b6f17739557e987f
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2023-01-27 10:36:37 +1000

    added marketing collection

--- .tina/config.tsx
+++ .tina/config.tsx
@@ -32,6 +32,43 @@ const config = defineStaticConfig({
   schema: {
     collections: [
       {
+        label: "Marketing",
+        name: "marketing",
+        path: "content/marketing",
+        format: "mdx",
+        ui: {
+          router: ({ document }) => {
+            return `/consulting/${document._sys.filename}`;
+          },
+        },
+        fields: [
+          {
+            type: "string",
+            label: "Title",
+            name: "title",
+            required: true,
+          },
+          {
+            type: "image",
+            label: "Background image",
+            name: "backgroundImage",
+            required: true,
+          },
+          {
+            type: "string",
+            label: "Marketing video URL",
+            name: "videoUrl",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            label: "Body",
+            name: "body",
+            isBody: true,
+          },
+        ],
+      },
+      {
         label: "Global",
         name: "global",
         path: "content/global",
