5a0763c008a3f2fdb20b88d3be80f87b282adcf2
Matt Wicks <mattwicks@ssw.com.au>
2023-01-30 14:03:15 +1100

    ⚡ Upcoming events - use next/image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ["assets.tina.io"],
+    domains: ["assets.tina.io", "www.ssw.com.au"],
   },
   output: "standalone", // required for Docker support
   webpack(config) {
