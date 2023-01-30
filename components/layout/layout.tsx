d2103c3196e8387751b53c94782fb36064b93033
Tylah Kapa [SSW] <tylahkapa@gmail.com>
2023-01-30 14:06:06 +1000

    ✨Add MegaMenu (#63)
    Implement the Megamenu by importing the raw files from the repository.

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Head from "next/head";
 import { Header } from "./header";
 import { Footer } from "./footer";
 import { Theme } from "./theme";
+import { Menu, MobileMenu } from "../../lib/ssw.megamenu"
 
 export const Layout = ({ children }) => {
+  const node = useRef<HTMLDivElement>();
+  const [isMenuOpened, setIsMenuOpened] = useState(false);
+
+  const actionOnToggleClick = () => {
+    setIsMenuOpened(!isMenuOpened);
+  };
+
+  const handleClick = (e) => {
+      if (node.current){
+        node.current.contains(e.target) ?? setIsMenuOpened(false);
+      }
+  };
+
   return (
     <>
       <Head>
@@ -12,13 +26,17 @@ export const Layout = ({ children }) => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Theme>
-        <div>
-          <Header  />
-          <div className="flex flex-1 flex-col bg-gradient-to-br from-white to-gray-50 text-gray-800 dark:from-gray-900 dark:to-gray-1000">
-            {children}
-          </div>
-          <Footer />
+        <div className="mx-auto max-w-7xl px-6 sm:px-8"
+          ref={node}
+          onMouseDown={isMenuOpened ? (event) => handleClick(event) : null}>
+          <Header />
+          <Menu  onClickToggle={() => actionOnToggleClick()} />
         </div>
+        <div className="flex flex-1 flex-col bg-gradient-to-br from-white to-gray-50 text-gray-800 dark:from-gray-900 dark:to-gray-1000">
+            {children}
+        </div>
+        <Footer />
+        <MobileMenu isMenuOpened={isMenuOpened}></MobileMenu>
       </Theme>
     </>
   );
