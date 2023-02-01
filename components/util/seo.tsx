8c37f8267ed84d5a9b356d58ad8e407110801412
Tylah Kapa [SSW] <tylahkapa@gmail.com>
2023-02-01 14:35:17 +1000

    Add Open Graph Data to SEO (#81)

--- components/util/seo.tsx
+++ components/util/seo.tsx
@@ -3,23 +3,33 @@ import React, { FC } from "react";
 import { NextSeo, NextSeoProps } from "next-seo";
 import { NEXT_SEO_DEFAULT } from "../../next-seo.config";
 
-interface SEOProps {
-  seo?: Partial<NextSeoProps>;
-}
-
-export const SEO: FC<SEOProps> = ({ seo }) => {
+export const SEO = ({ seo }) => {
   if (!seo) return null;
 
+  let seoPartial: Partial<NextSeoProps> = {};
+
+  seoPartial = {
+    title: seo.title,
+    description: seo.description,
+    canonical: seo.canonical,
+    openGraph: {
+      title: seo.title,
+      description: seo.description,
+      url: seo.canonical,
+      images: seo.images ?? null
+    }
+  }
+
   // Remove null values from SEO object
-  Object.keys(seo).forEach((key) => {
-    if (!seo[key]) {
+  Object.keys(seoPartial).forEach((key) => {
+    if (!seoPartial[key]) {
       delete seo[key];
     }
   });
-
+  
   const seoProps = {
     ...NEXT_SEO_DEFAULT,
-    ...seo,
+    ...seoPartial,
   };
 
   return <NextSeo {...seoProps} />;
@@ -27,7 +37,7 @@ export const SEO: FC<SEOProps> = ({ seo }) => {
 
 export const seoSchema = {
   type: "object",
-  label: "SEO",
+  label: "SEO Values",
   name: "seo",
   fields: [
     {
@@ -44,6 +54,45 @@ export const seoSchema = {
       type: "string",
       label: "Canonical URL",
       name: "canonical",
+    },
+    {
+      label: "Images",
+      name: "images",
+      list: true,
+      type: "object",
+      ui: {
+        itemProps: (item) => {
+          return { label: item.url }
+        },
+        defaultItem: {
+          width: 800,
+          height: 600,
+          alt: "Default alt text",
+        }
+      },
+      fields: [
+        {
+          type: "image",
+          label: "Image Url",
+          name: "url",
+          require: true
+        },
+        {
+          type: "number",
+          label: "Width (px)",
+          name: "width",
+        },
+        {
+          type: "number",
+          label: "Height (px)",
+          name: "height",
+        },
+        {
+          type: "string",
+          label: "Image Alt Text",
+          name: "alt",
+        },
+      ],
     },
   ],
 };
