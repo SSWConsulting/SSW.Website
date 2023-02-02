dc03cb1c11796afaa741f35059f087a2cc0594ec
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2023-02-02 13:45:29 +1000

    added marketing component body text animations

--- components/marketing/Marketing.tsx
+++ components/marketing/Marketing.tsx
@@ -7,6 +7,7 @@ import ReactPlayer from "react-player";
 
 export const Marketing = (props) => {
   const content = props.content.marketing;
+    console.log(content)
 
   if (!content) {
     return <div />
@@ -15,7 +16,7 @@ export const Marketing = (props) => {
   return (
     <Section style={{backgroundImage: `url(${content?.backgroundImage})`}} className="h-full bg-cover">
       <Container size="custom" className="h-full py-16 backdrop-blur-md">
-        <h1 className="pt-0 text-5xl text-white">{content?.title}</h1>
+        <h1 className="mt-0 pt-0 text-5xl text-white" dangerouslySetInnerHTML={{ __html: content?.title }}></h1>
         <div className="my-8 flex flex-col justify-start md:flex-row">
           <div className="relative mx-auto aspect-video w-full md:w-1/2">
             <ReactPlayer
@@ -26,7 +27,22 @@ export const Marketing = (props) => {
             />
           </div>
           <div className="mt-16 w-full pb-0 text-left text-white md:mt-0 md:w-1/2">
-            <TinaMarkdown content={content?.body} />
+          <TinaMarkdown
+              content={content?.body}
+              components={{
+                p: ({ children: { props: { content } } }) => {
+                  return (
+                    <>
+                      {content.map(({ text }, id: number) => (
+                        <p key={id} className="mx-auto w-3/4 font-sans text-md" data-aos="fade-up">
+                          {text}
+                        </p>
+                      ))}
+                    </>
+                  )
+                }
+              }}
+            />
           </div>
         </div>
       </Container>
