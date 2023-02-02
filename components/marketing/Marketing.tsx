f24268892ccfbf4a60340e486b1cf64b466da45d
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2023-02-02 15:11:29 +1000

    made marketing component styling more like website

--- components/marketing/Marketing.tsx
+++ components/marketing/Marketing.tsx
@@ -16,7 +16,7 @@ export const Marketing = (props) => {
     <Section style={{backgroundImage: `url(${content?.backgroundImage})`}} className="h-full bg-cover">
       <Container size="custom" className="h-full py-16 text-center">
         <h1 className="mt-0 pt-0 text-5xl text-white" dangerouslySetInnerHTML={{ __html: content?.title }}></h1>
-        <div className="my-8 flex flex-col justify-start md:flex-row">
+        <div className="my-8 flex flex-col justify-between md:flex-row">
           <div className="relative mx-auto aspect-video w-full md:w-1/2">
             <ReactPlayer
               className="absolute top-0 left-0"
@@ -25,23 +25,8 @@ export const Marketing = (props) => {
               height={"100%"}
             />
           </div>
-          <div className="mt-16 w-full pb-0 text-left text-white md:mt-0 md:w-1/2">
-            <TinaMarkdown
-              content={content?.body}
-              components={{
-                p: ({ children: { props: { content } } }) => {
-                  return (
-                    <>
-                      {content.map(({ text }, id: number) => (
-                          <p key={id} className="mx-auto w-full font-sans text-md md:w-3/4" data-aos="fade-up">
-                          {text}
-                        </p>
-                      ))}
-                    </>
-                  )
-                }
-              }}
-            />
+      <div className="mt-16 w-full pb-0 text-left font-sans text-md text-white md:mt-0 md:ml-10 md:w-1/3">
+            <TinaMarkdown content={content?.body} />
           </div>
         </div>
       </Container>
