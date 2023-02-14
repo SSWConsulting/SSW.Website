ad4fba8edac0fa4ea250b62429848bd2782b8338
Matt Wicks <mattwicks@ssw.com.au>
2023-02-14 16:47:32 +1100

    let next-sitemap generate sitemap and robots - uses SITE_URL to determine root of url - uses NEXT_PUBLIC_TINA_BRANCH to determine whether to disallow search engines crawling site

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,12 +3,22 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://ssw.com.au",
   generateRobotsTxt: true,
-  outDir: "./.next/static/",
   robotsTxtOptions: {
     additionalSitemaps: [
       // TODO: add sitemaps for other sites when they are available
       // 'https://ssw.com.au/people/sitemap.xml',
       // 'https://ssw.com.au/rules/sitemap.xml',
     ],
+    policies: [
+      process.env.NEXT_PUBLIC_TINA_BRANCH === "main"
+        ? {
+            userAgent: "*",
+            allow: "/",
+          }
+        : {
+            userAgent: "*",
+            disallow: "/",
+          },
+    ],
   },
-}
+};
