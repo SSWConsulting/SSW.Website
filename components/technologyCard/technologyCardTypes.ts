30c2f1f2956d1b59df35a515482990dc7efac0bc
John Xu [SSW] <113034339+johnxussw@users.noreply.github.com>
2023-02-18 06:44:33 +0800

    💄 Technology cards UI review round 2 (#220)
    * fix - centered text of tech cards
    
    * fix - consistent text size of 'Read More'
    
    * refactor
    - removed unneeded hooks
    - change type VFC to FC
    - upgrade nextjs 13 image
    
    * fix - Ensure technology card spans if it's alone

--- components/technologyCard/technologyCardTypes.ts
+++ components/technologyCard/technologyCardTypes.ts
@@ -1,10 +1,9 @@
 export type TechnologyCardProps = {
-  index: number;
   name: string;
-  techListLength: number;
   readMoreSlug?: string | null;
   thumbnail?: string | null;
   body?: any | null; // eslint-disable-line @typescript-eslint/no-explicit-any
+  className?: string;
 };
 
 export type TechnologyCardsProps = {
