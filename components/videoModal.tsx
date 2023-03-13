fd7f395d604400c7581e681768d62dd3257b1958
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2023-03-13 11:01:26 +1000

    whole media card now clickable

--- components/videoModal.tsx
+++ components/videoModal.tsx
@@ -2,15 +2,14 @@ import ReactPlayer from "./reactPlayer/reactPlayer";
 import { useState } from "react";
 import Popup from "./popup/popup";
 
-export const VideoModal = ({ url }) => {
+export const VideoModal = ({ children = null, url }) => {
     const [show, setShow] = useState(false)
 
-    const showModal = () => setShow(true);
-    const hideModal = () => setShow(false);
+    const toggleModal = () => setShow(!show);
 
     return (
         <div>
-            <Popup isVisible={show} onClose={hideModal} className="sm:max-w-7xl">
+            <Popup isVisible={show} onClose={toggleModal} className="sm:max-w-7xl">
                 <div className="relative mx-auto aspect-video h-full w-full">
                     <ReactPlayer
                         playing={show}
@@ -21,14 +20,17 @@ export const VideoModal = ({ url }) => {
                     />
                 </div>
             </Popup>
-            <div className="aspect-video" onClick={showModal}>
-                <ReactPlayer
-                    light
-                    className="pointer-events-none absolute top-0 left-0"
-                    url={url || ""}
-                    width={"100%"}
-                    height={"100%"}
-                />
+            <div onClick={toggleModal}>
+                <div className="relative mx-auto aspect-video">
+                    <ReactPlayer
+                        light
+                        className="pointer-events-none absolute top-0 left-0"
+                        url={url || ""}
+                        width={"100%"}
+                        height={"100%"}
+                    />
+                </div>
+                {children}
             </div>
         </div>
     );
