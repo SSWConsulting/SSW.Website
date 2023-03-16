deda036866da33f6b16e83b1136859edb3492c79
Matt Wicks <mattwicks@ssw.com.au>
2023-03-16 13:23:36 +1100

    styles.css avoid marker classes

--- components/technologyCard/technologyCard.tsx
+++ components/technologyCard/technologyCard.tsx
@@ -30,7 +30,7 @@ const TechnologyCard: FC<TechnologyCardProps> = ({
 				) : (
 					<h2>{name}</h2>
 				)}
-				<div className="tech-widget prose-ul-li:grid-6 prose max-w-full grow prose-p:text-justify prose-strong:text-sswRed prose-ul:grid prose-ul:grid-flow-col prose-ul:grid-rows-12 prose-ul:text-left">
+				<div className="prose-ul-li:grid-6 prose max-w-full grow prose-p:text-justify prose-strong:text-sswRed prose-ul:grid prose-ul:grid-flow-col prose-ul:grid-rows-12 prose-ul:text-left descendant-div:!m-0">
 					<TinaMarkdown content={body} />
 				</div>
 				{readMoreSlug && (
