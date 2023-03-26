d658e9c2b52b067c99a8f47f71a97a3631032682
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2023-03-27 09:16:30 +1000

    separated client logos

--- components/util/horizontalList.tsx
+++ components/util/horizontalList.tsx
@@ -34,7 +34,7 @@ const HorizontalList: FC<{ listItemProps: HorizontalListItemProps[] }> = ({
 
     return (
         <>
-            <div className="grid grid-cols-1 gap-4 lg:grid-cols-4">{listItems}</div>
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">{listItems}</div>
         </>
     );
 }
