65a820dc5b6b32bb9e1bb803c2fecc8eedbef952
Gilles Pothieu <gilles.pothieu@gmail.com>
2023-04-12 11:07:43 +0200

    Modification of subButton component to manage newsletter subscription
    Currently the endpoint url needs to be added to environment variable.

--- components/blocks/subButton.tsx
+++ components/blocks/subButton.tsx
@@ -1,26 +1,106 @@
 import React from "react";
 import Image from "next/image";
 import { Template } from "tinacms";
+import {
+	addContactToNewsletters,
+	AddContactToNewslettersData,
+} from "../../services";
 
 export const SubButton = (data) => {
-	const handleSubscribe = () => {
-		console.log("subscribe");
+	const [email, setEmail] = React.useState<string>("");
+	const [fullName, setFullName] = React.useState<string>("");
+	const [informationMessage, setInformationMessage] =
+		React.useState<string>("");
+	const [isContactAlreadyExisting, setIsContactAlreadyExisting] =
+		React.useState<boolean>(false);
+	const [loading, setLoading] = React.useState(false);
+
+	const handleSubscribe = async () => {
+		if (email !== "" && fullName !== "") {
+			setLoading(true);
+			setInformationMessage("Subscription to the newsletter...");
+			setIsContactAlreadyExisting(false);
+			const payLoad: AddContactToNewslettersData = {
+				Email: email,
+				FullName: fullName,
+			};
+			try {
+				const addContactToNewsLettersResult = await addContactToNewsletters(
+					payLoad
+				);
+				setInformationMessage(addContactToNewsLettersResult.data.message);
+			} catch (error) {
+				setInformationMessage(error.response.data.message);
+				setIsContactAlreadyExisting(true);
+			}
+			setLoading(false);
+		}
+	};
+
+	const handleOnEmailChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	): void => {
+		setEmail(event.target.value);
+	};
+
+	const handleOnNameChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	): void => {
+		setFullName(event.target.value);
 	};
 
 	return (
-		<button
-			className="box-border flex w-auto cursor-pointer items-center justify-center gap-1 bg-sswRed py-2 pl-4 pr-2 font-sans uppercase text-white hover:text-gray-100 hover:opacity-95"
-			onClick={handleSubscribe}
-		>
-			{data.subButtonText ?? "join"}
-			<Image
-				className="inline-block align-middle leading-8"
-				src={"/images/circle-tick.png"}
-				alt="circle-tick"
-				height={24}
-				width={24}
-			/>
-		</button>
+		<div>
+			<p className="mb-2">
+				Subscribe to our newsletter to receive NETUG updates!
+			</p>
+			<div className="container sm:w-full sm:max-w-full md:w-1/4">
+				<div className="mb-1">
+					<input
+						className="col-span-3 w-full appearance-none rounded border-1 border-gray-300 py-2 px-3 leading-tight text-gray-700 focus:shadow focus:outline md:col-span-2"
+						id="fullName"
+						type="text"
+						placeholder="Your Full Name"
+						onChange={handleOnNameChange}
+						value={fullName}
+					/>
+				</div>
+				<div className="mb-1">
+					<input
+						className="col-span-3 w-full appearance-none rounded border-1 border-gray-300 py-2 px-3 leading-tight text-gray-700 focus:shadow focus:outline md:col-span-2"
+						id="email"
+						type="email"
+						placeholder="Your Email"
+						onChange={handleOnEmailChange}
+						value={email}
+					/>
+				</div>
+				<div className="flex justify-center">
+					<button
+						className="box-border flex w-1/2 cursor-pointer items-center justify-center gap-1 bg-sswRed py-2 pl-4 pr-2 font-sans uppercase text-white hover:text-gray-100 hover:opacity-95"
+						onClick={handleSubscribe}
+					>
+						{data.subButtonText ?? "join"}
+						<Image
+							className="inline-block align-middle leading-8"
+							src={"/images/circle-tick.png"}
+							alt="circle-tick"
+							height={24}
+							width={24}
+						/>
+					</button>
+				</div>
+				<p
+					className={`mt-2 flex justify-center text-sm ${
+						isContactAlreadyExisting ? "text-sswRed" : "text-green-500"
+					}`}
+				>
+					<span className={loading ? "text-gray-500" : ""}>
+						{informationMessage}
+					</span>
+				</p>
+			</div>
+		</div>
 	);
 };
 
