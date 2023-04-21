6a048cb37049158c32b3d162c9b4a6dd55899776
Brady Stroud <bradystroud@ssw.com.au>
2023-04-21 14:31:04 +1000

    🚨  Fix linter issues - usused import

--- pages/training/[filename].tsx
+++ pages/training/[filename].tsx
@@ -1,10 +1,8 @@
-
 import { useTina } from "tinacms/dist/react";
 
 import { client } from "../../.tina/__generated__/client";
 import { Layout } from "../../components/layout";
 import { SEO } from "../../components/util/seo";
-import { TestimonialRow } from "../../components/testimonials/TestimonialRow";
 import { Section } from "../../components/util/section";
 import { ClientLogos } from "../../components/blocks";
 import { Container } from "../../components/util/container";
@@ -13,31 +11,35 @@ import { TrainingCarousel } from "../../components/training/trainingHeader";
 import { Blocks } from "../../components/blocks-renderer";
 
 export default function TrainingPage(
-    props: AsyncReturnType<typeof getStaticProps>["props"]
+  props: AsyncReturnType<typeof getStaticProps>["props"]
 ) {
-    const { data } = useTina({
-        data: props.data,
-        query: props.query,
-        variables: props.variables,
-    });
+  const { data } = useTina({
+    data: props.data,
+    query: props.query,
+    variables: props.variables,
+  });
 
-    const videoCardProps =
-        data.training.videos?.videoCards?.map<VideoCardProps>((m) => ({
-            title: m.title,
-            link: m.link,
-        })) || [];
+  const videoCardProps =
+    data.training.videos?.videoCards?.map<VideoCardProps>((m) => ({
+      title: m.title,
+      link: m.link,
+    })) || [];
 
-    return (
-        <>
-            <SEO seo={data.training.seo} />
-            <Layout>
-                <TrainingCarousel data={data.training.trainingHeaderCarousel} />
+  return (
+    <>
+      <SEO seo={data.training.seo} />
+      <Layout>
+        <TrainingCarousel data={data.training.trainingHeaderCarousel} />
 
-                <Blocks prefix="Training_body" blocks={data.training._body} />
+        <Blocks prefix="Training_body" blocks={data.training._body} />
 
-                <VideoCards cardProps={videoCardProps} channelLink={data.training.videos?.channelLink} defaultChannelLink={data.global.youtubeChannelLink} />
+        <VideoCards
+          cardProps={videoCardProps}
+          channelLink={data.training.videos?.channelLink}
+          defaultChannelLink={data.global.youtubeChannelLink}
+        />
 
-                {/* 
+        {/*
                     Blocked while waiting for testimonials
 
                 <Section color="white" className="">
@@ -48,17 +50,24 @@ export default function TrainingPage(
                     </Container>
                 </Section> */}
 
-                <Section color="white">
-                    <Container className={"flex-1 pt-0"}>
-                        <div className="flex flex-col items-center pb-15 text-center">
-                            <h2>Trusted by more than <span className="text-sswRed">1400+</span> clients in the world</h2>
-                            <p className="max-w-3xl text-lg font-light text-gray-500">Our software developers & consultants have delivered the best in the business to more than 1,400 clients in 15 countries. Read more</p>
-                        </div>
-                        <ClientLogos />
-                    </Container>
-                </Section>
+        <Section color="white">
+          <Container className={"flex-1 pt-0"}>
+            <div className="flex flex-col items-center pb-15 text-center">
+              <h2>
+                Trusted by more than <span className="text-sswRed">1400+</span>{" "}
+                clients in the world
+              </h2>
+              <p className="max-w-3xl text-lg font-light text-gray-500">
+                Our software developers & consultants have delivered the best in
+                the business to more than 1,400 clients in 15 countries. Read
+                more
+              </p>
+            </div>
+            <ClientLogos />
+          </Container>
+        </Section>
 
-                {/* 
+        {/*
                     Blocked by: https://github.com/SSWConsulting/SSW.Website-v3/issues/282
 
                     <Section
@@ -78,66 +87,65 @@ export default function TrainingPage(
                         </div>
                     </Container>
             </Section>*/}
-            </Layout>
-        </>
-    );
+      </Layout>
+    </>
+  );
 }
 
 export const getStaticProps = async ({ params }) => {
-    const tinaProps = await client.queries.trainingContentQuery({
-        relativePath: `${params.filename}.mdx`,
+  const tinaProps = await client.queries.trainingContentQuery({
+    relativePath: `${params.filename}.mdx`,
+  });
+
+  const testimonials = await client.queries.testimonalsQuery();
+
+  let testimonialsResult = testimonials.data.testimonialsConnection.edges.map(
+    (t) => t.node
+  );
+
+  testimonialsResult = testimonialsResult.sort(() => 0.5 - Math.random());
+
+  // Adds general testimonials if not filled by testimonials with matching categories
+  if (testimonialsResult.length < 3) {
+    const generalTestimonials = await client.queries.testimonalsQuery({
+      categories: "General",
     });
 
-    const testimonials = await client.queries.testimonalsQuery();
+    const generalTestimonialsResult =
+      generalTestimonials.data.testimonialsConnection.edges.map((t) => t.node);
 
-    let testimonialsResult = testimonials.data.testimonialsConnection.edges.map(
-        (t) => t.node
+    const randomGeneral = generalTestimonialsResult.sort(
+      () => 0.5 - Math.random()
     );
+    testimonialsResult.push(...randomGeneral);
+  }
 
-    testimonialsResult = testimonialsResult.sort(() => 0.5 - Math.random());
+  testimonialsResult = testimonialsResult.slice(0, 3);
+  testimonialsResult.map((testimonial) => (testimonial.rating = 5));
 
-    // Adds general testimonials if not filled by testimonials with matching categories
-    if (testimonialsResult.length < 3) {
-        const generalTestimonials = await client.queries.testimonalsQuery({
-            categories: "General",
-        });
-
-        const generalTestimonialsResult =
-            generalTestimonials.data.testimonialsConnection.edges.map((t) => t.node);
-
-        const randomGeneral = generalTestimonialsResult.sort(
-            () => 0.5 - Math.random()
-        );
-        testimonialsResult.push(...randomGeneral);
-    }
-
-    testimonialsResult = testimonialsResult.slice(0, 3);
-    testimonialsResult.map(testimonial => testimonial.rating = 5);
-
-
-    return {
-        props: {
-            data: tinaProps.data,
-            query: tinaProps.query,
-            variables: tinaProps.variables,
-            testimonialResult: testimonialsResult,
-            env: {
-                GOOGLE_RECAPTCHA_KEY: process.env.GOOGLE_RECAPTCHA_KEY || null,
-            },
-        },
-        revalidate: 10
-    };
+  return {
+    props: {
+      data: tinaProps.data,
+      query: tinaProps.query,
+      variables: tinaProps.variables,
+      testimonialResult: testimonialsResult,
+      env: {
+        GOOGLE_RECAPTCHA_KEY: process.env.GOOGLE_RECAPTCHA_KEY || null,
+      },
+    },
+    revalidate: 10,
+  };
 };
 
 export const getStaticPaths = async () => {
-    const pagesListData = await client.queries.trainingConnection();
-    return {
-        paths: pagesListData.data.trainingConnection.edges.map((page) => ({
-            params: { filename: page.node._sys.filename },
-        })),
-        fallback: false,
-    };
+  const pagesListData = await client.queries.trainingConnection();
+  return {
+    paths: pagesListData.data.trainingConnection.edges.map((page) => ({
+      params: { filename: page.node._sys.filename },
+    })),
+    fallback: false,
+  };
 };
 
 export type AsyncReturnType<T extends (...args: any) => Promise<any>> = // eslint-disable-line @typescript-eslint/no-explicit-any
-    T extends (...args: any) => Promise<infer R> ? R : any; // eslint-disable-line @typescript-eslint/no-explicit-any
+  T extends (...args: any) => Promise<infer R> ? R : any; // eslint-disable-line @typescript-eslint/no-explicit-any
