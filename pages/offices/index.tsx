bdff42cdda0a99c9b75b24719c18270c01e46c8a
Brady Stroud <bradystroud@ssw.com.au>
2023-04-24 16:15:49 +1000

    ✨  Define office order in global settings

--- pages/offices/index.tsx
+++ pages/offices/index.tsx
@@ -20,116 +20,120 @@ export default function OfficeIndex(
     data: props.data,
   });
 
-  const offices = data.officesConnection.edges.map((edge) => edge.node);
+  const offices = data?.map((office) => office.office);
+  console.log(offices);
 
   return (
-    <Layout>
-      {/* TODO: SEO */}
-      <Container className="flex-1 pt-2">
-        <Breadcrumbs
-          path={"/offices"}
-          suffix={layoutData.breadcrumbSuffix}
-          title={"Offices"}
-        />
+    offices && (
+      <Layout>
+        {/* TODO: SEO */}
+        <Container className="flex-1 pt-2">
+          <Breadcrumbs
+            path={"/offices"}
+            suffix={layoutData.breadcrumbSuffix}
+            title={"Offices"}
+          />
 
-        <div className="md:flex">
-          <div className="grow">
-            <h1 className="pt-0">Our Offices</h1>
-            {offices.map((office) => (
-              <div key={office.addressLocality} className="mb-10 block">
-                <span>
-                  <h2 className="mt-0 text-sswRed">
-                    {`${office.addressLocality} | ${office.addressCountry}`}{" "}
-                    <Flag country={office.addressCountry} />
-                  </h2>
-                </span>
-                {office.thumbnail ? (
-                  <Image
-                    className="float-left mr-4 pb-3"
-                    src={office.thumbnail}
-                    width={115}
-                    height={115}
-                    alt="Office Thumbnail"
-                  />
-                ) : (
-                  <></>
-                )}
-                <p className="block max-sm:clear-left">
-                  {office.streetAddress}
-                  <br />
-                  {office.suburb}, {office.addressRegion} {office.postalCode}
-                </p>
-                <p className="block max-sm:clear-left">
-                  <strong>Phone: {office.phone}</strong>
-                </p>
-                <p className="block max-sm:clear-left">
-                  <Link
-                    href={
-                      office.url ||
-                      `/offices/${office.addressLocality.toLowerCase()}`
-                    }
-                  >
-                    Learn more about our {office.addressLocality} office
-                  </Link>
-                </p>
-                <p className="block max-sm:clear-left">
-                  <Link
-                    href={`${`/offices/${office.addressLocality.toLowerCase()}`}#Directions`}
-                  >
-                    Directions to SSW {office.addressLocality}
-                  </Link>
-                </p>
-              </div>
-            ))}
-            <hr className="my-3" />
-            <div className="border-2 bg-gray-100 px-4 py-2">
-              <p>
-                Our staff are ready to work remotely for any country globally.
-                We have worked for clients from the
-                <strong> USA, Canada, the UK</strong>, and even European
-                countries, such as <strong>France, Germany</strong>, and as far
-                north as <strong>Sweden</strong>.
-              </p>
-            </div>
-
-            <br />
-            <p>
-              If you require any further information, don't hesitate to{" "}
-              <a href="mailto:info@ssw.com.au">contact us.</a>
-            </p>
-            <br />
-            <p>
-              Visit our worldwide website: <Link href="/">ssw.com</Link>.
-            </p>
-          </div>
-          <div className="md:max-w-sm md:pl-6">
-            <div className="prose max-w-full">
-              <h3>SSW Offices</h3>
-              <ul>
-                {offices.map((office) => (
-                  <li key={office.addressLocality}>
+          <div className="md:flex">
+            <div className="grow">
+              <h1 className="pt-0">Our Offices</h1>
+              {offices.map((office) => (
+                <div key={office.addressLocality} className="mb-10 block">
+                  <span>
+                    <h2 className="mt-0 text-sswRed">
+                      {`${office.addressLocality} | ${office.addressCountry}`}{" "}
+                      <Flag country={office.addressCountry} />
+                    </h2>
+                  </span>
+                  {office.thumbnail ? (
+                    <Image
+                      className="float-left mr-4 pb-3"
+                      src={office.thumbnail}
+                      width={115}
+                      height={115}
+                      alt="Office Thumbnail"
+                    />
+                  ) : (
+                    <></>
+                  )}
+                  <p className="block max-sm:clear-left">
+                    {office.streetAddress}
+                    <br />
+                    {office.suburb}, {office.addressRegion} {office.postalCode}
+                  </p>
+                  <p className="block max-sm:clear-left">
+                    <strong>Phone: {office.phone}</strong>
+                  </p>
+                  <p className="block max-sm:clear-left">
+                    <Link
+                      href={
+                        office.url ||
+                        `/offices/${office.addressLocality.toLowerCase()}`
+                      }
+                    >
+                      Learn more about our {office.addressLocality} office
+                    </Link>
+                  </p>
+                  <p className="block max-sm:clear-left">
                     <Link
                       href={`${`/offices/${office.addressLocality.toLowerCase()}`}#Directions`}
                     >
-                      {office.addressLocality}
+                      Directions to SSW {office.addressLocality}
                     </Link>
-                  </li>
-                ))}
-              </ul>
-              <div className="hidden sm:block">
-                <MicrosoftPanel />
-                <TestimonialPanel testimonial={props.testimonial} />
+                  </p>
+                </div>
+              ))}
+              <hr className="my-3" />
+              <div className="border-2 bg-gray-100 px-4 py-2">
+                <p>
+                  Our staff are ready to work remotely for any country globally.
+                  We have worked for clients from the
+                  <strong> USA, Canada, the UK</strong>, and even European
+                  countries, such as <strong>France, Germany</strong>, and as
+                  far north as <strong>Sweden</strong>.
+                </p>
+              </div>
+              <br />
+              <p>
+                If you require any further information, don't hesitate to{" "}
+                <a href="mailto:info@ssw.com.au">contact us.</a>
+              </p>
+              <br />
+              <p>
+                Visit our worldwide website: <Link href="/">ssw.com</Link>.
+              </p>
+            </div>
+            <div className="md:max-w-sm md:pl-6">
+              <div className="prose max-w-full">
+                <h3>SSW Offices</h3>
+                <ul>
+                  {offices.map((office) => (
+                    <li key={office.addressLocality}>
+                      <Link
+                        href={`${`/offices/${office.addressLocality.toLowerCase()}`}#Directions`}
+                      >
+                        {office.addressLocality}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+                <div className="hidden sm:block">
+                  <MicrosoftPanel />
+                  <TestimonialPanel testimonial={props.testimonial} />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </Container>
-    </Layout>
+        </Container>
+      </Layout>
+    )
   );
 }
 
 export const getStaticProps = async () => {
-  const tinaProps = await client.queries.officesConnection();
+  const tinaProps = await client.queries.officeIndexQuery({
+    relativePath: "index.json",
+  });
 
   const testimonialResult = await client.queries.allTestimonialsQuery();
   const testimonials =
@@ -137,9 +141,11 @@ export const getStaticProps = async () => {
   const testimonial =
     testimonials[Math.floor(Math.random() * testimonials.length)].Testimonial;
 
+  console.log(tinaProps);
+
   return {
     props: {
-      data: tinaProps.data,
+      data: tinaProps.data.global.officesIndex,
       query: tinaProps.query,
       variables: tinaProps.variables,
       testimonial: testimonial,
