ad65e757138e142b44439dc90dffe40971e84971
John Xu [SSW] <johnxu@ssw.com.au>
2023-05-05 09:09:10 +0800

    ✨ 282 - Newsletters (#327)
    * wip
    
    * Removed newsletter page collection, replaced with mdx file
    
    * Modification of subButton component to manage newsletter subscription
    
    Currently the endpoint url needs to be added to environment variable.
    
    * Adding Subscribtion to News Letter button to Tina CMS
    
    * Usage of Next.js API endpoint instead of calling the service by himself.
    
    * Migrate all newsletters + manage url to open the html file in another tab.
    
    * Adding newsletter list to Tina CMS, dynamicly render hard codded string in template html
    
    * (chore): Testing new way of displaying newsletters using TinaCMS and json files
    
    * (fix): Fixing the edit feature on TinaCMS side
    
    * News structure of json files + folder, years 2000/2001/2002 done
    
    * Adding newsletters to public folder, reorganize folders and files names as well
    
    * Reactoring, changing variable names and fixing subNewsLettersButton render
    
    * Removing all unnecessary files.
    
    * Improvement of SubNewsLettersButton component (with documentation)
    
    * Adding first Images folder, changing all url of html and htm files
    
    * Adding header link to old newsletters
    
    * Changing all the broken links of images
    
    * Adding recent images of newsletters
    
    * Adding all the missing images to the image folder in public folder, changing broken links in html files (regarding images), fix broken html files that doesnt open.
    
    * Using absolute path for urls.
    
    * remove unecessary package (xlm-js)
    
    * Adding template of newsletters
    
    * Fixing the breadcrumbs bug during compilation.
    
    * Using GraphQL for fetching datas from TinaCRM, update the flow about subscription that reflects production environement, adding firstname and lastname inputs (mandatory in production environement)
    
    * Minor improvement : newsletters stick together
    
    * Reverse month sorting
    
    * Minor refactorisation
    
    * Changing path to media in TinaCMS to allows adding newsletters to newsletters folder in public folder
    
    * Factorisation of inputs in subnewslettersbutton.tsx
    
    * Adding validation message for user when subscribing to newsletters when one input is empty.
    
    * Updating documentation about old newsletters scripts and files
    
    * Fixing broken descriptions in some newsletters json files
    
    * Revert mediaRoot folder in tinCMS config
    
    * ♻️  Split up readme to scoped readme
    
    ---------
    
    Co-authored-by: Harry Ross <hazross@hotmail.com>
    Co-authored-by: Gilles Pothieu <gilles.pothieu@gmail.com>
    Co-authored-by: Brady Stroud <bradystroud@ssw.com.au>

---
+++ components/blocks/newslettersTable.tsx
@@ -0,0 +1,97 @@
+import { useEffect, useState } from "react";
+import { Template } from "tinacms";
+import { transformIntToMonth } from "../../services/date.service";
+import client from "../../.tina/__generated__/client";
+
+/**
+ * Render a table of newsletters.
+ * @param data The data for the table.
+ * @returns The table component.
+ */
+export const NewslettersTable: React.FC<{ data: { headerText: string } }> = ({
+  data,
+}) => {
+  const [newsletters, setNewsletters] = useState([]);
+  const [hasLoaded, setHasLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!hasLoaded) {
+      loadNewsletters();
+    }
+  }, [hasLoaded]);
+
+  const loadNewsletters = () => {
+    client.queries.newslettersConnection().then((data) => {
+      const newsletters = data.data.newslettersConnection.edges.map((edge) => ({
+        newsletters: edge.node.newsletters,
+        newsletters_year: edge.node.newsletters_year,
+      }));
+      const sortedNewslettersData = newsletters.map((item) => {
+        const sortedNewsletters = item.newsletters.sort(
+          (a, b) => b.month - a.month
+        );
+        return {
+          newsletters: sortedNewsletters,
+          year: item.newsletters_year,
+        };
+      });
+
+      setNewsletters(sortedNewslettersData?.reverse());
+      setHasLoaded(true);
+    });
+  };
+
+  const renderTable = ({ newsletters, year }) => (
+    <table key={year} className="m-0 w-full">
+      <thead>
+        <tr>
+          <th className="w-1/6 bg-gray-300 px-3 py-1" align="left">
+            {year}
+          </th>
+          <th className="w-5/6 bg-gray-300 px-3 py-1" align="left">
+            Description
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {newsletters.map(({ url, month, description }) => (
+          <tr key={url} className="bg-gray-125">
+            <td className="border-1 border-white px-3 py-1">
+              <a
+                href={`/newsletters/${year}/${url}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {transformIntToMonth(month)}
+              </a>
+            </td>
+            <td className="border-1 border-white px-3 py-1">{description}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+
+  return (
+    <>
+      <h2 className="mb-3 mt-5 text-2xl text-sswRed">{data.headerText}</h2>
+      {hasLoaded ? (
+        newsletters.map(renderTable)
+      ) : (
+        <p className="text-xl">Loading Newsletters...</p>
+      )}
+    </>
+  );
+};
+
+export const newslettersTableBlockSchema: Template = {
+  name: "NewslettersTable",
+  label: "Newsletters Table",
+  fields: [
+    {
+      type: "string",
+      label: "Header text",
+      name: "headerText",
+    },
+  ],
+};
