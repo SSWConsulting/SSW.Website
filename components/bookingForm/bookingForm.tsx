68b4ef207da8a1ace5da308c149856ef34f230e7
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2023-05-10 10:18:38 +1000

    Booking form - adding reCaptcha validation on server side (#473)
    * Booking form - adding reCaptcha validation on server side
    
    * Update pages/consulting/[filename].tsx - empty string instead of null
    
    Co-authored-by: Matt Wicks <wicksipedia@users.noreply.github.com>
    
    * Update - env variables to avoid conflict and put it in template-build
    
    * Update _schema.json
    
    * Update - Error msg for reCaptcha
    
    * Update - schema and Filename for Site Key
    
    * Update action.yml
    
    * Update - template build for secrets
    
    * Revert "Merge branch 'validate-recaptcha' of https://github.com/SSWConsulting/SSW.Website-v3 into validate-recaptcha"
    
    This reverts commit 2c0a02b1d09a585456aff59f40a1e526b4de16aa, reversing
    changes made to 42a9c6b044acec4f6252fda2f0263611f4e17b05.
    
    * Update - adding reCaptcha validation on the backend
    
    * Update - removed the unused google recaptcha key
    
    * Update Infra - adding key vault to app config
    
    * Updating parameter file's path
    
    * updating projectname in SSW website
    
    * Updating --parameter while deploying the resource
    
    * Update - adding App settings to action.yml
    
    * Updated - capital v for v2 of recaptcha key
    
    * Update - adding secrets as input to action.yml
    
    * Update the syntax for the slot name comparison
    
    * Update the condition for recreating the slot
    
    * Updating the syntax for applying settings
    
    * Update - adding double quote for the settings variables for production
    
    * Update - clean up
    
    * Update .github/actions/deploy/action.yml
    
    Co-authored-by: Matt Wicks <wicksipedia@users.noreply.github.com>
    
    * Update infra/addSecrets.bicep
    
    Co-authored-by: Matt Wicks <wicksipedia@users.noreply.github.com>
    
    * deleted unused file
    
    * Update - removed hardcoded value, providing one from acr
    
    * Update - passing rolename to assignment instead of id
    
    * Update - keyVault name in the keyVault instead of main.bicep
    
    * Update - KV Assignment role to PR slots
    
    * Update - fixed typo and removed the unnecessary variable
    
    * Update - separating PR resources and pipeline clean up
    
    * Update - adding single quote with string
    
    * Fixed - slot were not found
    
    * Update - removing guards from module
    
    * Update - adding keyvault creation in the bicep
    
    * Update - secret name to match keyvault
    
    * Update - graphql and schema
    
    * Update - removing required field from the template file
    
    * Update - removing some undefined variables from template
    
    * Removing files with old names
    
    * Update - graphql and schema.json
    
    * Adding console log to see the all variables
    
    * Update - adding secrets in the pipeline
    
    * Update - clean up for the code
    
    * Update - adding key vault reference for kv
    
    * Adding assignment role to the logged in user
    
    * Update - adding carriage
    
    * Updating the syntax
    
    * removing carriage at the end of line
    
    * Update - resource group variable
    
    * Update - key vault role
    
    * Update adding Object Id for role assignment
    
    * fixing - site key not being passed to next step
    
    * Fixed - error on setting env variable
    
    * Added query to return only value of secret
    
    * Setting up the output variable
    
    * Update - adding github output
    
    * Update - adding deployment name with date
    
    * Template - Clean up
    
    * Update - removed bicep from pipeline to env deployment
    
    ---------
    
    Co-authored-by: Matt Wicks <wicksipedia@users.noreply.github.com>

--- components/bookingForm/bookingForm.tsx
+++ components/bookingForm/bookingForm.tsx
@@ -7,13 +7,13 @@ import FormGroupInput from "../form/formGroupInput";
 import FormGroupSelect from "../form/formGroupSelect";
 import FormGroupTextArea from "../form/formGroupTextArea";
 import {
-	ACTIVE_INPUT,
-	AUSTRALIA,
-	AustralianStatesList,
-	CONTACT_FORM_TITLE,
-	FORM_INPUT,
-	FormCountriesList,
-	STATE_DEFAULT_VALUE,
+  ACTIVE_INPUT,
+  AUSTRALIA,
+  AustralianStatesList,
+  CONTACT_FORM_TITLE,
+  FORM_INPUT,
+  FormCountriesList,
+  STATE_DEFAULT_VALUE,
 } from "../util/constants";
 import { bookingFormSubmissionData } from "./bookingFormSubmissionData";
 import { ValidationSchema } from "./validationSchema";
@@ -22,246 +22,262 @@ import { Open_Sans } from "next/font/google";
 import classNames from "classnames";
 
 const openSans = Open_Sans({
-	variable: "--open-sans-font",
-	subsets: ["latin"],
+  variable: "--open-sans-font",
+  subsets: ["latin"],
 });
 
 export const BookingForm = ({ recaptchaKey }) => {
-	//Show FormStates and Active label
-	const [contactSuccess, setContactSuccess] = useState(false);
-	const [country, setCountry] = useState("");
-	const [activeInputLabel, setActiveInputLabel] = useState({});
-	const router = useRouter();
+  //Show FormStates and Active label
+  const [contactSuccess, setContactSuccess] = useState(false);
+  const [country, setCountry] = useState("");
+  const [activeInputLabel, setActiveInputLabel] = useState({});
+  const router = useRouter();
 
-	const initialFormValues = {
-		fullName: "",
-		email: "",
-		phone: "",
-		location: "",
-		states: STATE_DEFAULT_VALUE,
-		note: "",
-		company: "",
-		referredCompany: "",
-		referredFullName: "",
-		referredEmail: "",
-	};
+  const initialFormValues = {
+    fullName: "",
+    email: "",
+    phone: "",
+    location: "",
+    states: STATE_DEFAULT_VALUE,
+    note: "",
+    company: "",
+    referredCompany: "",
+    referredFullName: "",
+    referredEmail: "",
+  };
 
-	//Condition to avoid SSR (Server-Side Rendering) for getting page path
-	let sourceWebPageURL: string;
-	if (typeof window !== "undefined") {
-		sourceWebPageURL = window.location.href;
-	} else {
-		sourceWebPageURL = "";
-	}
+  //Condition to avoid SSR (Server-Side Rendering) for getting page path
+  let sourceWebPageURL: string;
+  if (typeof window !== "undefined") {
+    sourceWebPageURL = window.location.href;
+  } else {
+    sourceWebPageURL = "";
+  }
 
-	//ReCaptcha
-	const [contactReCaptcha, setContactReCaptcha] = useState("");
+  //ReCaptcha
+  const [contactReCaptcha, setContactReCaptcha] = useState("");
 
-	//returns true if country is Australia
-	const handleStates = (country: string) => {
-		return country === AUSTRALIA;
-	};
+  //returns true if country is Australia
+  const handleStates = (country: string) => {
+    return country === AUSTRALIA;
+  };
 
-	//useMemo is call, whenever country value is changed
-	const isShowStates = useMemo(() => handleStates(country), [country]);
+  //useMemo is call, whenever country value is changed
+  const isShowStates = useMemo(() => handleStates(country), [country]);
 
-	//Changing state of Validation Schema
-	const [schema, setSchema] = useState(() =>
-		ValidationSchema(isShowStates, false)
-	);
+  //Changing state of Validation Schema
+  const [schema, setSchema] = useState(() =>
+    ValidationSchema(isShowStates, false)
+  );
 
-	useEffect(() => {
-		// every time isShowState changes, recreate the schema and set it in the state
-		setSchema(ValidationSchema(isShowStates, false));
-	}, [isShowStates]);
+  const [invalidRecaptcha, setInvalidReptcha] = useState("");
 
-	const handleActiveInputLabel = (targetInput, value) => {
-		if (targetInput !== FORM_INPUT.States && !!value.trim()) {
-			setActiveInputLabel({ ...activeInputLabel, [targetInput]: true });
-		} else if (targetInput === FORM_INPUT.States && isShowStates) {
-			setActiveInputLabel({ ...activeInputLabel, [targetInput]: true });
-		} else {
-			setActiveInputLabel({ ...activeInputLabel, [targetInput]: false });
-		}
-	};
+  useEffect(() => {
+    // every time isShowState changes, recreate the schema and set it in the state
+    setSchema(ValidationSchema(isShowStates, false));
+  }, [isShowStates]);
 
-	const handleOnSubmit = async (values, actions) => {
-		const data = bookingFormSubmissionData(
-			values,
-			false,
-			contactReCaptcha,
-			sourceWebPageURL
-		);
-		actions.setSubmitting(false);
+  const handleActiveInputLabel = (targetInput, value) => {
+    if (targetInput !== FORM_INPUT.States && !!value.trim()) {
+      setActiveInputLabel({ ...activeInputLabel, [targetInput]: true });
+    } else if (targetInput === FORM_INPUT.States && isShowStates) {
+      setActiveInputLabel({ ...activeInputLabel, [targetInput]: true });
+    } else {
+      setActiveInputLabel({ ...activeInputLabel, [targetInput]: false });
+    }
+  };
 
-		await axios
-			.post("/api/create-lead", data)
-			.then(() => {
-				setContactSuccess(true);
-				setTimeout(function () {
-					setContactSuccess(false);
-					router.push("/thankyou/");
-				}, 1000);
-			})
-			.catch((err) => {
-				console.error(err);
-				return alert("Failed to create lead in CRM");
-			});
-	};
+  const handleOnSubmit = async (values, actions) => {
+    const data = bookingFormSubmissionData(
+      values,
+      false,
+      contactReCaptcha,
+      sourceWebPageURL
+    );
+    actions.setSubmitting(false);
 
-	const getCommonFieldProps = (fieldName: string) => ({
-		name: fieldName,
-		activeLabelClass: activeInputLabel[fieldName]
-			? ACTIVE_INPUT.ClassShow
-			: ACTIVE_INPUT.None,
-		handleChange: ({ name }, e) =>
-			handleActiveInputLabel(name, e.currentTarget.value),
-	});
+    await axios
+      .post("/api/create-lead", data)
+      .then((response) => {
+        if (response.data && !response.data.success) {
+          setInvalidReptcha("Invalid ReCaptcha!");
+        } else {
+          onSuccess();
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        return alert("Failed to create lead in CRM");
+      });
+  };
 
-	const getDefaultOption = (fieldName: string) => {
-		return {
-			name: fieldName,
-			value: fieldName.charAt(0).toUpperCase() + fieldName.slice(1),
-		};
-	};
-	const locationDefaultOption = getDefaultOption(FORM_INPUT.Location);
-	const statesDefaultOption = getDefaultOption(FORM_INPUT.States);
+  const onSuccess = () => {
+    setInvalidReptcha("");
+    setContactSuccess(true);
+    setTimeout(function () {
+      setContactSuccess(false);
+      router.push("/thankyou/");
+    }, 1000);
+  };
 
-	return (
-		<div
-			className={classNames(
-				"rounded-none bg-gray-125 font-sans",
-				openSans.variable
-			)}
-		>
-			<div className="relative p-4">
-				<div className="m-0 bg-white px-6 pb-5 pt-1">
-					<h2 className="mb-14 mt-1.5 pt-1.5 !text-2xl text-sswRed">
-						{CONTACT_FORM_TITLE}
-					</h2>
-					{!!contactSuccess && (
-						<div
-							className={
-								"relative mb-8 rounded border-1 border-solid border-green-100 bg-green-50 p-4 text-green-900"
-							}
-							role="alert"
-						>
-							An email has been sent to the SSW Sales team and someone will be
-							in contact with you soon
-						</div>
-					)}
+  const getCommonFieldProps = (fieldName: string) => ({
+    name: fieldName,
+    activeLabelClass: activeInputLabel[fieldName]
+      ? ACTIVE_INPUT.ClassShow
+      : ACTIVE_INPUT.None,
+    handleChange: ({ name }, e) =>
+      handleActiveInputLabel(name, e.currentTarget.value),
+  });
 
-					<Formik
-						validationSchema={schema}
-						initialValues={initialFormValues}
-						onSubmit={handleOnSubmit}
-					>
-						{({ values, isSubmitting }) => (
-							<Form noValidate>
-								<FormGroupInput
-									label={ACTIVE_INPUT.FullName}
-									type="text"
-									{...getCommonFieldProps(FORM_INPUT.FullName)}
-								/>
+  const getDefaultOption = (fieldName: string) => {
+    return {
+      name: fieldName,
+      value: fieldName.charAt(0).toUpperCase() + fieldName.slice(1),
+    };
+  };
+  const locationDefaultOption = getDefaultOption(FORM_INPUT.Location);
+  const statesDefaultOption = getDefaultOption(FORM_INPUT.States);
 
-								<FormGroupInput
-									label={ACTIVE_INPUT.Email}
-									type="email"
-									{...getCommonFieldProps(FORM_INPUT.Email)}
-								/>
+  return (
+    <div
+      className={classNames(
+        "rounded-none bg-gray-125 font-sans",
+        openSans.variable
+      )}
+    >
+      <div className="relative p-4">
+        <div className="m-0 bg-white px-6 pb-5 pt-1">
+          <h2 className="mb-14 mt-1.5 pt-1.5 !text-2xl text-sswRed">
+            {CONTACT_FORM_TITLE}
+          </h2>
+          {!!contactSuccess && (
+            <div
+              className={
+                "relative mb-8 rounded border-1 border-solid border-green-100 bg-green-50 p-4 text-green-900"
+              }
+              role="alert"
+            >
+              An email has been sent to the SSW Sales team and someone will be
+              in contact with you soon
+            </div>
+          )}
 
-								<FormGroupInput
-									label={ACTIVE_INPUT.Phone}
-									type="phone"
-									{...getCommonFieldProps(FORM_INPUT.Phone)}
-								/>
+          <Formik
+            validationSchema={schema}
+            initialValues={initialFormValues}
+            onSubmit={handleOnSubmit}
+          >
+            {({ values, isSubmitting }) => (
+              <Form noValidate>
+                <FormGroupInput
+                  label={ACTIVE_INPUT.FullName}
+                  type="text"
+                  {...getCommonFieldProps(FORM_INPUT.FullName)}
+                />
 
-								<FormGroupSelect
-									label={ACTIVE_INPUT.Location}
-									{...getCommonFieldProps(locationDefaultOption.name)}
-									handleChange={(field, e) => {
-										setCountry(e.currentTarget.value);
-										handleActiveInputLabel(field.name, e.currentTarget.value);
-									}}
-								>
-									<option className="hidden" value="">
-										{locationDefaultOption.value}
-									</option>
-									{FormCountriesList.map((country) => (
-										<option
-											className="cursor-pointer !p-1"
-											key={country.value}
-											value={country.value}
-										>
-											{country.label}
-										</option>
-									))}
-								</FormGroupSelect>
+                <FormGroupInput
+                  label={ACTIVE_INPUT.Email}
+                  type="email"
+                  {...getCommonFieldProps(FORM_INPUT.Email)}
+                />
 
-								{isShowStates ? (
-									<FormGroupSelect
-										label={ACTIVE_INPUT.States}
-										{...getCommonFieldProps(statesDefaultOption.name)}
-									>
-										<option className="hidden" value="">
-											{statesDefaultOption.value}
-										</option>
-										{AustralianStatesList.map((state) => (
-											<option
-												key={state.value}
-												value={state.value}
-												className="cursor-pointer !p-1"
-											>
-												{state.label}
-											</option>
-										))}
-									</FormGroupSelect>
-								) : (
-									<input
-										type="hidden"
-										name={statesDefaultOption.name}
-										value={(values.states = STATE_DEFAULT_VALUE)}
-									/>
-								)}
+                <FormGroupInput
+                  label={ACTIVE_INPUT.Phone}
+                  type="phone"
+                  {...getCommonFieldProps(FORM_INPUT.Phone)}
+                />
 
-								<FormGroupInput
-									label={ACTIVE_INPUT.Company}
-									{...getCommonFieldProps(FORM_INPUT.Company)}
-								/>
+                <FormGroupSelect
+                  label={ACTIVE_INPUT.Location}
+                  {...getCommonFieldProps(locationDefaultOption.name)}
+                  handleChange={(field, e) => {
+                    setCountry(e.currentTarget.value);
+                    handleActiveInputLabel(field.name, e.currentTarget.value);
+                  }}
+                >
+                  <option className="hidden" value="">
+                    {locationDefaultOption.value}
+                  </option>
+                  {FormCountriesList.map((country) => (
+                    <option
+                      className="cursor-pointer !p-1"
+                      key={country.value}
+                      value={country.value}
+                    >
+                      {country.label}
+                    </option>
+                  ))}
+                </FormGroupSelect>
 
-								<FormGroupTextArea
-									label={ACTIVE_INPUT.Note}
-									placeholder="How can we help you?"
-									rows={4}
-									maxLength={2000}
-									{...getCommonFieldProps(FORM_INPUT.Note)}
-								/>
+                {isShowStates ? (
+                  <FormGroupSelect
+                    label={ACTIVE_INPUT.States}
+                    {...getCommonFieldProps(statesDefaultOption.name)}
+                  >
+                    <option className="hidden" value="">
+                      {statesDefaultOption.value}
+                    </option>
+                    {AustralianStatesList.map((state) => (
+                      <option
+                        key={state.value}
+                        value={state.value}
+                        className="cursor-pointer !p-1"
+                      >
+                        {state.label}
+                      </option>
+                    ))}
+                  </FormGroupSelect>
+                ) : (
+                  <input
+                    type="hidden"
+                    name={statesDefaultOption.name}
+                    value={(values.states = STATE_DEFAULT_VALUE)}
+                  />
+                )}
 
-								<div className="mb-4 w-full overflow-x-auto">
-									<div className="h-22 w-88">
-										{recaptchaKey && (
-											<ReCAPTCHA
-												sitekey={recaptchaKey}
-												onChange={(value) => {
-													setContactReCaptcha(value);
-												}}
-											/>
-										)}
-									</div>
-								</div>
+                <FormGroupInput
+                  label={ACTIVE_INPUT.Company}
+                  {...getCommonFieldProps(FORM_INPUT.Company)}
+                />
 
-								<button
-									type="submit"
-									disabled={isSubmitting}
-									className="done px-3 py-1.5"
-								>
-									SUBMIT
-								</button>
-							</Form>
-						)}
-					</Formik>
-				</div>
-			</div>
-		</div>
-	);
+                <FormGroupTextArea
+                  label={ACTIVE_INPUT.Note}
+                  placeholder="How can we help you?"
+                  rows={4}
+                  maxLength={2000}
+                  {...getCommonFieldProps(FORM_INPUT.Note)}
+                />
+
+                <div className="mb-4 w-full overflow-x-auto">
+                  <div className="h-22 w-88">
+                    {recaptchaKey && (
+                      <ReCAPTCHA
+                        sitekey={recaptchaKey}
+                        onChange={(value) => {
+                          setContactReCaptcha(value);
+                        }}
+                      />
+                    )}
+                  </div>
+                  {invalidRecaptcha && (
+                    <span className="text-sm text-red-600">
+                      {invalidRecaptcha}
+                    </span>
+                  )}
+                </div>
+
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="done px-3 py-1.5"
+                >
+                  SUBMIT
+                </button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </div>
+    </div>
+  );
 };
--- pages/api/create-lead.ts
+++ pages/api/create-lead.ts
@@ -1,13 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { createLead } from "../../services";
+import { createLead, validateRecaptcha } from "../../services";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "POST") {
-    const createLeadRes = await createLead(req.body);
-    res.status(createLeadRes.status).json(createLeadRes.data);
+    const recaptchaResponse = await validateRecaptcha(req.body);
+
+    if (recaptchaResponse && recaptchaResponse.data.success === true) {
+      const createLeadRes = await createLead(req.body);
+      res.status(createLeadRes.status).json(createLeadRes.data);
+    } else {
+      console.log(
+        "🚀 ~ file: validate-token.ts:15 ~ validition.data.data:",
+        recaptchaResponse.data
+      );
+      res.status(200).json(recaptchaResponse.data);
+    }
   } else {
     res.status(405).json({ message: "Unsupported method" });
   }
