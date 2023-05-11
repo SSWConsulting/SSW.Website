3d8ce24848b40e8df324a2ed8fbde7b5dacda198
Gilles Pothieu <gilles.pothieu@gmail.com>
2023-05-11 11:16:54 +0200

    Adding a missing key to clients logo images that warn compilation.

--- components/blocks/clientLogos.tsx
+++ components/blocks/clientLogos.tsx
@@ -3,45 +3,44 @@ import type { Template } from "tinacms";
 import Image from "next/image";
 
 const clients = {
-    "allianz": "allianz.jpg",
-    "carnival": "carnival.jpg",
-    "cisco": "cisco.jpg",
-    "commonwealthbank": "commbank.jpg",
-    "symantec": "symatec.jpg",
-    "domain": "domain.jpg",
-    "eventcinemas": "events.jpg",
-    "microsoft": "microsoft.jpg",
-    "toll": "toll.jpg",
+  allianz: "allianz.jpg",
+  carnival: "carnival.jpg",
+  cisco: "cisco.jpg",
+  commonwealthbank: "commbank.jpg",
+  symantec: "symatec.jpg",
+  domain: "domain.jpg",
+  eventcinemas: "events.jpg",
+  microsoft: "microsoft.jpg",
+  toll: "toll.jpg",
 };
 
 export const ClientLogos = () => {
-    return (
-        <div className="flex flex-wrap justify-center gap-6">
-            {
-                Object.keys(clients).map(client =>
-                    <Image
-                        src={`/images/clientLogos/${clients[client]}`}
-                        alt={client}
-                        height={50}
-                        width={200}
-                        className="my-4 max-w-full rounded-lg"
-                    />
-                )
-            }
-        </div>
-    );
+  return (
+    <div className="flex flex-wrap justify-center gap-6">
+      {Object.keys(clients).map((client) => (
+        <Image
+          src={`/images/clientLogos/${clients[client]}`}
+          alt={client}
+          height={50}
+          width={200}
+          className="my-4 max-w-full rounded-lg"
+          key={client}
+        />
+      ))}
+    </div>
+  );
 };
 
 export const clientLogosBlockSchema: Template = {
-    name: "ClientLogos",
-    label: "Client Logos",
-    // Todo: Find a way to have no fields - the one below is to satisfy compiler
-    fields: [
-        {
-            type: "string",
-            label: "Alt text",
-            name: "altText",
-            required: true,
-        },
-    ],
+  name: "ClientLogos",
+  label: "Client Logos",
+  // Todo: Find a way to have no fields - the one below is to satisfy compiler
+  fields: [
+    {
+      type: "string",
+      label: "Alt text",
+      name: "altText",
+      required: true,
+    },
+  ],
 };
