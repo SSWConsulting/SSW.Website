c2d8969581b272d90cd68f020fa6b9419a9fd36f
Gilles Pothieu <gilles.pothieu@gmail.com>
2023-05-11 17:37:29 +0200

    Delete hard codded clients logo fonctionnality using now TinaCMS use interface

--- components/blocks/clientLogos.tsx
+++ components/blocks/clientLogos.tsx
@@ -1,47 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import type { Template } from "tinacms";
 import Image from "next/image";
-
-const clients = {
-    "allianz": "allianz.jpg",
-    "carnival": "carnival.jpg",
-    "cisco": "cisco.jpg",
-    "commonwealthbank": "commbank.jpg",
-    "symantec": "symatec.jpg",
-    "domain": "domain.jpg",
-    "eventcinemas": "events.jpg",
-    "microsoft": "microsoft.jpg",
-    "toll": "toll.jpg",
-};
+import client from "../../.tina/__generated__/client";
 
 export const ClientLogos = () => {
-    return (
-        <div className="flex flex-wrap justify-center gap-6">
-            {
-                Object.keys(clients).map(client =>
-                    <Image
-                        src={`/images/clientLogos/${clients[client]}`}
-                        alt={client}
-                        height={50}
-                        width={200}
-                        className="my-4 max-w-full rounded-lg"
-                    />
-                )
-            }
-        </div>
-    );
+  const [clientsData, setclientsData] = useState([]);
+
+  useEffect(() => {
+    loadClientsData();
+  }, []);
+
+  const loadClientsData = () => {
+    client.queries.globalConnection().then((data) => {
+      const clientsData =
+        data.data.globalConnection.edges[0].node.clients.clientsList;
+      setclientsData(clientsData);
+    });
+  };
+
+  return (
+    <div className="flex flex-wrap justify-center gap-6">
+      {clientsData.length &&
+        clientsData.map((client) => (
+          <Image
+            key={client.clientName}
+            src={client.imageUrl}
+            alt={client}
+            height={50}
+            width={200}
+            className="my-4 max-w-full rounded-lg"
+          />
+        ))}
+    </div>
+  );
 };
 
 export const clientLogosBlockSchema: Template = {
-    name: "ClientLogos",
-    label: "Client Logos",
-    // Todo: Find a way to have no fields - the one below is to satisfy compiler
-    fields: [
-        {
-            type: "string",
-            label: "Alt text",
-            name: "altText",
-            required: true,
-        },
-    ],
+  name: "ClientLogos",
+  label: "Client Logos",
+  // Todo: Find a way to have no fields - the one below is to satisfy compiler
+  fields: [
+    {
+      type: "string",
+      label: "Alt text",
+      name: "altText",
+      required: true,
+    },
+  ],
 };
