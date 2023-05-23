75925aaac6dadc36711b37931d4e337b18594a64
Gilles Pothieu <gilles.pothieu@gmail.com>
2023-05-23 16:37:39 +0200

    Changing logo, and text

--- components/training/trainingInformation.tsx
+++ components/training/trainingInformation.tsx
@@ -8,60 +8,69 @@ import { verticalListItemSchema } from "../blocks/verticalListItem";
 import { recurringEventSchema } from "../blocks/recurringEvent";
 
 export type TrainingInformationItemProps = {
-    header: string;
-    body: TinaMarkdownContent | TinaMarkdownContent[];
+  header: string;
+  body: TinaMarkdownContent | TinaMarkdownContent[];
 };
 
-const TrainingInformationItem: FC<TrainingInformationItemProps> = ({ header, body }) => {
-    return (
-        <div className="flex flex-col text-center lg:text-left">
-            <h1 className="mt-0 pt-0" dangerouslySetInnerHTML={{ __html: header }}></h1>
-            <div className="w-full items-center text-left sm:w-3/4 lg:w-full">
-                <TinaMarkdown
-                    components={componentRenderer}
-                    content={body}
-                />
-            </div>
-        </div>
-    )
-}
+const TrainingInformationItem: FC<TrainingInformationItemProps> = ({
+  header,
+  body,
+}) => {
+  return (
+    <div className="flex flex-col text-center lg:text-left">
+      <h1
+        className="mt-0 pt-0"
+        dangerouslySetInnerHTML={{ __html: header }}
+      ></h1>
+      <div className="w-full items-center p-5 text-left sm:w-3/4 lg:w-full">
+        <TinaMarkdown components={componentRenderer} content={body} />
+      </div>
+    </div>
+  );
+};
 
 export const TrainingInformation = ({ data }) => {
-    return (
-        <Section color="white">
-            <Container className={"flex-1 p-0"}>
-                <div className="grid grid-cols-1 justify-between py-10 lg:grid-cols-3">
-                    {data.trainingInformationItems?.map((item, key) => <TrainingInformationItem key={key} header={item.header} body={item.body} />)}
-                </div>
-            </Container>
-        </Section>
-    );
-}
+  return (
+    <Section color="white">
+      <Container className={"flex-1 p-0"}>
+        <div className="grid grid-cols-1 justify-between py-10 lg:grid-cols-3">
+          {data.trainingInformationItems?.map((item, key) => (
+            <TrainingInformationItem
+              key={item.body}
+              header={item.header}
+              body={item.body}
+            />
+          ))}
+        </div>
+      </Container>
+    </Section>
+  );
+};
 
 export const trainingInformationSchema: Template = {
-    label: "Training Information",
-    name: "TrainingInformation",
-    fields: [
+  label: "Training Information",
+  name: "TrainingInformation",
+  fields: [
+    {
+      type: "object",
+      label: "Training Information Items",
+      name: "trainingInformationItems",
+      list: true,
+      fields: [
         {
-            type: "object",
-            label: "Training Information Items",
-            name: "trainingInformationItems",
-            list: true,
-            fields: [
-                {
-                    type: "string",
-                    label: "Header",
-                    name: "header",
-                },
-                {
-                    type: "rich-text",
-                    label: "Body",
-                    name: "body",
-                    templates: [verticalListItemSchema, recurringEventSchema],
-                },
-            ],
+          type: "string",
+          label: "Header",
+          name: "header",
         },
-    ],
+        {
+          type: "rich-text",
+          label: "Body",
+          name: "body",
+          templates: [verticalListItemSchema, recurringEventSchema],
+        },
+      ],
+    },
+  ],
 };
 
 export default TrainingInformation;
