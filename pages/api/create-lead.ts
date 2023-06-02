5fb0c4312767d1b2ca2d582f2c9760deddacedcc
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2023-06-02 15:21:56 +1000

    Create Lead - Exception handling

--- pages/api/create-lead.ts
+++ pages/api/create-lead.ts
@@ -1,15 +1,39 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import * as appInsight from "applicationinsights";
-import { BookingFormSubmissionData } from "../../services";
-import axios from "axios";
+import { BookingFormSubmissionData, HttpStatusCode } from "../../services";
+import { CustomError } from "../../services/customError";
+import axios, { AxiosError } from "axios";
+
+enum STEP {
+  PA_FLOW_AXIOS = "Power Automate flow - Axio",
+  PA_FLOW = "Power Automate flow",
+  GOOGLE_RECAPTCHA = "Google - Recaptcha",
+}
 
 const createLead = async (data: BookingFormSubmissionData) => {
-  return await axios.post(process.env.CREATE_LEAD_ENDPOINT, data, {
-    headers: { "Content-Type": "application/json" },
-  });
+  try {
+    const response = await axios.post(process.env.CREATE_LEAD_ENDPOINT, data, {
+      headers: { "Content-Type": "application/json" },
+    });
+    return response;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const axiosError = error as AxiosError;
+
+      throw new CustomError(
+        axiosError.cause,
+        HttpStatusCode.NotFound,
+        data,
+        appInsight.Contracts.SeverityLevel.Critical,
+        STEP.PA_FLOW_AXIOS
+      );
+    } else {
+      throw new Error(error);
+    }
+  }
 };
 
-const validateRecaptcha = async ({ Recaptcha }) => {
+const validateRecaptcha = async (Recaptcha) => {
   return await axios.post(
     `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.GOOGLE_RECAPTCHA_KEY_V2}&response=${Recaptcha}`
   );
@@ -19,33 +43,61 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
-    const recaptchaResponse = await validateRecaptcha(req.body);
+  try {
+    if (req.method === "POST") {
+      const { Recaptcha } = req.body;
+      if (Recaptcha) {
+        const recaptchaValidation = await validateRecaptcha(Recaptcha);
+        // const recaptchaValidation = { data: { success: true } }; uncomment this to bypass recaptcha for testing purpose
 
-    const logData = {
-      error: recaptchaResponse.data,
-      lead: req.body,
-      method: "Create-lead-API",
-    };
+        if (recaptchaValidation && recaptchaValidation.data.success) {
+          const createLeadFlow = await createLead(req.body);
 
-    if (recaptchaResponse && recaptchaResponse.data.success === true) {
-      const createLeadRes = await createLead(req.body);
+          if (createLeadFlow.status !== HttpStatusCode.Accepted) {
+            throw new CustomError(
+              JSON.stringify(createLeadFlow.data),
+              createLeadFlow.status,
+              req.body,
+              appInsight.Contracts.SeverityLevel.Critical,
+              STEP.PA_FLOW
+            );
+          }
 
-      if (createLeadRes.status !== 202) {
-        logData.error = createLeadRes.statusText;
-        appInsight.defaultClient?.trackException({
-          exception: new Error(JSON.stringify(logData)),
-        });
+          res
+            .status(createLeadFlow.status)
+            .json({ message: "Lead successfully submitted!" });
+        } else {
+          throw new CustomError(
+            JSON.stringify(recaptchaValidation.data),
+            HttpStatusCode.OK,
+            req.body,
+            appInsight.Contracts.SeverityLevel.Error,
+            STEP.GOOGLE_RECAPTCHA
+          );
+        }
+      } else {
+        throw new Error("Recaptcha not found!");
       }
-
-      res.status(createLeadRes.status).json(createLeadRes.data);
     } else {
-      appInsight.defaultClient?.trackException({
-        exception: new Error(JSON.stringify(logData)),
-      });
-      res.status(200).json(recaptchaResponse.data);
+      throw new Error("Unsupported method");
     }
-  } else {
-    res.status(405).json({ message: "Unsupported method" });
+  } catch (error) {
+    if (error instanceof CustomError) {
+      appInsight.defaultClient?.trackException({
+        exception: new Error(error.message),
+        properties: {
+          Method: `Create lead API - ${error.method}`,
+          RequestBody: error.requestBody,
+          Status: error.statusCode,
+        },
+        severity: error.severity,
+      });
+
+      res.status(error.statusCode).json({ message: error.message });
+    } else {
+      res
+        .status(HttpStatusCode.MethodNotAllowed)
+        .json({ message: error.message });
+    }
   }
 }
