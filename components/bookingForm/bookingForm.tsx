f76a8532efdc7868c3bc868271186afc8c3cfd88
Kristen <kristenhu@ssw.com.au>
2023-06-07 16:08:09 +0800

    💄Update modal style as per Jayden's comment

--- components/bookingForm/bookingForm.tsx
+++ components/bookingForm/bookingForm.tsx
@@ -16,7 +16,6 @@ import {
 } from "../util/constants";
 import { bookingFormSubmissionData } from "./bookingFormSubmissionData";
 import { ValidationSchema } from "./validationSchema";
-import Popup from "../popup/popup";
 
 import { useAppInsightsContext } from "@microsoft/applicationinsights-react-js";
 import {
@@ -24,7 +23,7 @@ import {
   RecaptchaContextType,
 } from "../../context/RecaptchaContext";
 
-export const BookingForm = () => {
+export const BookingForm = ({ onClose }) => {
   const { recaptchaKey } = useContext<RecaptchaContextType>(RecaptchaContext);
 
   //Show FormStates and Active label
@@ -143,149 +142,144 @@ export const BookingForm = () => {
     <div className="rounded-none bg-gray-125 font-sans">
       <div className="relative p-4">
         <div className="m-0 bg-white px-6 pb-5 pt-1">
-          <h2 className="mb-14 mt-1.5 pt-1.5 !text-2xl text-sswRed">
-            {CONTACT_FORM_TITLE}
-          </h2>
-          <Popup isVisible={contactSuccess} onClose={setContactSuccess}>
-            <div
-              className={
-                "rounded bg-white font-sans"
-              }
-            >
-              <div className="p-4">
-                <div className="m-0 px-6 pb-5 pt-1 text-lg font-bold">
-                  Submitted
-                </div>
-                <div className="m-0 px-6 pb-5 pt-1">
-                  Thank you, your form has been submitted successfully. We will be in contact as soon as possible
-                  In the meantime, check out our other services and meet our amazing team.
-                </div>
-                <div className="px-6 text-end">
-                  <button
-                    className="bg-sswRed px-3 py-1.5 text-white"
-                    onClick={() => setContactSuccess(false)}
-                  >
-                    OK
-                  </button>
-                </div>
-              </div>
+          {contactSuccess && <div>
+            <div className="pb-5 text-lg font-bold">
+              Submitted
             </div>
-          </Popup>
+            <div className="pb-5 pt-1">
+              Thank you, your form has been submitted successfully. <br />
+              We will be in contact as soon as possible.In the meantime, check out our other services and meet our amazing team.
+            </div>
+            <div className="text-end">
+              <button
+                className="bg-sswRed px-3 py-1.5 text-white"
+                onClick={() => onClose((curr) => !curr)}
+              >
+                OK
+              </button>
+            </div>
+          </div>}
 
-          <Formik
-            validationSchema={schema}
-            initialValues={initialFormValues}
-            onSubmit={handleOnSubmit}
-          >
-            {({ values, isSubmitting }) => (
-              <Form noValidate>
-                <FormGroupInput
-                  label={ACTIVE_INPUT.FullName}
-                  type="text"
-                  {...getCommonFieldProps(FORM_INPUT.FullName)}
-                />
+          {!contactSuccess && <div>
+            <h2 className="mb-14 mt-1.5 pt-1.5 !text-2xl text-sswRed">
+              {CONTACT_FORM_TITLE}
+            </h2>
+            <Formik
+              validationSchema={schema}
+              initialValues={initialFormValues}
+              onSubmit={handleOnSubmit}
+            >
+              {({ values, isSubmitting }) => (
+                <Form noValidate>
+                  <FormGroupInput
+                    label={ACTIVE_INPUT.FullName}
+                    type="text"
+                    {...getCommonFieldProps(FORM_INPUT.FullName)}
+                  />
 
-                <FormGroupInput
-                  label={ACTIVE_INPUT.Email}
-                  type="email"
-                  {...getCommonFieldProps(FORM_INPUT.Email)}
-                />
+                  <FormGroupInput
+                    label={ACTIVE_INPUT.Email}
+                    type="email"
+                    {...getCommonFieldProps(FORM_INPUT.Email)}
+                  />
 
-                <FormGroupInput
-                  label={ACTIVE_INPUT.Phone}
-                  type="phone"
-                  {...getCommonFieldProps(FORM_INPUT.Phone)}
-                />
+                  <FormGroupInput
+                    label={ACTIVE_INPUT.Phone}
+                    type="phone"
+                    {...getCommonFieldProps(FORM_INPUT.Phone)}
+                  />
 
-                <FormGroupSelect
-                  label={ACTIVE_INPUT.Location}
-                  {...getCommonFieldProps(locationDefaultOption.name)}
-                  handleChange={(field, e) => {
-                    setCountry(e.currentTarget.value);
-                    handleActiveInputLabel(field.name, e.currentTarget.value);
-                  }}
-                >
-                  <option className="hidden" value="">
-                    {locationDefaultOption.value}
-                  </option>
-                  {FormCountriesList.map((country) => (
-                    <option
-                      className="cursor-pointer !p-1"
-                      key={country.value}
-                      value={country.value}
-                    >
-                      {country.label}
-                    </option>
-                  ))}
-                </FormGroupSelect>
-
-                {isShowStates ? (
                   <FormGroupSelect
-                    label={ACTIVE_INPUT.States}
-                    {...getCommonFieldProps(statesDefaultOption.name)}
+                    label={ACTIVE_INPUT.Location}
+                    {...getCommonFieldProps(locationDefaultOption.name)}
+                    handleChange={(field, e) => {
+                      setCountry(e.currentTarget.value);
+                      handleActiveInputLabel(field.name, e.currentTarget.value);
+                    }}
                   >
                     <option className="hidden" value="">
-                      {statesDefaultOption.value}
+                      {locationDefaultOption.value}
                     </option>
-                    {AustralianStatesList.map((state) => (
+                    {FormCountriesList.map((country) => (
                       <option
-                        key={state.value}
-                        value={state.value}
                         className="cursor-pointer !p-1"
+                        key={country.value}
+                        value={country.value}
                       >
-                        {state.label}
+                        {country.label}
                       </option>
                     ))}
                   </FormGroupSelect>
-                ) : (
-                  <input
-                    type="hidden"
-                    name={statesDefaultOption.name}
-                    value={(values.states = STATE_DEFAULT_VALUE)}
+
+                  {isShowStates ? (
+                    <FormGroupSelect
+                      label={ACTIVE_INPUT.States}
+                      {...getCommonFieldProps(statesDefaultOption.name)}
+                    >
+                      <option className="hidden" value="">
+                        {statesDefaultOption.value}
+                      </option>
+                      {AustralianStatesList.map((state) => (
+                        <option
+                          key={state.value}
+                          value={state.value}
+                          className="cursor-pointer !p-1"
+                        >
+                          {state.label}
+                        </option>
+                      ))}
+                    </FormGroupSelect>
+                  ) : (
+                    <input
+                      type="hidden"
+                      name={statesDefaultOption.name}
+                      value={(values.states = STATE_DEFAULT_VALUE)}
+                    />
+                  )}
+
+                  <FormGroupInput
+                    label={ACTIVE_INPUT.Company}
+                    {...getCommonFieldProps(FORM_INPUT.Company)}
                   />
-                )}
 
-                <FormGroupInput
-                  label={ACTIVE_INPUT.Company}
-                  {...getCommonFieldProps(FORM_INPUT.Company)}
-                />
+                  <FormGroupTextArea
+                    label={ACTIVE_INPUT.Note}
+                    placeholder="How can we help you?"
+                    rows={4}
+                    maxLength={2000}
+                    {...getCommonFieldProps(FORM_INPUT.Note)}
+                  />
 
-                <FormGroupTextArea
-                  label={ACTIVE_INPUT.Note}
-                  placeholder="How can we help you?"
-                  rows={4}
-                  maxLength={2000}
-                  {...getCommonFieldProps(FORM_INPUT.Note)}
-                />
-
-                <div className="mb-4 w-full overflow-x-auto">
-                  <div className="h-22 w-88">
-                    {recaptchaKey && (
-                      <ReCAPTCHA
-                        sitekey={recaptchaKey}
-                        onChange={(value) => {
-                          setContactReCaptcha(value);
-                        }}
-                      />
+                  <div className="mb-4 w-full overflow-x-auto">
+                    <div className="h-22 w-88">
+                      {recaptchaKey && (
+                        <ReCAPTCHA
+                          sitekey={recaptchaKey}
+                          onChange={(value) => {
+                            setContactReCaptcha(value);
+                          }}
+                        />
+                      )}
+                    </div>
+                    {invalidRecaptcha && (
+                      <span className="text-sm text-red-600">
+                        {invalidRecaptcha}
+                      </span>
                     )}
                   </div>
-                  {invalidRecaptcha && (
-                    <span className="text-sm text-red-600">
-                      {invalidRecaptcha}
-                    </span>
-                  )}
-                </div>
 
-                <button
-                  type="submit"
-                  disabled={isSubmitting}
-                  className="done px-3 py-1.5"
-                >
-                  SUBMIT
-                </button>
-              </Form>
-            )}
-          </Formik>
+                  <button
+                    type="submit"
+                    disabled={isSubmitting}
+                    className="done px-3 py-1.5"
+                  >
+                    SUBMIT
+                  </button>
+                </Form>
+              )}
+            </Formik>
+          </div>
+          }
         </div>
       </div>
     </div>
