9ec77c8ca092106cdcddc6839d8b3d352b1c011a
Harry Ross <hazross@hotmail.com>
2023-06-13 15:40:50 +1000

    Basic implementation of file upload in TinaCMS

--- .tina/collections/newsletters-copy.tsx
+++ .tina/collections/newsletters-copy.tsx
@@ -1,8 +1,6 @@
-import { wrapFieldsWithMeta, type Collection, useCMS, Schema, Field, FieldPlugin, TinaField, useForm } from "tinacms";
+import { wrapFieldsWithMeta, type Collection, useCMS, Schema, Field, FieldPlugin, TinaField, useForm, useCMSEvent, Input, Button } from "tinacms";
 import * as React from "react";
 import { FaPray } from "react-icons/fa";
-
-
 
 export const newsletterCopySchema: Collection = {
   label: "Newsletters Copy",
@@ -18,40 +16,61 @@ export const newsletterCopySchema: Collection = {
 			description: "How it do",
 			ui: {
 				parse: (val) => {
-					return String(val);
+					return String(val || "");
+				},
+				format: (val) => {
+					return String(val || "");
 				},
 				component: ({ input, meta, field }) => {
+
 					const cms = useCMS();
-					console.log(cms);
-					console.log(input);
 					
 					const fileUploadRef = React.useRef(null);
 
-					const handleSubmit = (event) => {
-						event?.preventDefault();
-							
-						const file = fileUploadRef.current.files[0];
-						console.log(file);
-						cms.media.persist([{ directory: "newsletters", file: file }])
-						cms.media.store.list({ directory: "newsletters" }).then((res) => {
+					const [fileName, setFileName] = React.useState<string>(input.value);
 
+					React.useEffect(() => {
+						// @ts-ignore
+						input.onChange(fileName);
+					}, [fileName])
+
+					// @ts-ignore
+					input.onChange(fileName);
+
+					const handleSubmit = async () => {
+							
+						const file = fileUploadRef?.current?.files[0];
+						if (!file) return;
+						
+						setFileName(file.name);
+						console.log(file);
+						const [media] = await cms.media.persist([{ directory: "newsletters", file: file }])
+						cms.media.store.list({ directory: "newsletters" }).then((res) => {
 							console.log(res);
 						})
 					}
 
 					return (
 						<div>
-							<form onSubmit={handleSubmit}> 
-								<input
-									id="saturation"
-									type="text"
-									// This will pass along props.input.onChange to set our form values as this input changes.
-									{...input}
-								/>
-								<br />
-								<input type="file" ref={fileUploadRef} />
-								<button type="submit">Submit</button>
-							</form>
+							<Input type="file" ref={fileUploadRef} onChange={(e) => {
+								setFileName(e.target.files[0].name);
+							}} />
+							<Button type="submit" onClick={() => handleSubmit().then(() => console.log("success"))}>Upload File</Button>
+							{/* 
+							// Component that made Tina display the component, not needed anymore
+							<Input
+								id="saturation"
+								type="text"
+								className="hidden" 
+								// This will pass along props.input.onChange to set our form values as this input changes.
+								{...input}
+								onChange={(e) => { 
+									setFileName(e.target.value); 
+									// @ts-ignore
+									input.onChange(fileName); 
+								}}
+							/> */}
+							
 						</div>
 					)
 				}
