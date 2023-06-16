77aa4342ba75a1022e7d65aee01a8ecba16fbb0f
Harry Ross <hazross@hotmail.com>
2023-06-16 15:27:11 +1000

    Removed useless newsletter API routes

--- pages/api/newsletters/get-newsletters.ts
+++
@@ -1,6 +0,0 @@
-import path from "path";
-import { promises as fs } from "fs";
-
-export default async function handler(req, res) {
-  const newslettersDirectory = path.join(process.cwd(), "public/newsletters");
-}
