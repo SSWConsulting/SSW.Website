170b2220c97e229c039c9028d4e826a06cf60fc7
Harry Ross <hazross@hotmail.com>
2023-06-23 09:29:39 +1000

    Working token request system with correct permissions

--- pages/api/get-upcoming-events.ts
+++ pages/api/get-upcoming-events.ts
@@ -1,7 +1,7 @@
+import * as msal from "@azure/msal-node";
+import { BearerToken } from "@pnp/queryable";
+import { spfi } from "@pnp/sp";
 import { NextApiRequest, NextApiResponse } from "next";
-import { EventInfo, getUpcomingEvents } from "../../services";
-import { SPDefault } from "@pnp/nodejs";
-import { sp } from "@pnp/sp/presets/all"; 
 
 export default async function handler(
   req: NextApiRequest,
@@ -14,48 +14,75 @@ export default async function handler(
     //   res.status(401).json({ message: "Unsupported query param" });
     // }
 
-    // const upcomingEventsRes: { status: number, data: EventInfo[] } = await getUpcomingEvents(
-    //   <string>datetimeParam,
-    //   topCountParam
-    // );
-
     // const odataFilter = `$filter=Enabled ne false \
     //   and EndDateTime gt datetime'${datetimeParam}'\
     //   &$orderby=StartDateTime asc\
     //   &$top=${topCountParam}`;
 
-    // const siteUrl = "https://sswcom.sharepoint.com/";
-    
-    // const eventsListRes = await fetch(`${siteUrl}sites/Events/_api/web/lists/getbytitle('Events')/items`, {
-    //   headers: {
-    //     Authorization: "Bearer " + process.env.SPO_ACCESS_TOKEN,
-    //     Accept: "application/json;odata=nometadata",
+    // const upcomingEventsRes: { status: number, data: EventInfo[] } = await getUpcomingEvents(
+    //   <string>datetimeParam,
+    //   topCountParam
+    // );
+
+    // const authRes = await fetch(
+    //   `https://login.microsoft.com/${process.env.MICROSOFT_OAUTH_TENANT_ID}/oauth2/v2.0/token`,
+    //   {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/x-www-form-urlencoded",
+    //     },
+    //     body: new URLSearchParams({
+    //       client_id: process.env.MICROSOFT_OAUTH_CLIENT_ID,
+    //       scope: "https://sswcom.sharepoint.com/Sites.Read.All",
+    //       client_secret: process.env.MICROSOFT_OAUTH_CLIENT_SECRET,
+    //       grant_type: "client_credentials",
+    //     }),
     //   }
-    // });
-    // const eventsList = await eventsListRes.json();
-    // console.log(eventsList);
-    
+    // );
 
-    // res.status(upcomingEventsRes.status).json(upcomingEventsRes.data);
-
-    fetch(`https://login.microsoft.com/${process.env.MICROSOFT_OAUTH_TENANT_ID}/oauth2/v2.0/token`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+    const clientConfig = {
+      auth: {
+        clientId: process.env.MICROSOFT_OAUTH_CLIENT_ID,
+        authority: `https://login.microsoftonline.com/${process.env.MICROSOFT_OAUTH_TENANT_ID}`,
+        clientSecret: process.env.MICROSOFT_OAUTH_CLIENT_SECRET,
       },
-      body: new URLSearchParams({
-        'client_id': process.env.MICROSOFT_OAUTH_CLIENT_ID,
-        'scope': 'https://graph.microsoft.com/.default',
-        'client_secret': process.env.MICROSOFT_OAUTH_CLIENT_SECRET,
-        'grant_type': 'client_credentials'
-      })
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error)); 
+    };
 
-    
-    res.status(200).json({});
+    const clientApp = new msal.ConfidentialClientApplication(clientConfig);
+
+    try {
+      const authParams = {
+        scopes: ["https://sswcom.sharepoint.com/.default"],
+      };
+
+      const authRes = await clientApp.acquireTokenByClientCredential(
+        authParams
+      );
+
+      const sp = await spfi("https://sswcom.sharepoint.com/sites/Events").using(
+        BearerToken(authRes.accessToken)
+      );
+
+      //const eventsList = await sp.get().lists.getByTitle("Events").items.get();
+      console.log(authRes);
+
+      // const eventsListRes = await fetch(
+      //   "https://sswcom.sharepoint.com/sites/Events/_api/web/lists/getbytitle('Events')/items",
+      //   {
+      //     headers: {
+      //       Authorization: "Bearer " + "authRes.accessToken",
+      //       Accept: "application/json;odata=nometadata",
+      //     },
+      //   }
+      // );
+
+      // console.log(await eventsListRes.json());
+      // const eventsList = await eventsListRes.json();
+      res.status(200).send(authRes);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: true });
+    }
   } else {
     res.status(405).json({ message: "Unsupported method" });
   }
