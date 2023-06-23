437c6a04c8712b66678a15dd37e36df987f6c1b9
Harry Ross <hazross@hotmail.com>
2023-06-23 15:15:32 +1000

    Removed archived code

--- pages/api/get-upcoming-events.ts
+++ pages/api/get-upcoming-events.ts
@@ -12,20 +12,10 @@ export default async function handler(
     //   res.status(401).json({ message: "Unsupported query param" });
     // }
 
-    // const odataFilter = `$filter=Enabled ne false \
-    //   and EndDateTime gt datetime'${datetimeParam}'\
-    //   &$orderby=StartDateTime asc\
-    //   &$top=${topCountParam}`;
-
     const odataFilter =
       "$filter=fields/Enabled ne false\
       &$orderby=fields/StartDateTime desc\
       &$top=10";
-
-    // const upcomingEventsRes: { status: number, data: EventInfo[] } = await getUpcomingEvents(
-    //   <string>datetimeParam,
-    //   topCountParam
-    // );
 
     const siteId =
       "sswcom.sharepoint.com,8b375f80-d2e4-42a5-9ed3-54a3cfeb61b5,732990a3-6822-4895-b68a-3653da9f5910";
@@ -43,42 +33,12 @@ export default async function handler(
       }
     );
 
-    const eventBody = await eventsRes.json();
+    const eventsBody = await eventsRes.json();
+    const events = eventsBody.value.map((item) => {
+      return item.fields;
+    });
 
-    res.status(200).send(eventBody);
-
-    // try {
-    //   const authParams = {
-    //     scopes: ["https://sswcom.sharepoint.com/.default"],
-    //   };
-
-    //   const authRes = await clientApp.acquireTokenByClientCredential(
-    //     authParams
-    //   );
-
-    //   const sp = await spfi("https://sswcom.sharepoint.com/sites/Events").using(
-    //     BearerToken(authRes.accessToken)
-    //   );
-
-    //   //const eventsList = await sp.get().lists.getByTitle("Events").items.get();
-
-    //   // const eventsListRes = await fetch(
-    //   //   "https://sswcom.sharepoint.com/sites/Events/_api/web/lists/getbytitle('Events')/items",
-    //   //   {
-    //   //     headers: {
-    //   //       Authorization: "Bearer " + "authRes.accessToken",
-    //   //       Accept: "application/json;odata=nometadata",
-    //   //     },
-    //   //   }
-    //   // );
-
-    //   // console.log(await eventsListRes.json());
-    //   // const eventsList = await eventsListRes.json();
-    //   res.status(200).send(authRes);
-    // } catch (error) {
-    //   console.error(error);
-    //   res.status(500).json({ error: true });
-    // }
+    res.status(200).send(events);
   } else {
     res.status(405).json({ message: "Unsupported method" });
   }
