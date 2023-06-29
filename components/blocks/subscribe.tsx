0eab6ed5bb04768df7dac7147b6852768579249e
Kristen <kristenhu@ssw.com.au>
2023-06-29 15:09:14 +0800

    💄Add mix blend mode to title text

--- components/blocks/subscribe.tsx
+++ components/blocks/subscribe.tsx
@@ -9,9 +9,9 @@ export const Subscribe = () => {
         "flex h-80 w-full items-center bg-subscribeBackground bg-cover bg-center bg-no-repeat text-center font-light after:absolute"
       }
     >
-      <Container padding="px-4" className="w-full z-content">
+      <Container padding="px-4" className="w-full">
         <SubNewsLetters
-          headerText="Subscribe to the <span class='font-bold text-sswRed'>SSW Newsletter</span>"
+          headerText="<span class='mix-blend-difference mr-3'>Subscribe to the</span><span class='font-bold text-sswRed'>SSW Newsletter</span>"
           subscribeButtonText="Sign Up"
           subscribeSubTitle="Stay tuned for SSW News & upcoming events."
         />
