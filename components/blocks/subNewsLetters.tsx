164ea7175e889abcae9157ef1f590dade9534252
Kristen Hu [SSW] <52522913+KristenHu@users.noreply.github.com>
2023-06-30 14:50:28 +0800

    Newsletter page (#860)
    * 💄Reskin newsletter page
    
    * 🎨Fix eslint format
    
    * ⚡Remove useless variable
    
    * 💄Change color of subtitle in newsletter page
    
    * 💄Add mix blend mode to title text
    
    * 💄Update subscribe form style in liveStream page

--- components/blocks/subNewsLetters.tsx
+++ components/blocks/subNewsLetters.tsx
@@ -119,10 +119,10 @@ export const SubNewsLetters = ({
   return (
     <div className="flex flex-col items-center justify-center">
       <h2
-        className="mb-2 mt-6 text-center font-helvetica text-4xl font-medium text-sswBlack"
+        className="mb-2 mt-6 text-center font-helvetica text-4xl font-medium text-white"
         dangerouslySetInnerHTML={{ __html: headerText }}
       ></h2>
-      <div className="mb-5 text-center text-base text-sswBlack">
+      <div className="mb-5 text-center text-base text-white mix-blend-difference">
         {subscribeSubTitle}
       </div>
       <div className="container flex flex-wrap justify-center sm:w-full sm:max-w-full md:w-full">
