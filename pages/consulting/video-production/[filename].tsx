2d9201ae6d16400926e162b64858de9af9079438
Jack Reimers <jack.reimers.00@gmail.com>
2023-07-07 11:26:19 +1000

    Add page content

--- pages/consulting/video-production/[filename].tsx
+++ pages/consulting/video-production/[filename].tsx
@@ -1,9 +1,22 @@
 import { InferGetStaticPropsType } from "next";
 
-import { useTina } from "tinacms/dist/react";
+import { tinaField, useTina } from "tinacms/dist/react";
+import { TinaMarkdown } from "tinacms/dist/rich-text";
 import { client } from "../../../.tina/__generated__/client";
 
+import { ReactElement } from "react";
+import { BuiltOnAzure } from "../../../components/blocks";
+import { Blocks } from "../../../components/blocks-renderer";
+import { Booking } from "../../../components/blocks/booking";
+import { Breadcrumbs } from "../../../components/blocks/breadcrumbs";
+import { componentRenderer } from "../../../components/blocks/mdxComponentRenderer";
+import { BookingButton } from "../../../components/bookingButton/bookingButton";
 import { Layout } from "../../../components/layout";
+import { Container } from "../../../components/util/container";
+import { Section } from "../../../components/util/section";
+import { removeExtension } from "../../../services/client/utils.service";
+
+import ReactDOMServer from "react-dom/server";
 
 export default function VideoProductionPage(
   props: InferGetStaticPropsType<typeof getStaticProps>
@@ -14,34 +27,130 @@ export default function VideoProductionPage(
     variables: props.variables,
   });
 
+  const bookingButtonProps = {
+    buttonText: data.global.bookingButtonText,
+  };
+
   return (
     <Layout>
-      <h1>TEST</h1>
+      <Section className="mx-auto w-full max-w-9xl px-8 py-5">
+        <Breadcrumbs
+          path={removeExtension(props.variables.relativePath)}
+          suffix={data.global.breadcrumbSuffix}
+          title={data.videoProduction.seo?.title}
+          seoSchema={data.videoProduction.seo}
+        />
+      </Section>
+
+      <Section className="w-full" color="black">
+        <Booking {...data.videoProduction.booking}>
+          <BookingButton {...bookingButtonProps} />
+        </Booking>
+      </Section>
+
+      <Section
+        className={`
+            prose-consulting
+            border-y-4 border-y-sswRed
+            text-center`}
+      >
+        <a id="more" />
+        <div className="w-full py-12">
+          <div
+            data-tina-field={tinaField(data.videoProduction, "_body")}
+            className="mx-auto max-w-9xl px-4"
+          >
+            <TinaMarkdown
+              components={componentRenderer}
+              content={data.videoProduction._body}
+            />
+          </div>
+        </div>
+      </Section>
+
+      <Section className="!bg-gray-75 pb-25 text-center">
+        <Container size="custom" className="w-full">
+          <h1
+            data-tina-field={tinaField(data.videoProduction, "callToAction")}
+            dangerouslySetInnerHTML={{
+              __html: parseCallToAction(
+                data.videoProduction.callToAction,
+                data.videoProduction.solution?.project,
+                data.videoProduction.solution
+              ),
+            }}
+          ></h1>
+          <p className="text-lg">
+            Jump on a call with one of our Account Managers to discuss how we
+            can help you.
+          </p>
+          <BookingButton {...bookingButtonProps} />
+        </Container>
+      </Section>
+
+      <Section className="mb-16">
+        <Container padding="px-4" className="flex w-full flex-wrap">
+          {data.videoProduction.afterBody ? (
+            <div>
+              <Blocks
+                prefix={"VideoProductionAfterBody"}
+                blocks={data.videoProduction.afterBody}
+              />
+            </div>
+          ) : (
+            <></>
+          )}
+        </Container>
+      </Section>
+
+      <Section>
+        <BuiltOnAzure data={{ backgroundColor: "default" }} />
+      </Section>
     </Layout>
   );
 }
+
+const parseCallToAction = (
+  content: string,
+  project: string,
+  data: { project?: string }
+) => {
+  const HTMLelement: ReactElement = (
+    <span
+      className="text-sswRed"
+      {...(data ? { "data-tina-field": tinaField(data, "project") } : {})}
+    >
+      {project}
+    </span>
+  );
+
+  return content?.replace(
+    "{{TITLE}}",
+    ReactDOMServer.renderToString(HTMLelement)
+  );
+};
 
 export const getStaticProps = async ({ params }) => {
   const tinaProps = await client.queries.videoProductionContentQuery({
     relativePath: `${params.filename}.mdx`,
   });
 
-  //const canonical = `${tinaProps.data.global.header.url}consulting/videoproduction/${params.filename}`;
-  //const seo = tinaProps.data.videoProduction.seo;
-  //if (seo) {
-  //  seo.canonical = canonical;
-  //}
+  const canonical = `${tinaProps.data.global.header.url}consulting/video-production/${params.filename}`;
+  const seo = tinaProps.data.videoProduction.seo;
+  if (seo) {
+    seo.canonical = canonical;
+  }
 
   return {
     props: {
       data: tinaProps.data,
       query: tinaProps.query,
       variables: tinaProps.variables,
-      //env: {
-      //  GOOGLE_RECAPTCHA_SITE_KEY:
-      //    process.env.GOOGLE_RECAPTCHA_SITE_KEY || null,
-      //},
-      //seo,
+      env: {
+        GOOGLE_RECAPTCHA_SITE_KEY:
+          process.env.GOOGLE_RECAPTCHA_SITE_KEY || null,
+      },
+      seo,
     },
   };
 };
