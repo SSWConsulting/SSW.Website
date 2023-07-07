208c1b4457d1cbbad5ad5c265f4652fb439cc016
Jack Reimers <jack.reimers.00@gmail.com>
2023-07-07 15:08:57 +1000

    Implement review changes

--- pages/consulting/video-production/[filename].tsx
+++ pages/consulting/video-production/[filename].tsx
@@ -14,6 +14,8 @@ import { BookingButton } from "../../../components/bookingButton/bookingButton";
 import { Layout } from "../../../components/layout";
 import { Container } from "../../../components/util/container";
 import { Section } from "../../../components/util/section";
+import { SEO } from "../../../components/util/seo";
+import { RecaptchaContext } from "../../../context/RecaptchaContext";
 import { removeExtension } from "../../../services/client/utils.service";
 
 import ReactDOMServer from "react-dom/server";
@@ -32,81 +34,86 @@ export default function VideoProductionPage(
   };
 
   return (
-    <Layout>
-      <Section className="mx-auto w-full max-w-9xl px-8 py-5">
-        <Breadcrumbs
-          path={removeExtension(props.variables.relativePath)}
-          suffix={data.global.breadcrumbSuffix}
-          title={data.videoProduction.seo?.title}
-          seoSchema={data.videoProduction.seo}
-        />
-      </Section>
+    <RecaptchaContext.Provider
+      value={{ recaptchaKey: props.env.GOOGLE_RECAPTCHA_SITE_KEY }}
+    >
+      <SEO seo={props.seo} />
+      <Layout>
+        <Section className="mx-auto w-full max-w-9xl px-8 py-5">
+          <Breadcrumbs
+            path={removeExtension(props.variables.relativePath)}
+            suffix={data.global.breadcrumbSuffix}
+            title={data.videoProduction.seo?.title}
+            seoSchema={data.videoProduction.seo}
+          />
+        </Section>
 
-      <Section className="w-full" color="black">
-        <Booking {...data.videoProduction.booking}>
-          <BookingButton {...bookingButtonProps} />
-        </Booking>
-      </Section>
+        <Section className="w-full" color="black">
+          <Booking {...data.videoProduction.booking}>
+            <BookingButton {...bookingButtonProps} />
+          </Booking>
+        </Section>
 
-      <Section
-        className={`
+        <Section
+          className={`
             prose-consulting
             border-y-4 border-y-sswRed
             text-center`}
-      >
-        <a id="more" />
-        <div className="w-full py-12">
-          <div
-            data-tina-field={tinaField(data.videoProduction, "_body")}
-            className="mx-auto max-w-9xl px-4"
-          >
-            <TinaMarkdown
-              components={componentRenderer}
-              content={data.videoProduction._body}
-            />
-          </div>
-        </div>
-      </Section>
-
-      <Section className="!bg-gray-75 pb-25 text-center">
-        <Container size="custom" className="w-full">
-          <h1
-            data-tina-field={tinaField(data.videoProduction, "callToAction")}
-            dangerouslySetInnerHTML={{
-              __html: parseCallToAction(
-                data.videoProduction.callToAction,
-                data.videoProduction.solution?.project,
-                data.videoProduction.solution
-              ),
-            }}
-          ></h1>
-          <p className="text-lg">
-            Jump on a call with one of our Account Managers to discuss how we
-            can help you.
-          </p>
-          <BookingButton {...bookingButtonProps} />
-        </Container>
-      </Section>
-
-      <Section className="mb-16">
-        <Container padding="px-4" className="flex w-full flex-wrap">
-          {data.videoProduction.afterBody ? (
-            <div>
-              <Blocks
-                prefix={"VideoProductionAfterBody"}
-                blocks={data.videoProduction.afterBody}
+        >
+          <a id="more" />
+          <div className="w-full py-12">
+            <div
+              data-tina-field={tinaField(data.videoProduction, "_body")}
+              className="mx-auto max-w-9xl px-4"
+            >
+              <TinaMarkdown
+                components={componentRenderer}
+                content={data.videoProduction._body}
               />
             </div>
-          ) : (
-            <></>
-          )}
-        </Container>
-      </Section>
+          </div>
+        </Section>
 
-      <Section>
-        <BuiltOnAzure data={{ backgroundColor: "default" }} />
-      </Section>
-    </Layout>
+        <Section className="!bg-gray-75 pb-25 text-center">
+          <Container size="custom" className="w-full">
+            <h1
+              data-tina-field={tinaField(data.videoProduction, "callToAction")}
+              dangerouslySetInnerHTML={{
+                __html: parseCallToAction(
+                  data.videoProduction.callToAction,
+                  data.videoProduction.solution?.project,
+                  data.videoProduction.solution
+                ),
+              }}
+            ></h1>
+            <p className="text-lg">
+              Jump on a call with one of our Account Managers to discuss how we
+              can help you.
+            </p>
+            <BookingButton {...bookingButtonProps} />
+          </Container>
+        </Section>
+
+        <Section className="mb-16">
+          <Container padding="px-4" className="flex w-full flex-wrap">
+            {data.videoProduction.afterBody ? (
+              <div>
+                <Blocks
+                  prefix={"VideoProductionAfterBody"}
+                  blocks={data.videoProduction.afterBody}
+                />
+              </div>
+            ) : (
+              <></>
+            )}
+          </Container>
+        </Section>
+
+        <Section>
+          <BuiltOnAzure data={{ backgroundColor: "default" }} />
+        </Section>
+      </Layout>
+    </RecaptchaContext.Provider>
   );
 }
 
