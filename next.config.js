0a0adfa402ad322d7e3b57e5b41d7a814e41a0f5
Harry Ross <hazross@hotmail.com>
2023-07-07 17:33:21 +1000

    Added bundle analyser

--- next.config.js
+++ next.config.js
@@ -1,38 +1,44 @@
-module.exports = {
-	i18n: {
-		locales: ["en"],
-		defaultLocale: "en",
-	},
-	poweredByHeader: false,
-	images: {
-		domains: ["assets.tina.io", "i.ytimg.com", "ssw.com.au", "www.ssw.com.au"],
-	},
-	output: "standalone", // required for Docker support
-	swcMinify: false,
-	webpack(config) {
-		config.module.rules.push({
-			test: /\.svg$/i,
-			issuer: /\.[jt]sx?$/,
-			use: ["@svgr/webpack"],
-		});
+const config = {
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  poweredByHeader: false,
+  images: {
+    domains: ["assets.tina.io", "i.ytimg.com", "ssw.com.au", "www.ssw.com.au"],
+  },
+  output: "standalone", // required for Docker support
+  swcMinify: false,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
 
-		return config;
-	},
-	async rewrites() {
-		return [
-			{
-				source: "/",
-				destination: "/home",
-			},
-		];
-	},
-	async redirects() {
-		return [
-			{
-				source: "/admin",
-				destination: "/admin/index.html",
-				permanent: true,
-			},
-		];
-	},
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+      },
+    ];
+  },
+  async redirects() {
+    return [
+      {
+        source: "/admin",
+        destination: "/admin/index.html",
+        permanent: true,
+      },
+    ];
+  },
 };
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: true,
+});
+
+module.exports = withBundleAnalyzer(config);
