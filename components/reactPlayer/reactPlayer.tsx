62646d4a8be86a4d54188528ced743e704efb13b
Harry Ross <hazross@hotmail.com>
2023-07-11 10:13:13 +1000

    Removed unused packages, changed React player to just youtube import

--- components/reactPlayer/reactPlayer.tsx
+++ components/reactPlayer/reactPlayer.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 
-const ReactPlayer = dynamic(() => import("react-player/lazy"), {
+const ReactPlayer = dynamic(() => import("react-player/youtube"), {
   ssr: false,
 });
 
