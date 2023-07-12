53870b1d01f7d7a6b67a9dbef1d074e700e37eab
Harry Ross <hazross@hotmail.com>
2023-07-12 11:36:26 +1000

    Added debug partytown

--- components/layout/analytics.tsx
+++ components/layout/analytics.tsx
@@ -1,17 +1,21 @@
-import Script from "next/script";
+import { Partytown } from "@builder.io/partytown/react";
+import Head from "next/head";
 
 const gtmId = process.env.NEXT_PUBLIC_GOOGLE_GTM_ID;
 
 export const Analytics = () => {
   return (
-    <Script id="google-tag-manager" strategy="worker">
-      {`
+    <Head>
+      <Partytown debug={true} />
+      <script id="google-tag-manager" type="text/partytown">
+        {`
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','${gtmId}');
       `}
-    </Script>
+      </script>
+    </Head>
   );
 };
