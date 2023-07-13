d70039a5e42e822c7bd8b6ac060c65d7c8a336a4
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2023-07-13 12:25:41 +1000

    Prettier - fixing formatting error

--- components/blocks/agenda.tsx
+++ components/blocks/agenda.tsx
@@ -7,31 +7,26 @@ import { verticalListItemSchema } from "../blocks/verticalListItem";
 import { Container } from "../util/container";
 import { Section } from "../util/section";
 
-
 const alignmentClasses = {
   center: "md:!text-center",
   right: "md:!text-right",
-    left: "md:!text-left",
+  left: "md:!text-left",
 };
 
 const textColorClasses = {
   red: "text-sswRed",
   grey: "text-gray-500",
-  default: ""
+  default: "",
 };
-
 
 export type AgendaItemProps = {
   body: TinaMarkdownContent[];
 };
 
-const AgendaItem: FC<AgendaItemProps> = ({
-  body,
-}) => {
+const AgendaItem: FC<AgendaItemProps> = ({ body }) => {
   return (
     <div className="flex flex-col text-center text-base lg:text-left">
-
-      <div className="w-full items-center lg:p-5 md:py-5 md:pl-4 text-left sm:w-3/4 lg:w-full">
+      <div className="w-full items-center text-left sm:w-3/4 md:py-5 md:pl-4 lg:w-full lg:p-5">
         <TinaMarkdown components={componentRenderer} content={body} />
       </div>
     </div>
@@ -46,33 +41,34 @@ export const Agenda = ({ data }) => {
         size={"xsmall"}
         className={"flex-1 pb-12"}
       >
-      <h3
-        className={classNames("my-0 py-5 text-3xl text-center md:text-left", alignmentClasses[data.align], textColorClasses[data.textColor])}
-        dangerouslySetInnerHTML={{ __html: data.header }}
-      ></h3>
-        <div className={classNames("grid grid-cols-1 justify-between px-4 md:px-0", getGridColumns(data?.agendaItemList.length))}>
-          {data?.agendaItemList?.map(
-            (item: AgendaItemProps, key: Key) => (
-              <AgendaItem
-                key={key}
-                body={item.body}
-              />
-            )
+        <h3
+          className={classNames(
+            "my-0 py-5 text-center text-3xl md:text-left",
+            alignmentClasses[data.align],
+            textColorClasses[data.textColor]
           )}
+          dangerouslySetInnerHTML={{ __html: data.header }}
+        ></h3>
+        <div
+          className={classNames(
+            "grid grid-cols-1 justify-between px-4 md:px-0",
+            getGridColumns(data?.agendaItemList.length)
+          )}
+        >
+          {data?.agendaItemList?.map((item: AgendaItemProps, key: Key) => (
+            <AgendaItem key={key} body={item.body} />
+          ))}
         </div>
       </Container>
     </Section>
   );
 };
 
-const getGridColumns = (length)=>{
-  if(length === 1)
-  return "lg:grid-cols-1"
-  else if(length === 2)
-  return "lg:grid-cols-2"
-  else
-  return "lg:grid-cols-3 md:grid-cols-2"
-}
+const getGridColumns = (length) => {
+  if (length === 1) return "lg:grid-cols-1";
+  else if (length === 2) return "lg:grid-cols-2";
+  else return "lg:grid-cols-3 md:grid-cols-2";
+};
 
 export const agendaSchema: Template = {
   label: "Agenda",
@@ -107,9 +103,9 @@ export const agendaSchema: Template = {
       type: "object",
       label: "Agenda Items",
       name: "agendaItemList",
-      ui:{
+      ui: {
         itemProps(item) {
-          return {label:item?.placeholder}
+          return { label: item?.placeholder };
         },
       },
       list: true,
