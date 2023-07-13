d8292a2a187bf89b235adf5a1614c151e4b5ef4a
Harry Ross <hazross@hotmail.com>
2023-07-14 08:18:43 +1000

    Removed constants file, duplicated constants (required)

--- components/util/constants/partytown.ts
+++
@@ -1,8 +0,0 @@
-export const proxiedWhitelist = [
-  "connect.facebook.net",
-  "snap.licdn.com",
-  "cdn3l.ink",
-  "googleads.g.doubleclick.net",
-  "js.hs-analytics.net",
-  "www.google.com",
-];
--- pages/api/proxy.ts
+++ pages/api/proxy.ts
@@ -1,7 +1,15 @@
 import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 import { object, string } from "yup";
-import { proxiedWhitelist } from "../../components/util/constants/partytown";
+
+const proxiedWhitelist = [
+  "connect.facebook.net",
+  "snap.licdn.com",
+  "cdn3l.ink",
+  "googleads.g.doubleclick.net",
+  "js.hs-analytics.net",
+  "www.google.com",
+];
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
