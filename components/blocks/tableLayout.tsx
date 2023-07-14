0b7efb5a0023e3511de6afabd05152caa845fe49
John Xu <JohnXu@ssw.com.au>
2023-07-14 13:39:11 +0800

    Add presets on TableLayout Remove unused fields

--- components/blocks/tableLayout.tsx
+++ components/blocks/tableLayout.tsx
@@ -1,37 +1,46 @@
 import classNames from "classnames";
 import MarkdownIt from "markdown-it";
 import markdownItMultimdTable from "markdown-it-multimd-table";
+import path from "path";
 import { useEffect, useState } from "react";
 import type { Template } from "tinacms";
-
-const tableStyles = {
-  none: "",
-  basicBorder:
-    "descendant-table:border-1 descendant-table:border-solid descendant-table:p-2 descendant-th:border-1 descendant-th:border-solid descendant-th:p-2 descendant-td:border-1 descendant-td:border-solid descendant-td:p-2",
-  styled:
-    "descendant-th:border-b-sswRed [&>table>tbody>*:nth-child(even)]:bg-gray-75 descendant-th:bg-gray-75 descendant-th:border-b-sswRed descendant-table:w-full",
-};
+import client from "../../.tina/__generated__/client";
 
 const md = new MarkdownIt().use(markdownItMultimdTable, {
   multiline: true,
 });
 
 export const TableLayout = ({ data }) => {
+  const [tableClasses, setTableClasses] = useState([]);
   const [mdxTableString, setMdxTableString] = useState("");
 
+  const presetNames = data.tablePresets.map((preset) =>
+    path.basename(preset.preset, ".json")
+  );
+
   useEffect(() => {
+    async function init() {
+      const presets = await client.queries.presetsQuery({
+        categories: ["table"],
+        name: presetNames,
+      });
+      const presetClassNames = presets.data.presetsConnection.edges.map(
+        (p) => p.node.className
+      );
+      setTableClasses(presetClassNames);
+    }
+
+    init();
+
     const html = md.render(data.mdxTable ?? "");
     setMdxTableString(html);
   }, [data]);
-
-  console.log(data);
 
   return (
     <div
       className={classNames(
         "not-prose child-table:border-1 descendant-th:border-1 descendant-th:border-gray-75  descendant-th:py-2 descendant-th:pl-2 descendant-td:border-y-1 descendant-td:py-1.5 descendant-td:pl-2",
-        data.className,
-        tableStyles[data.tableStyle]
+        ...tableClasses
       )}
       dangerouslySetInnerHTML={{ __html: mdxTableString }}
     />
@@ -47,22 +56,6 @@ export const tableBlockSchema: Template = {
     },
   },
   fields: [
-    {
-      label: "Table Style",
-      name: "tableStyle",
-      type: "string",
-      options: Object.keys(tableStyles).map((key) => {
-        return {
-          label: key,
-          value: key,
-        };
-      }),
-    },
-    {
-      type: "string",
-      label: "CSS Class Name",
-      name: "className",
-    },
     {
       type: "object",
       label: "Table Presets",
