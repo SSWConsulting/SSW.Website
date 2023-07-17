34bc4e0c338fe674eedccf250c9c243a4863646b
Jack Reimers [SSW] <10693364+jackreimers@users.noreply.github.com>
2023-07-17 16:16:28 +1000

    Update favicon and associated files (#1035)
    * Update favicon and associated files
    
    * Delete mstile-150x150.png
    
    * Add files via upload
    
    * Add files via upload
    
    * Update layout.tsx
    
    ---------
    
    Co-authored-by: Brady Stroud [SSW] <bradystroud@ssw.com.au>

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -58,7 +58,28 @@ export const Layout = ({ children, className = "" }) => {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="icon" href="/favicon.ico"></link>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#cc4141" />
+        <meta name="msapplication-TileColor" content="#cc4141" />
+        <meta name="theme-color" content="#ffffff" />
+
         {router.query.filename === "home" && (
           <script
             type="application/ld+json"
