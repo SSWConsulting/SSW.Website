6047c274e7ec41c1ca51a7dd2121e02dc30435cd
Harry Ross <hazross@hotmail.com>
2023-07-21 18:02:38 +1000

    Fixed incorrect livestream parameter

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,7 +5,7 @@ import { Container } from "../components/util/container";
 
 export default function FourOhFour() {
   return (
-    <Layout event={props.liveStreamEvent}>
+    <Layout>
       <Container
         width="large"
         size="custom"
