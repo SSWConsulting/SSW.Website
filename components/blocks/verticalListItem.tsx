158a74696bcece883e0dc7682d53e4f99bef78d1
John Xu <JohnXu@ssw.com.au>
2023-07-24 13:36:50 +0800

    Add numbers as icon in `VerticalListItem`

--- components/blocks/verticalListItem.tsx
+++ components/blocks/verticalListItem.tsx
@@ -1,3 +1,4 @@
+import { isNumber } from "lodash";
 import Image from "next/image";
 import { Template, classNames } from "tinacms";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
@@ -12,8 +13,40 @@ export const VerticalListItem = ({ data }) => {
           <h3 className="text-sswRed">{data.header}</h3>
         </div>
       )}
-      <div className={classNames("flex flex-row items-center")}>
-        {data.icon && (
+      <div className={classNames("flex flex-row items-center font-helvetica")}>
+        {data.icon && isNumber(data.icon) ? (
+          <svg
+            width="45"
+            height="45"
+            viewBox="0 0 42 42"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="mr-5 text-sswRed"
+          >
+            <g>
+              <circle
+                cx="21"
+                cy="21"
+                r="21"
+                fill="currentColor"
+                fillOpacity="0.1"
+              />
+              <text
+                x="50%"
+                y="50%"
+                height="19"
+                fontSize="20"
+                fontWeight="700"
+                textAnchor="middle"
+                fill="currentColor"
+                fontFamily="helvetica"
+                dominantBaseline="central"
+              >
+                {data.icon}
+              </text>
+            </g>
+          </svg>
+        ) : (
           <Image
             src={data.icon || ""}
             alt={`${data.title} icon`}
