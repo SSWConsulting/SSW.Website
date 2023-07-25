f5ad34cb2aae3d333a001a3768d104cd28900a4e
Harry Ross <hazross@hotmail.com>
2023-07-25 16:27:33 +1000

    Added additional checks to getEvents for missing env variables

--- services/server/events.ts
+++ services/server/events.ts
@@ -29,8 +29,9 @@ export const getToken = async () => {
 
 export const getEvents = async (odataFilter: string): Promise<EventInfo[]> => {
   if (
-    process.env.NODE_ENV === "development" &&
-    !process.env.MICROSOFT_OAUTH_TENANT_ID
+    !process.env.MICROSOFT_OAUTH_TENANT_ID ||
+    !process.env.MICROSOFT_OAUTH_TENANT_ID ||
+    !process.env.MICROSOFT_OAUTH_CLIENT_SECRET
   ) {
     console.warn(
       "⚠️ You are missing the SharePoint environment variables. Please see the .env.example file for the required variables."
