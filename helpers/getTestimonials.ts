f1379a729b930b5bad505ae3d8ead28b0588b5f2
John Xu <JohnXu@ssw.com.au>
2023-08-17 11:21:35 +0800

    Adds default testimonials only when there are no testimonials

--- helpers/getTestimonials.ts
+++ helpers/getTestimonials.ts
@@ -12,10 +12,8 @@ export const GetTestimonialsByCategories = async (
     (t) => t.node
   );
 
-  testimonialsResult = testimonialsResult.sort(() => 0.5 - Math.random());
-
-  // Adds general testimonials if not filled by testimonials with matching categories
-  if (testimonialsResult.length < 3) {
+  // Adds default/general testimonials if not filled by any testimonial with matching categories
+  if (testimonialsResult.length === 0) {
     const generalTestimonials = await client.queries.testimonalsQuery({
       categories: defaultCategory ?? "General",
     });
@@ -23,13 +21,12 @@ export const GetTestimonialsByCategories = async (
     const generalTestimonialsResult =
       generalTestimonials.data.testimonialsConnection.edges.map((t) => t.node);
 
-    const randomGeneral = generalTestimonialsResult.sort(
-      () => 0.5 - Math.random()
-    );
-    testimonialsResult.push(...randomGeneral);
+    testimonialsResult = generalTestimonialsResult;
   }
 
-  testimonialsResult = testimonialsResult.slice(0, 3);
+  testimonialsResult = testimonialsResult
+    .sort(() => 0.5 - Math.random())
+    .slice(0, 3);
 
   return testimonialsResult;
 };
