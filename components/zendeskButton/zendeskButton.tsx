79fcd73d04234eeff74feb37c7be276c662916e3
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2023-08-23 17:43:28 +1000

    Adding async with the script to load it

--- components/zendeskButton/zendeskButton.tsx
+++ components/zendeskButton/zendeskButton.tsx
@@ -41,9 +41,9 @@ const ZendeskButton = () => {
       <>
         {open && (
           <Script
+            async
             src="https://www.chatbase.co/embed.min.js"
             id="8ZT43FdIerlr8zuOP7D5s"
-            defer
           />
         )}
       </>
