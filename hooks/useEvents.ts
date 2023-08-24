8d1b58893496e51df456a94607dc9e48cf7bf0ef
Harry Ross <hazross@hotmail.com>
2023-08-24 11:56:49 +1000

    Removed unused endpoint, refactored to use object instead of arbitary array

--- hooks/useEvents.ts
+++ hooks/useEvents.ts
@@ -1,9 +1,13 @@
 import { useMemo, useState } from "react";
+import { NO_SELECTION } from "../components/filter/FilterBlock";
 import { FilterGroupProps } from "../components/filter/FilterGroup";
 import { EventInfo } from "../services/server/events";
 
 export const useEvents = (events: EventInfo[]) => {
-  const [filterControls, setFilterControls] = useState<number[]>([-1, -1]);
+  const [filterControls, setFilterControls] = useState<{
+    technology: number;
+    format: number;
+  }>({ technology: NO_SELECTION, format: NO_SELECTION });
 
   const options = useMemo(() => {
     const categories =
@@ -26,14 +30,16 @@ export const useEvents = (events: EventInfo[]) => {
 
     const groups: FilterGroupProps[] = [
       {
-        selected: filterControls[0],
-        setSelected: (value) => setFilterControls((curr) => [value, curr[1]]),
+        selected: filterControls.technology,
+        setSelected: (value) =>
+          setFilterControls((curr) => ({ ...curr, technology: value })),
         options: options.categories,
         allText: "All Technology",
       },
       {
-        selected: filterControls[1],
-        setSelected: (value) => setFilterControls((curr) => [curr[0], value]),
+        selected: filterControls.format,
+        setSelected: (value) =>
+          setFilterControls((curr) => ({ ...curr, format: value })),
         options: options.formats,
         allText: "All Formats",
       },
@@ -45,10 +51,10 @@ export const useEvents = (events: EventInfo[]) => {
   const filteredEvents = useMemo(() => {
     return events?.filter(
       (event) =>
-        (filterControls[0] === -1 ||
+        (filterControls[0] === NO_SELECTION ||
           event.Category_f5a9cf4c_x002d_8228_x00 ===
             options.categories[filterControls[0]]) &&
-        (filterControls[1] === -1 ||
+        (filterControls[1] === NO_SELECTION ||
           event.CalendarType === options.formats[filterControls[1]])
     );
   }, [events, filterControls]);
