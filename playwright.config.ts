72d26cd84edf5244dd861c14f00869aff211c7e3
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2023-09-27 14:40:51 +1000

    disabling prettier log error

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,7 @@ import { defineConfig, devices } from "@playwright/test";
  * https://github.com/motdotla/dotenv
  */
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 require("dotenv").config();
 
 /**
