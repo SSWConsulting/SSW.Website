238d36db0d8cf234a3e9d82ba9c798f88f5c338f
Harry Ross <hazross@hotmail.com>
2023-09-28 10:30:59 +1000

    Changed Power Automate classes to exported methods

--- services/server/power-automate-flow.ts
+++ services/server/power-automate-flow.ts
@@ -7,36 +7,34 @@ import {
   BookingFormSubmissionData,
 } from "./events";
 
-export class PA_FLOW {
-  static invokePowerAutomateFlow = async (
-    data: BookingFormSubmissionData | AddContactToNewslettersData,
-    flowURL: string
-  ) => {
-    try {
-      const response = await axios.post(flowURL, data, {
-        headers: { "Content-Type": "application/json" },
-      });
-      return response;
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        const axiosError = error as AxiosError;
-        throw new CustomError(
-          JSON.stringify(
-            axiosError.response ? axiosError.response.data : axiosError.cause
-          ),
-          axiosError.response
-            ? axiosError.response.status
-            : HttpStatusCode.NotFound,
-          JSON.stringify(data),
-          axiosError.response &&
-          axiosError.response.status === HttpStatusCode.Conflict
-            ? appInsight.Contracts.SeverityLevel.Information
-            : appInsight.Contracts.SeverityLevel.Critical,
-          STAGE.PA_FLOW_AXIOS
-        );
-      } else {
-        throw new Error(error);
-      }
+export const invokePowerAutomateFlow = async (
+  data: BookingFormSubmissionData | AddContactToNewslettersData,
+  flowURL: string
+) => {
+  try {
+    const response = await axios.post(flowURL, data, {
+      headers: { "Content-Type": "application/json" },
+    });
+    return response;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      const axiosError = error as AxiosError;
+      throw new CustomError(
+        JSON.stringify(
+          axiosError.response ? axiosError.response.data : axiosError.cause
+        ),
+        axiosError.response
+          ? axiosError.response.status
+          : HttpStatusCode.NotFound,
+        JSON.stringify(data),
+        axiosError.response &&
+        axiosError.response.status === HttpStatusCode.Conflict
+          ? appInsight.Contracts.SeverityLevel.Information
+          : appInsight.Contracts.SeverityLevel.Critical,
+        STAGE.PA_FLOW_AXIOS
+      );
+    } else {
+      throw new Error(error);
     }
-  };
-}
+  }
+};
