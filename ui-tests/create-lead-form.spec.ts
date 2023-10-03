b979af58b00e77c582e8590768a41f72e7fb99fa
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2023-10-04 09:24:49 +1100

    Updated filename to make it more descriptive

--- ui-tests/create-lead-form.spec.ts
+++ ui-tests/create-lead-form.spec.ts
@@ -1,5 +1,4 @@
 import { expect, test } from "@playwright/test";
-import { FORM_INPUT } from "../components/util/constants";
 
 const TEST_PAYLOAD = {
   Name: "🧪 Test",
@@ -15,6 +14,19 @@ const TEST_PAYLOAD = {
   ReferralSource: "14",
 };
 
+export const LEAD_FORM = {
+  FullName: "fullName",
+  Email: "email",
+  Phone: "phone",
+  Location: "location",
+  States: "states",
+  Note: "note",
+  Company: "company",
+  ReferredCompany: "referredCompany",
+  None: "",
+  ReferralSource: "referralSource",
+};
+
 const successToastId = "#success-toaster";
 const route = "/consulting/angular";
 
@@ -28,39 +40,39 @@ test("Submit Enquiry Form", async ({ page }) => {
     .click();
 
   await page
-    .locator(`input[name="${FORM_INPUT.FullName}"]`)
+    .locator(`input[name="${LEAD_FORM.FullName}"]`)
     .fill(TEST_PAYLOAD.Name);
 
   await page
-    .locator(`input[name="${FORM_INPUT.Email}"]`)
+    .locator(`input[name="${LEAD_FORM.Email}"]`)
     .fill(TEST_PAYLOAD.Email);
 
   await page
-    .locator(`input[name="${FORM_INPUT.Phone}"]`)
+    .locator(`input[name="${LEAD_FORM.Phone}"]`)
     .fill(TEST_PAYLOAD.Phone);
 
   await page
-    .locator(`select[name="${FORM_INPUT.Location}"]`)
+    .locator(`select[name="${LEAD_FORM.Location}"]`)
     .selectOption(TEST_PAYLOAD.Country);
 
   await page
-    .locator(`input[name="${FORM_INPUT.Company}"]`)
+    .locator(`input[name="${LEAD_FORM.Company}"]`)
     .fill(TEST_PAYLOAD.Company);
 
   await page
-    .locator(`input[name="${FORM_INPUT.Company}"]`)
+    .locator(`input[name="${LEAD_FORM.Company}"]`)
     .fill(TEST_PAYLOAD.Company);
 
   await page
-    .locator(`select[name="${FORM_INPUT.States}"]`)
+    .locator(`select[name="${LEAD_FORM.States}"]`)
     .selectOption(TEST_PAYLOAD.State);
 
   await page
-    .locator(`select[name="${FORM_INPUT.ReferralSource}"]`)
+    .locator(`select[name="${LEAD_FORM.ReferralSource}"]`)
     .selectOption(TEST_PAYLOAD.ReferralSource);
 
   await page
-    .locator(`textarea[name="${FORM_INPUT.Note}"]`)
+    .locator(`textarea[name="${LEAD_FORM.Note}"]`)
     .fill(TEST_PAYLOAD.Note);
 
   await page.locator("button[type='submit']").first().click();
