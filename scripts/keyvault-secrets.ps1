c2e6cd3db4800a2875b66e31cf7ccb7e26279047
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2023-10-05 11:33:38 +1100

    Removing loop to retrieve the secrets

--- scripts/keyvault-secrets.ps1
+++ scripts/keyvault-secrets.ps1
@@ -1,17 +1,29 @@
 
 param (
-    [string]$keyvault
+    [string]$KEY_VAULT
 )
 
-$secrets=("Google-Recaptcha-Site-KEY","MICROSOFT-OAUTH-TENANT-ID","MICROSOFT-OAUTH-CLIENT-ID","MICROSOFT-OAUTH-CLIENT-SECRET",
-"SHAREPOINT-SITE-ID","SHAREPOINT-EVENTS-LIST-ID","SHAREPOINT-EXTERNAL-PRESENTERS-LIST-ID")
-# Iterate over the secret names
-foreach ($secret in $secrets) {
-  # Retrieve the secret value from Azure Key Vault
-  $secret_value = $(az keyvault secret show --name $secret --vault-name $keyvault --query value -o tsv)
-  # Mask the secret value in the workflow logs
-  echo "::add-mask::$secret_value"
-  $secret_name = $secret.replace("-","_")
-  echo "$secret_value=$secret_value" >> $env:GITHUB_OUTPUT
-}
-Write-Host '✅ KV - Secrets retrieved'
+  $GoogleRecaptchaSiteKey = (az keyvault secret show --name Google-Recaptcha-Site-KEY --vault-name $KEY_VAULT --query value -o tsv)
+  $MICROSOFT_OAUTH_TENANT_ID = (az keyvault secret show --name MICROSOFT-OAUTH-TENANT-ID --vault-name $KEY_VAULT --query value -o tsv)
+  $MICROSOFT_OAUTH_CLIENT_ID = (az keyvault secret show --name MICROSOFT-OAUTH-CLIENT-ID --vault-name $KEY_VAULT --query value -o tsv)
+  $MICROSOFT_OAUTH_CLIENT_SECRET = (az keyvault secret show --name MICROSOFT-OAUTH-CLIENT-SECRET --vault-name $KEY_VAULT --query value -o tsv)
+  $SHAREPOINT_SITE_ID = (az keyvault secret show --name SHAREPOINT-SITE-ID --vault-name $KEY_VAULT --query value -o tsv)
+  $SHAREPOINT_EVENTS_LIST_ID = (az keyvault secret show --name SHAREPOINT-EVENTS-LIST-ID --vault-name $KEY_VAULT --query value -o tsv)
+  $SHAREPOINT_EXTERNAL_PRESENTERS_LIST_ID = (az keyvault secret show --name SHAREPOINT-EXTERNAL-PRESENTERS-LIST-ID --vault-name $KEY_VAULT --query value -o tsv)
+  # Masking the secrets
+  echo "::add-mask::$GoogleRecaptchaSiteKey"
+  echo "::add-mask::$MICROSOFT_OAUTH_TENANT_ID"
+  echo "::add-mask::$MICROSOFT_OAUTH_CLIENT_ID"
+  echo "::add-mask::$MICROSOFT_OAUTH_CLIENT_SECRET"
+  echo "::add-mask::$SHAREPOINT_SITE_ID"
+  echo "::add-mask::$SHAREPOINT_EVENTS_LIST_ID"
+  echo "::add-mask::$SHAREPOINT_EXTERNAL_PRESENTERS_LIST_ID"
+  # GITHUB_OUTPUT
+  echo "GoogleRecaptchaSiteKey=$GoogleRecaptchaSiteKey" >> $env:GITHUB_OUTPUT
+  echo "MICROSOFT_OAUTH_TENANT_ID=$MICROSOFT_OAUTH_TENANT_ID" >> $env:GITHUB_OUTPUT
+  echo "MICROSOFT_OAUTH_CLIENT_ID=$MICROSOFT_OAUTH_CLIENT_ID" >> $env:GITHUB_OUTPUT
+  echo "MICROSOFT_OAUTH_CLIENT_SECRET=$MICROSOFT_OAUTH_CLIENT_SECRET" >> $env:GITHUB_OUTPUT
+  echo "SHAREPOINT_SITE_ID=$SHAREPOINT_SITE_ID" >> $env:GITHUB_OUTPUT
+  echo "SHAREPOINT_EVENTS_LIST_ID=$SHAREPOINT_EVENTS_LIST_ID" >> $env:GITHUB_OUTPUT
+  echo "SHAREPOINT_EXTERNAL_PRESENTERS_LIST_ID=$SHAREPOINT_EXTERNAL_PRESENTERS_LIST_ID" >> $env:GITHUB_OUTPUT
+  Write-Host '✅ KV - Secret retrieved'
