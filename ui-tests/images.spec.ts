901d49c3745f6d9d0e5b9e8f974ee08448761062
Harry Ross <hazross@hotmail.com>
2023-10-06 13:03:16 +1100

    Added consulting page image test

--- ui-tests/images.spec.ts
+++ ui-tests/images.spec.ts
@@ -1,6 +1,6 @@
 import test, { expect } from "@playwright/test";
 
-test("All images loaded successfully", async ({ page }) => {
+test("Images load successfully on index", async ({ page }) => {
   await page.on("response", (response) => {
     if (response.request().resourceType() === "image") {
       expect(!(response.status() >= 400)).toBeTruthy();
@@ -11,4 +11,17 @@ test("All images loaded successfully", async ({ page }) => {
 
   await page.waitForSelector("[data-testid='clientLogos']");
   await page.getByTestId("clientLogos").scrollIntoViewIfNeeded();
+});
+
+test("Images load successfully on consulting page", async ({ page }) => {
+  await page.on("response", (response) => {
+    if (response.request().resourceType() === "image") {
+      expect(!(response.status() >= 400)).toBeTruthy();
+    }
+  });
+
+  await page.goto("/consulting/react", { waitUntil: "networkidle" });
+
+  await page.waitForSelector("[data-testid='technologyCards']");
+  await page.getByTestId("technologyCards").scrollIntoViewIfNeeded();
 });
