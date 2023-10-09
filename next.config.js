520f4ab05bea22cabbfc377e6fc6e56d349a3575
Harry Ross <HarryRoss@ssw.com.au>
2023-10-09 17:16:28 +1100

    Replaced domains with remotePatterns

--- next.config.js
+++ next.config.js
@@ -5,14 +5,42 @@ const config = {
   },
   poweredByHeader: false,
   images: {
-    domains: [
-      "assets.tina.io",
-      "i.ytimg.com",
-      "img.youtube.com",
-      "ssw.com.au",
-      "www.ssw.com.au",
-      "i.vimeocdn.com",
-      "secure-content.meetupstatic.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "assets.tina.io",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "**ssw.com.au",
+        port: "",
+      },
+      {
+        protocol: "http",
+        hostname: "**ssw.com.au",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "i.vimeocdn.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "secure-content.meetupstatic.com",
+        port: "",
+      },
     ],
   },
   output: "standalone", // required for Docker support
