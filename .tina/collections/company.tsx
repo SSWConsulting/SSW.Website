8d2f79cbb3d2964a6ccf93ad322bfe5838f6858f
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2023-10-17 10:41:15 +1100

    Capitalizing categories

--- .tina/collections/company.tsx
+++ .tina/collections/company.tsx
@@ -222,7 +222,7 @@ export const companyIndexSchema: Collection = {
 };
 
 export const clientsCategorySchema: Collection = {
-  label: "Company - Client categories",
+  label: "Company - Client Categories",
   name: "clientCategories",
   path: "content/company/clientCategories",
   format: "json",
