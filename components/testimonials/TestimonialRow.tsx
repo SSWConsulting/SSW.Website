7cf9efe53d0c1e96ab961b2fb3a6581033a1a7ab
Kristen Hu [SSW] <52522913+KristenHu@users.noreply.github.com>
2023-10-19 11:38:56 +0800

    ✨Migrate the testimonials page (#1401)
    * ✨Migrate the testimonials page
    
    * ✨Add breadcrumb and SEO description
    
    * ✨Add testimonials component
    
    * ✨Add button to hide internship testimonials from all
    
    * Merge from main
    
    * 🐛Fix the eslint warning
    
    * ✨Migrate part of the testimonial cards from old page
    
    * 📃Migrate testimonial cards
    
    * 🐛center the loading animation
    
    * 🐛Fix the display issue of testimonial cards
    
    * ✨Extract testimonialCards as a public component
    
    * Merge from `main` branch
    
    * 🐛Fix the bug to show testimonials cards correctly
    
    * ⚡Remove useless code from company.tsx
    
    Co-authored-by: Harry Ross [SSW] <harryross@ssw.com.au>
    
    * ⚡Remove useless code from company.tsx
    
    Co-authored-by: Harry Ross [SSW] <harryross@ssw.com.au>
    
    * ⚡Remove useless props form testimonials.mdx
    
    * 🐛Reverse the file
    
    * ⚡Remove the wrong description in testimonialsList
    
    Co-authored-by: Harry Ross [SSW] <harryross@ssw.com.au>
    
    ---------
    
    Co-authored-by: Harry Ross [SSW] <harryross@ssw.com.au>

--- components/testimonials/TestimonialRow.tsx
+++ components/testimonials/TestimonialRow.tsx
@@ -1,16 +1,11 @@
-import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useEditState } from "tinacms/dist/react";
-import { TinaMarkdown } from "tinacms/dist/rich-text";
 import {
-  getTestimonialsByCategories,
   TestimonialType,
+  getTestimonialsByCategories,
 } from "../../helpers/getTestimonials";
-import { Rating } from "../util/consulting/rating";
+import { TestimonialCard } from "../blocks/testimonialsCard";
 import { Container } from "../util/container";
-import classNames from "classnames";
-
-const defaultAvatar = "/images/thumbs/avatar-thumbnail.png";
 
 type TestimonialRowProps = {
   testimonialsResult: TestimonialType[];
@@ -53,43 +48,6 @@ export const TestimonialRow = ({
         ))}
       </div>
     </Container>
-  );
-};
-
-type TestimonialCardProps = {
-  testimonial: TestimonialType;
-};
-
-const TestimonialCard = ({ testimonial }: TestimonialCardProps) => {
-  return (
-    <div
-      className="flex w-full grow flex-col rounded-md border-b-4 border-b-sswRed bg-gray-100 p-8 text-center text-xl drop-shadow md:min-h-96 md:max-w-sm md:grow-0 md:p-10 md:basis_gap-96-6"
-      data-aos="flip-right"
-    >
-      <div className="flex flex-col items-center">
-        <Image
-          alt={`Picture of ${testimonial?.name} as an avatar`}
-          src={testimonial?.avatar ?? defaultAvatar}
-          height={120}
-          width={120}
-          quality={90}
-          className="rounded-full"
-        />
-      </div>
-      <Rating className="mx-auto mt-8" rating={testimonial?.rating} />
-      <p className={classNames("mt-4", testimonial?.company && "min-h-24")}>
-        {testimonial?.name}
-        {testimonial?.company && (
-          <>
-            {", "}
-            <span className="mb-2 font-semibold">{testimonial?.company}</span>
-          </>
-        )}
-      </p>
-      <div className="text-sm text-gray-900">
-        <TinaMarkdown content={testimonial?.body} />
-      </div>
-    </div>
   );
 };
 
