d835fdfa5b6ae4a14864da614ee713760a20d742
Harry Ross <hazross@hotmail.com>
2023-10-23 15:55:01 +1100

    Added alt text

--- components/blocks/eventLink.tsx
+++ components/blocks/eventLink.tsx
@@ -6,18 +6,28 @@ import { flagSchema } from "./flag";
 import { componentRenderer } from "./mdxComponentRenderer";
 
 export type EventLinkProps = {
-  heading?: string;
   link?: string;
   eventThumbnail?: string;
+  thumbnailAlt?: string;
   content?: TinaMarkdownContent;
 };
 
-export const EventLink = ({ heading, link, eventThumbnail, content }) => {
+export const EventLink = ({
+  link,
+  eventThumbnail,
+  thumbnailAlt,
+  content,
+}: EventLinkProps) => {
   return (
     <Link href={link || ""} passHref className="!no-underline">
       <div className="flex flex-row gap-4 rounded-lg px-4 hover:bg-gray-100">
         {eventThumbnail && (
-          <Image src={eventThumbnail} alt={heading} width={150} height={150} />
+          <Image
+            src={eventThumbnail}
+            alt={thumbnailAlt || "Event link thumbnail"}
+            width={150}
+            height={150}
+          />
         )}
         <div className="children-h3:!font-semibold children-h3:!text-sswRed">
           <TinaMarkdown content={content} components={componentRenderer} />
@@ -48,6 +58,11 @@ export const eventLinkSchema: Template = {
       name: "eventThumbnail",
       // @ts-expect-error tinacms types are wrong
       uploadDir: () => "events",
+    },
+    {
+      type: "string",
+      label: "Thumbnail Alt Text",
+      name: "thumbnailAlt",
     },
   ],
 };
