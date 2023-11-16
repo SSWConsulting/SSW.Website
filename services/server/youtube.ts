7d6eefeec913d75ffa37aac51af601718eab5e64
Jeoffrey Fischer [SSW] <jeoffrey.fischer@gmail.com>
2023-11-16 16:47:39 +1100

    updated youtube key name in env

--- services/server/youtube.ts
+++ services/server/youtube.ts
@@ -1,5 +1,5 @@
 export async function getYoutubePlaylist(playlistId: string) {
-  const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&part=id&part=snippet&part=status&maxResults=100&playlistId=${playlistId}&key=${process.env.PRIVATE_KEY}`;
+  const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&part=id&part=snippet&part=status&maxResults=100&playlistId=${playlistId}&key=${process.env.YOUTUBE_PRIVATE_KEY}`;
   const res = await fetch(url);
   const data = await res.json();
   return data;
