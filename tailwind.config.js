0e3b0c2b9a52fd426da10492a1c70d79a75cb344
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2023-11-17 14:23:21 +1100

    Converting white text color to code

--- tailwind.config.js
+++ tailwind.config.js
@@ -56,7 +56,7 @@ module.exports = {
         facebook: "#3b5998",
         xtwitter: "#000",
         tiktok: "#000",
-        github: "white",
+        github: "#fff",
         meetup: "#f05664",
       },
       sswRed: "#cc4141",
