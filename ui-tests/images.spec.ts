706ba5b2b46103904dc96e4b7bcb776272872585
Matt Wicks [SSW] <wicksipedia@users.noreply.github.com>
2023-11-21 14:07:10 +1100

    Fix deployments (#1739)
    * UI Tests - Add response.ok() checks
    
    * Deploy - swap slots - source env variables from repo
    
    * UI tests - add URL health check action before running tests

--- ui-tests/images.spec.ts
+++ ui-tests/images.spec.ts
@@ -7,7 +7,8 @@ test("Images load successfully on index", async ({ page }) => {
     }
   });
 
-  await page.goto("/", { waitUntil: "networkidle" });
+  const response = await page.goto("/", { waitUntil: "networkidle" });
+  expect(response.ok()).toBeTruthy();
 
   // Taken from https://github.com/microsoft/playwright/issues/19277
   const sizes = await page.evaluate(() => {
@@ -30,7 +31,10 @@ test("Images load successfully on consulting page", async ({ page }) => {
     }
   });
 
-  await page.goto("/consulting/react", { waitUntil: "networkidle" });
+  const response = await page.goto("/consulting/react", {
+    waitUntil: "networkidle",
+  });
+  expect(response.ok()).toBeTruthy();
 
   // Taken from https://github.com/microsoft/playwright/issues/19277
   const sizes = await page.evaluate(() => {
