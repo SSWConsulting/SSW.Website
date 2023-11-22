45be617b65be250fbc8a578f0b7ed3a86b8fb925
Harry Ross <hazross@hotmail.com>
2023-11-23 10:13:11 +1100

    Added console log request

--- middleware/noIndex.ts
+++ middleware/noIndex.ts
@@ -4,6 +4,7 @@ export const addNoIndexHeaders = (
   request: NextRequest,
   response: NextResponse
 ) => {
+  console.log(request);
   try {
     const siteUrl = new URL(process.env.SITE_URL || "https://www.ssw.com.au");
 
