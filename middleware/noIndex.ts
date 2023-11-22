4780079ebaf43abb8a8b79a5a4d8139438f204c6
Harry Ross <hazross@hotmail.com>
2023-11-23 10:37:35 +1100

    Added debug header

--- middleware/noIndex.ts
+++ middleware/noIndex.ts
@@ -4,7 +4,7 @@ export const addNoIndexHeaders = (
   request: NextRequest,
   response: NextResponse
 ) => {
-  console.log(request);
+  response.headers.set("Debug-Header-Here", JSON.stringify(request.nextUrl));
   try {
     const siteUrl = new URL(process.env.SITE_URL || "https://www.ssw.com.au");
 
