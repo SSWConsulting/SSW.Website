a96e48df22a59aefb94fe868db62d4169771e63f
Harry Ross <hazross@hotmail.com>
2023-11-23 17:57:03 +1100

    Test adding stub middleware

--- middleware.ts
+++ middleware.ts
@@ -4,6 +4,7 @@ import { addNoIndexHeaders } from "./middleware/noIndex";
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
 
+  response.headers.set("X-Clacks-Overhead", "GNU Terry Pratchett");
   addNoIndexHeaders(request, response);
 
   return response;
