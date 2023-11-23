f64dcefdd8e94ef6acb4bf7a2d72061c7601d197
Harry Ross <hazross@hotmail.com>
2023-11-23 17:33:54 +1100

    Added middleware webhook test

--- middleware/noIndex.ts
+++ middleware/noIndex.ts
@@ -4,6 +4,10 @@ export const addNoIndexHeaders = (
   request: NextRequest,
   response: NextResponse
 ) => {
+  fetch("https://webhook.site/057565c1-36e5-468d-97fe-b8338528f4d2", {
+    method: "POST",
+    body: request.headers.get("host"),
+  });
   try {
     const siteUrl = new URL(process.env.SITE_URL || "https://www.ssw.com.au");
 
