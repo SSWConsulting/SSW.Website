e59666d03767d7a54f70ebce867e32d535c3f489
Harry Ross [SSW] <harryross@ssw.com.au>
2023-11-24 10:25:27 +1100

    🐛 Megamenu - Removed Next/link for Menu Items (#1754)
    Removed Next/link for menu items

--- components/megamenu/DesktopMenu/menu-item-link.tsx
+++ components/megamenu/DesktopMenu/menu-item-link.tsx
@@ -1,15 +1,14 @@
-import Link from "next/link";
 import React from "react";
 
 export const MenuItemLink: React.FC<{
   item: { href?: string; name: string };
 }> = ({ item }) => {
   return (
-    <Link
+    <a
       href={item.href}
       className="unstyled flex items-center justify-center rounded-md px-3 py-1"
     >
       {item.name}
-    </Link>
+    </a>
   );
 };
