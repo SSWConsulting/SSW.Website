bc008e62c7b8a1773561d1c67a1c2b37c6c6debd
Harry Ross <hazross@hotmail.com>
2023-11-23 12:56:51 +1100

    Fixed to use host in header instead of nextUrl

--- middleware/noIndex.ts
+++ middleware/noIndex.ts
@@ -4,12 +4,11 @@ export const addNoIndexHeaders = (
   request: NextRequest,
   response: NextResponse
 ) => {
-  console.log(request.nextUrl);
   try {
     const siteUrl = new URL(process.env.SITE_URL || "https://www.ssw.com.au");
 
     if (
-      sanitizeHostname(request.nextUrl.hostname) !==
+      sanitizeHostname(request.headers.get("host")) !==
       sanitizeHostname(siteUrl.hostname)
     ) {
       response.headers.set("X-Robots-Tag", "noindex");
