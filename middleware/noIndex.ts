57cd09155d5c2c62e1e780f621e6271a65d51597
Harry Ross <hazross@hotmail.com>
2023-11-23 11:55:36 +1100

    Added back console log

--- middleware/noIndex.ts
+++ middleware/noIndex.ts
@@ -1,18 +1,10 @@
-import * as appInsight from "applicationinsights";
 import { NextRequest, NextResponse } from "next/server";
 
 export const addNoIndexHeaders = (
   request: NextRequest,
   response: NextResponse
 ) => {
-  appInsight.defaultClient.trackEvent({
-    name: "addNoIndexHeaders",
-    properties: {
-      url: request.nextUrl.pathname,
-      hostname: request.nextUrl.hostname,
-      urlObj: JSON.stringify(request.nextUrl),
-    },
-  });
+  console.log(request.nextUrl);
   try {
     const siteUrl = new URL(process.env.SITE_URL || "https://www.ssw.com.au");
 
