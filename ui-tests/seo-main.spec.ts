1a8b20166721d91c224adf9f5188a0195b46337a
Harry Ross <hazross@hotmail.com>
2023-11-24 11:34:28 +1100

    Added tests for SEO cookies

---
+++ ui-tests/seo-main.spec.ts
@@ -0,0 +1,9 @@
+import test, { expect } from "@playwright/test";
+
+test("No index header not present", async ({ page }) => {
+  const response = await page.goto("/", {
+    waitUntil: "networkidle",
+  });
+  expect(response.ok()).toBeTruthy();
+  expect(response.headerValue("X-Robots-Tag")).not.toEqual("noindex");
+});
