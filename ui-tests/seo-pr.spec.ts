1f200bb76d0af6b5ac58644518fcdae22019d108
Harry Ross <hazross@hotmail.com>
2023-11-24 14:19:22 +1100

    Adding response await in correct place

--- ui-tests/seo-pr.spec.ts
+++ ui-tests/seo-pr.spec.ts
@@ -1,12 +1,12 @@
 import test, { expect } from "@playwright/test";
 
-test("No index header not present", async ({ page }) => {
-  await page.on("response", async (response) => {
-    expect(await response.allHeaders["X-Robots-Tag"]).toEqual("noindex");
-  });
-
+test("No index header present", async ({ page }) => {
   const response = await page.goto("/", {
     waitUntil: "networkidle",
   });
   expect(response.ok()).toBeTruthy();
+
+  await page.on("response", async (response) => {
+    expect(await response.allHeaders["X-Robots-Tag"]).toEqual("noindex");
+  });
 });
