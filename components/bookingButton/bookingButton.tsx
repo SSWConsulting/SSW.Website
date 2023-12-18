180911e1077e493c59ff1012f8cecbb5ebf0ea14
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2023-12-19 09:22:37 +1100

    Removing empty react component causing Lint issue

--- components/bookingButton/bookingButton.tsx
+++ components/bookingButton/bookingButton.tsx
@@ -4,7 +4,6 @@ import { twMerge } from "tailwind-merge";
 import type { Template } from "tinacms";
 import layoutData from "../../content/global/index.json";
 import { recaptchaToastId, useRecaptcha } from "../../context/RecaptchaContext";
-import { JotFormIframe } from "../../pages/jotForm-script";
 import { UtilityButton } from "../button/utilityButton";
 
 export interface BookingButtonProps {
@@ -27,6 +26,30 @@ export const BookingButton = ({ data }) => {
   if (recaptchaError) {
     toast.error("Failed to load recaptcha key.", { toastId: recaptchaToastId });
   }
+
+  const JotFormIframe = `
+      if(!window.scriptExecuted ){
+        window.scriptExecuted = true;
+        var _sf = new JotformFeedback({
+          formId: '233468468973070',
+          base: 'https://form.jotform.com/',
+          windowTitle: 'Book an initial meeting now',
+          backgroundColor: '#BD4B47',
+          fontColor: '#FFFFFF',
+          type: '0',
+          height: 800,
+          width: 700,
+          openOnLoad: false
+        });
+
+        window.handleIFrameMessage = function(e) {
+          if (window.addEventListener) {
+              window.addEventListener("message", handleIFrameMessage, false);
+          } else if (window.attachEvent) {
+              window.attachEvent("onmessage", handleIFrameMessage);
+          }
+        }
+    }`;
 
   const JOTFORM_ID = "233468468973070"; // TODO: Process.env.JOTID
 
