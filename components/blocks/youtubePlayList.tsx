c9bf6593564c703a9a1c846d9c64a02211392040
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2023-12-21 17:34:11 +1100

    Separate component for Youtube list Block

---
+++ components/blocks/youtubePlayList.tsx
@@ -0,0 +1,75 @@
+import { BsArrowRightCircle } from "react-icons/bs";
+import { Template, TinaField } from "tinacms";
+import { UtilityButton } from "../button/utilityButton";
+import { VideoCard } from "../util/videoCards";
+
+export const YoutubePlayListBlock = ({ props, playlistVideosLinks }) => {
+  return (
+    <>
+      <span className="text-sswRed">
+        <h2>{props.title}</h2>
+      </span>
+      <div className="grid grid-cols-1 justify-center gap-8 lg:grid-cols-3">
+        {playlistVideosLinks.map((video, index) => (
+          <div key={index}>
+            <VideoCard {...video} theme="light" />
+          </div>
+        ))}
+      </div>
+      <div className="flex justify-center">
+        <UtilityButton
+          size="small"
+          uncentered={false}
+          link={`https://www.youtube.com/playlist?list=${props.playlistId}`}
+          buttonText={
+            <>
+              {props.textForPlayListLink}
+              <BsArrowRightCircle className="ml-1 inline" />
+            </>
+          }
+          noAnimate
+          openInNewTab={true}
+        />
+      </div>
+    </>
+  );
+};
+
+export const youtubePlayListBlockSchema: TinaField = {
+  type: "object",
+  label: "Youtube Playlist",
+  name: "youtubePlaylist",
+  ui: {
+    itemProps: (item) => {
+      return { label: item?.title };
+    },
+  },
+  fields: [
+    {
+      type: "string",
+      label: "Title",
+      name: "title",
+    },
+    {
+      type: "string",
+      label: "Playlist Id",
+      name: "playlistId",
+    },
+    {
+      type: "number",
+      label: "Number of vidoes",
+      name: "numberOfVideos",
+    },
+    {
+      type: "string",
+      name: "textForPlayListLink",
+      label: "Text for Playlist link",
+    },
+  ],
+};
+
+export const youtubePlayListBlockSchemaTempalte: Template = {
+  name: "YoutubePlayListBlock",
+  label: "Youtube PlayList Block",
+  fields: [youtubePlayListBlockSchema],
+};
