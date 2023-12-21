7d4f3cc9cf0ff49cfb36de9420323d80e807acde
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2023-12-21 15:13:02 +1100

    Removing secret from the commit

--- services/server/youtube.ts
+++ services/server/youtube.ts
@@ -7,7 +7,7 @@ export async function getYoutubePlaylist(
   playlistId: string,
   videosCount: number
 ): Promise<PlayListType> {
-  const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&part=id&part=snippet&part=status&maxResults=100&playlistId=${playlistId}&key=AIzaSyArtxZfNPXmFma3C3y2pTol7PiQg8qou3g`;
+  const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&part=id&part=snippet&part=status&maxResults=100&playlistId=${playlistId}&key=${process.env.YOUTUBE_PRIVATE_KEY}`;
   const res = await fetch(url);
   const data = await res.json();
 
