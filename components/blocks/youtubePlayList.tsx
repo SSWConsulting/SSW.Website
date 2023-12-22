7cd2c3503b4e0d716895c79bfbe73975162f2fb6
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2023-12-22 11:23:11 +1100

    Adding type binding with youtube component

--- components/blocks/youtubePlayList.tsx
+++ components/blocks/youtubePlayList.tsx
@@ -1,30 +1,54 @@
+import { Key } from "react";
 import { BsArrowRightCircle } from "react-icons/bs";
 import { Template, TinaField } from "tinacms";
 import { CustomLink } from "../customLink";
 import { VideoCard } from "../util/videoCards";
 
-export const YoutubePlayListBlock = ({ props, playlistVideosLinks }) => {
+export type VideoProps = {
+  title: string;
+  link: string;
+};
+
+export type YoutubePlayListProps = {
+  title?: string;
+  playlistId: string;
+  numberOfVideos: number;
+  textForPlayListLink?: string;
+};
+
+export type YoutubePlayListBlockProps = {
+  playlistVideosLinks: VideoProps[];
+  props: YoutubePlayListProps;
+};
+export const YoutubePlayListBlock: React.FC<YoutubePlayListBlockProps> = ({
+  props,
+  playlistVideosLinks,
+}) => {
   return (
     <>
-      <span className="text-sswRed">
-        <h2>{props?.title}</h2>
-      </span>
+      {props?.title && (
+        <span className="text-sswRed">
+          <h2>{props?.title}</h2>
+        </span>
+      )}
       <div className="grid grid-cols-1 justify-center gap-8 lg:grid-cols-3">
-        {playlistVideosLinks?.map((video, index) => (
+        {playlistVideosLinks?.map((video: VideoProps, index: Key) => (
           <div key={index}>
             <VideoCard {...video} theme="light" />
           </div>
         ))}
       </div>
-      <div className="flex justify-center">
-        <CustomLink
-          href={`https://www.youtube.com/playlist?list=${props?.playlistId}`}
-          className="done relative mx-2 mt-8 inline-flex overflow-hidden rounded border-none bg-sswRed pl-3 text-white"
-        >
-          {props?.textForPlayListLink}
-          <BsArrowRightCircle className="ml-1 inline" />
-        </CustomLink>
-      </div>
+      {props?.textForPlayListLink && (
+        <div className="flex justify-center">
+          <CustomLink
+            href={`https://www.youtube.com/playlist?list=${props?.playlistId}`}
+            className="done relative mx-2 mt-8 inline-flex overflow-hidden rounded border-none bg-sswRed pl-3 text-white"
+          >
+            {props?.textForPlayListLink}
+            <BsArrowRightCircle className="ml-1 inline" />
+          </CustomLink>
+        </div>
+      )}
     </>
   );
 };
@@ -48,11 +72,13 @@ export const youtubePlayListSchema: TinaField = {
       type: "string",
       label: "Playlist Id",
       name: "playlistId",
+      required: true,
     },
     {
       type: "number",
       label: "Number of vidoes",
       name: "numberOfVideos",
+      required: true,
     },
     {
       type: "string",
