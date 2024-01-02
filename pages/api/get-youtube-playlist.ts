077cdb339040a2bed71ae99d4be7ecc6067933c9
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-01-02 13:22:01 +1100

    Refactoring API for youtube playlist

--- pages/api/get-youtube-playlist.ts
+++ pages/api/get-youtube-playlist.ts
@@ -1,18 +1,39 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+import { cache } from "../../services/server/cacheService";
+const CACHE_HOURS = 4 * 60 * 60; // ~ 4 hours
+
 import { getYoutubePlaylist } from "../../services/server/youtube";
 
+type NextApiRequestWithParams = NextApiRequest & {
+  query: {
+    playlistId: string;
+    videosCount: number;
+  };
+};
+
 export default async function handler(
-  req: NextApiRequest,
+  req: NextApiRequestWithParams,
   res: NextApiResponse
 ) {
   if (req.method === "GET") {
     try {
-      const playlistId: string = req.query.playlistId;
-      const videoCount: number = req.query.videosCount;
-      const videos = await getYoutubePlaylist(playlistId, videoCount);
-      res.status(200).send(videos);
+      const { playlistId, videosCount } = req.query;
+
+      if (!playlistId) {
+        return res.status(400).json({ message: "Invalid PlaylistId" });
+      }
+
+      if (!cache.has(playlistId)) {
+        const playlist = await getYoutubePlaylist(playlistId, videosCount);
+        cache.set(playlistId, playlist, CACHE_HOURS);
+        res.status(200).send(playlist);
+      } else {
+        const cachedPlaylist = cache.get(playlistId);
+        res.status(200).send(cachedPlaylist);
+      }
     } catch (error) {
+      console.error("Error occurred:", error);
       res.status(error.statusCode).json({ message: error.message });
     }
   } else {
