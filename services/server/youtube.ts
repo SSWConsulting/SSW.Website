ec543c655e13aee8ce7a736842897a64934bc235
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-01-03 16:53:01 +1100

    Adding max videos value

--- services/server/youtube.ts
+++ services/server/youtube.ts
@@ -3,18 +3,22 @@ export type YoutubeVideoLinkType = {
   link: string;
 };
 
+const defaultVideoCount = 6;
+
 export async function getYoutubePlaylist(
   playlistId: string,
   videosCount: number
 ): Promise<YoutubeVideoLinkType[]> {
-  const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&part=id&part=snippet&part=status&maxResults=100&playlistId=${playlistId}&key=${process.env.YOUTUBE_PRIVATE_KEY}`;
+  const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&part=id&part=snippet&part=status&maxResults=${
+    videosCount ?? defaultVideoCount
+  }&playlistId=${playlistId}&key=${process.env.YOUTUBE_PRIVATE_KEY}`;
   const res = await fetch(url);
   const data = await res.json();
 
   if (data && data.items) {
     const playListVideosBaseUrl = "https://www.youtube.com/watch?v=";
 
-    return data.items.slice(0, videosCount).map((item) => ({
+    return data.items.map((item) => ({
       title: item.snippet.title,
       link: `${playListVideosBaseUrl}${item.snippet.resourceId.videoId}&list=${item.snippet.playlistId}`,
     }));
