b92201fe0fd1a3ccf3ee0bc1635bd7d02a6e123a
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-01-03 15:13:40 +1100

    Adding meaningful name for return type

--- services/server/youtube.ts
+++ services/server/youtube.ts
@@ -3,10 +3,12 @@ export type PlaylistType = {
   link: string;
 };
 
+type PlaylistArray = PlaylistType[];
+
 export async function getYoutubePlaylist(
   playlistId: string,
   videosCount: number
-): Promise<PlaylistType[]> {
+): Promise<PlaylistArray> {
   const url = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&part=id&part=snippet&part=status&maxResults=100&playlistId=${playlistId}&key=${process.env.YOUTUBE_PRIVATE_KEY}`;
   const res = await fetch(url);
   const data = await res.json();
