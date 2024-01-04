7f425d6f3c4fc463ca5dca97227be65de264abfd
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-01-05 09:47:43 +1100

    Adding animation option and a caption

--- components/blocks/customImage.tsx
+++ components/blocks/customImage.tsx
@@ -10,19 +10,35 @@ export type CustomImageProps = {
   width?: number;
   link?: string;
   customClass?: string;
+  caption?: string;
+  captionColor?: string;
 };
 
 // To Update the screenshots of PowerBi reports on Consulting Options (i.e /consulting/consulting), here is the link - https://github.com/SSWConsulting/SSW.Website/wiki/Consulting-options-%E2%80%90-Updating-screenshots
 export const CustomImage = ({ data }: { data: CustomImageProps }) => {
   return (
     <LinkWrapper link={data.link}>
-      <Image
-        src={data.src}
-        alt={data.altText}
-        height={data.height || 400}
-        width={data.width || 400}
-        className={classNames("inline-block", data.customClass ?? "")}
-      />
+      <div className="flex flex-col items-center">
+        <div className="relative">
+          <Image
+            src={data.src}
+            alt={data.altText}
+            height={data.height || 400}
+            width={data.width || 400}
+            className={classNames("inline-block", data.customClass ?? "")}
+          />
+          {data.caption && (
+            <p
+              className={classNames(
+                "font-bold text-start",
+                data.captionColor ?? ""
+              )}
+            >
+              {data.caption}
+            </p>
+          )}
+        </div>
+      </div>
     </LinkWrapper>
   );
 };
@@ -73,6 +89,28 @@ export const customImageBlockSchema: Template = {
       label: "Custom Class (optional)",
       name: "customClass",
       required: false,
+    },
+    {
+      type: "string",
+      label: "Caption (optional)",
+      name: "caption",
+      required: false,
+    },
+    {
+      type: "string",
+      label: "Caption Color (optional)",
+      name: "captionColor",
+      required: false,
+      options: [
+        {
+          label: "Black",
+          value: "text-sswBlack",
+        },
+        {
+          label: "White",
+          value: "text-white",
+        },
+      ],
     },
   ],
 };
