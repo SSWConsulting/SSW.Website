08f8b9c249d3a7c2c794c7f7b607629d29802c44
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-01-23 16:28:05 +1100

    Fixing the function for replacing multiple dashes

--- helpers/functions.ts
+++ helpers/functions.ts
@@ -1,4 +1,4 @@
 export const extractFileName = (filePath: string) =>
   filePath && typeof filePath === "string"
-    ? filePath?.split("/").pop().split(".")[0].replace("-", " ")
+    ? filePath?.split("/").pop().split(".")[0].replaceAll("-", " ")
     : "";
