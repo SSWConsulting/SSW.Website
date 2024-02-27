fbc9de9308f41dbad978a52afcb6c81082c349fc
Harry Ross <hazross@hotmail.com>
2024-02-27 17:10:55 +1100

    Added app insights tracking on error page

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,11 +1,25 @@
 import { ErrorPage } from "@/components/util/error-page";
+import { useAppInsightsContext } from "@microsoft/applicationinsights-react-js";
 import { ErrorProps } from "next/error";
 
-export default function Error({ statusCode }: ErrorProps) {
+export default function UnderscoreErrorPage({ statusCode, title }: ErrorProps) {
+  const appInsights = useAppInsightsContext();
+
+  if (appInsights) {
+    appInsights.trackException({
+      exception: new Error(`Error ${statusCode}`),
+      properties: {
+        Request: "GET /_error",
+        Status: statusCode,
+        Title: title,
+      },
+    });
+  }
+
   return <ErrorPage code={statusCode.toString()} />;
 }
 
-Error.getInitialProps = ({ res, err }) => {
+UnderscoreErrorPage.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
   return { statusCode };
 };
