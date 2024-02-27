e953508ea3eab5c52c62f4b16856bc7425129f3f
Harry Ross <hazross@hotmail.com>
2024-02-27 15:24:01 +1100

    changed event name

--- components/util/app-insights-test.tsx
+++ components/util/app-insights-test.tsx
@@ -5,7 +5,7 @@ export const AppInsightTest = () => {
   const appInsights = useAppInsightsContext();
 
   useEffect(() => {
-    appInsights.trackEvent({ name: "testing frontend event" });
+    appInsights.trackEvent({ name: "testing frontend event on production" });
   }, [appInsights]);
 
   return <></>;
