6a645de0e0d825c53f5bd409027b5e7a8c23eace
Harry Ross [SSW] <harryross@ssw.com.au>
2024-02-27 12:43:53 +1100

    ♻️ Megamenu - Refactor SocialIcons (#1971)
    * Changed to use union types for socialIcons, TODO: broken
    
    * Added stub API route
    
    * Added solid violation comment
    
    * Added CORS headers + cache control headers
    
    * added props
    
    * Fixed types
    
    * moved back to using exclude
    
    * Fixed to replicate old logic
    
    * Fixed desktop menu
    
    * Removed SOLID todo
    
    * Moved socialIcons to own folder, created separate phone component
    
    * Simplified Tina structure, removed unused header component
    
    * removed "(used on menu)"
    
    * fixed schema
    
    * removed erraneous phone
    
    * removed phone from json data

---
+++ components/socialIcons/phoneButton.tsx
@@ -0,0 +1,44 @@
+import classNames from "classnames";
+import { isMobile } from "react-device-detect";
+import { FaPhoneAlt } from "react-icons/fa";
+import { CustomLink } from "../customLink";
+
+type PhoneButtonProps = {
+  className?: string;
+  desktop: {
+    text: string;
+    url: string;
+  };
+  mobile: {
+    text: string;
+    url: string;
+  };
+};
+
+export const PhoneButton = ({
+  className,
+  mobile,
+  desktop,
+}: PhoneButtonProps) => {
+  const url =
+    (isMobile ? mobile.url : desktop.url) ||
+    "https://ssw.com.au/company/contact-us";
+  const text = (isMobile ? mobile.text : desktop.text) || "CONTACT US";
+
+  return (
+    <div
+      className={classNames(
+        className,
+        "flex flex-grow flex-wrap gap-2 sm:flex-grow-0"
+      )}
+    >
+      <CustomLink
+        href={url}
+        className="flex h-12 w-full shrink-0 cursor-pointer items-center justify-center rounded-lg bg-ssw-red px-5 text-xl hover:opacity-70 max-sm:my-5 sm:w-fit"
+      >
+        <FaPhoneAlt color="white" className="text-2xl" />
+        <span className="ml-2 inline text-sm font-bold text-white">{text}</span>
+      </CustomLink>
+    </div>
+  );
+};
