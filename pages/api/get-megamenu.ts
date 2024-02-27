37f7de6478c777b603f2a299d3d97a2e90959b36
Harry Ross <hazross@hotmail.com>
2024-02-27 12:12:38 +1100

    added production test

--- pages/api/get-megamenu.ts
+++ pages/api/get-megamenu.ts
@@ -9,6 +9,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  appInsights?.defaultClient.trackEvent({
+    name: "testing testing testing production",
+  });
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Cache-Control", `s-maxage=${CACHE_SECS}`);
 
