880e3d09af3c35068d6f2bcbdb2b97777d04fa29
Harry Ross <hazross@hotmail.com>
2024-02-28 12:44:47 +1100

    hide chatbase iframe behind button

--- components/zendeskButton/chatBaseBot.tsx
+++ components/zendeskButton/chatBaseBot.tsx
@@ -1,15 +1,26 @@
-import Script from "next/script";
+import { useState } from "react";
 
 const chatBaseBotId = process.env.NEXT_PUBLIC_CHATBASE_BOT_ID;
 
 const ChatBaseBot = () => {
+  const [chatOpen, setChatOpen] = useState(false);
+
   return (
     chatBaseBotId && (
-      <Script
-        src="https://www.chatbase.co/embed.min.js"
-        id={chatBaseBotId}
-        defer
-      />
+      <>
+        <button onClick={() => setChatOpen((prev) => !prev)}>
+          Test button here
+        </button>
+        {chatOpen && (
+          <iframe
+            src="https://www.chatbase.co/chatbot-iframe/gnbZXuG_5tgI_R3yzKcOn"
+            width="100%"
+            className=""
+            style={{ height: "100%", minHeight: "700px" }}
+            frameBorder={0}
+          />
+        )}
+      </>
     )
   );
 };
