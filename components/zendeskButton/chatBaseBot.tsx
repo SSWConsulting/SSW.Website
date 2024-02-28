c1ee28453311ea0327765244604bda2aff7edbbf
Harry Ross <hazross@hotmail.com>
2024-02-28 11:55:00 +1100

    temp: removed chatbase to test hydration

--- components/zendeskButton/chatBaseBot.tsx
+++ components/zendeskButton/chatBaseBot.tsx
@@ -1,15 +1,16 @@
-import Script from "next/script";
+// import Script from "next/script";
 
 const chatBaseBotId = process.env.NEXT_PUBLIC_CHATBASE_BOT_ID;
 
 const ChatBaseBot = () => {
   return (
     chatBaseBotId && (
-      <Script
-        src="https://www.chatbase.co/embed.min.js"
-        id={chatBaseBotId}
-        defer
-      />
+      // <Script
+      //   src="https://www.chatbase.co/embed.min.js"
+      //   id={chatBaseBotId}
+      //   defer
+      // />
+      <></>
     )
   );
 };
