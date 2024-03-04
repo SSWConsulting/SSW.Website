29c99a452ff5704b9ecdb5a80b7ebd77e98312ef
Harry Ross <hazross@hotmail.com>
2024-03-05 10:35:26 +1100

    added app insights for 500 pages

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,10 +1,26 @@
 import { ErrorPage } from "@/components/util/error-page";
+import { useAppInsightsContext } from "@microsoft/applicationinsights-react-js";
 import { InferGetStaticPropsType } from "next";
+import { useEffect } from "react";
 import client from "../.tina/__generated__/client";
 
-export default function FourOhFour(
+export default function FiveHundred(
   props: InferGetStaticPropsType<typeof getStaticProps>
 ) {
+  const appInsights = useAppInsightsContext();
+
+  useEffect(() => {
+    if (appInsights) {
+      appInsights.trackException({
+        exception: new Error(`Error ${500}`),
+        properties: {
+          Request: "GET /_error",
+          Status: 500,
+        },
+      });
+    }
+  }, [appInsights]);
+
   return (
     <ErrorPage
       code="500"
