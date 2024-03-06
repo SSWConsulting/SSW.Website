0ae0bb33307ebf5253a9f727a0f151c72dc0cc5d
Harry Ross <hazross@hotmail.com>
2024-03-06 12:06:49 +1100

    fixed broken unit test

--- helpers/dates.ts
+++ helpers/dates.ts
@@ -1,6 +1,8 @@
 import dayjs from "dayjs";
 import isBetween from "dayjs/plugin/isBetween";
+import timezone from "dayjs/plugin/timezone";
 dayjs.extend(isBetween);
+dayjs.extend(timezone);
 
 export const EventStatus = {
   TODAY: "today",
