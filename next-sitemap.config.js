dd2b72c1f348f83aba754175c0471731ed05f58a
Harry Ross <hazross@hotmail.com>
2024-03-13 12:46:59 +1100

    added old sitemap urls

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,20 +6,51 @@ module.exports = {
   priority: 0.7,
   sitemapSize: 5000,
   generateRobotsTxt: true,
-  // add redirects, codeauditor
+  additionalPaths: async () => {
+    const otherURLs = [
+      "https://www.ssw.com.au/rules/",
+      "https://sswdigital.com/",
+      "https://sswsophie.com/sophiebot/",
+      "https://sugarlearning.com/",
+      "https://sswtimepro.com/",
+      "https://codeauditor.com/",
+      "https://sswhealthcheck.com/",
+      "https://smashingbarrier.com/",
+      "https://fireusergroup.com/",
+      "https://bettersoftwaresuggestions.com/",
+      "https://blog.ssw.com.au/",
+      "https://adamcogan.com/",
+      "https://tv.ssw.com/",
+    ];
+
+    return otherURLs.map((url) => ({
+      loc: url,
+      changefreq: "daily",
+      priority: 0.7,
+      lastmod: new Date().toISOString(),
+    }));
+  },
   robotsTxtOptions: {
     policies: [
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/404", "/500", "/ssw/Redirect", "/ssw/CodeAuditor", "/ssw/Version.aspx", "/ssw/LinkAuditor"],
+        disallow: [
+          "/404",
+          "/500",
+          "/ssw/Redirect",
+          "/ssw/CodeAuditor",
+          "/ssw/Version.aspx",
+          "/ssw/LinkAuditor",
+        ],
       },
     ],
     additionalSitemaps: [
       // TODO: add sitemaps for other sites when they are available
       // 'https://www.ssw.com.au/people/sitemap.xml',
       // 'https://www.ssw.com.au/rules/sitemap.xml',
-      "https://www.ssw.com.au/ssw/sitemap.xml",
+      // Removed v1 sitemap as its a duplication of bunch of Next.js pages - coming from
+      // "https://www.ssw.com.au/ssw/sitemap.xml",
       "https://www.ssw.com.au/history/sitemap.xml",
     ],
   },
