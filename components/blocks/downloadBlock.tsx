4ecfe63d232bd024d584bb5a29fa272c3da2200a
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-03-21 12:12:02 +1100

    Adding placeholder image

--- components/blocks/downloadBlock.tsx
+++ components/blocks/downloadBlock.tsx
@@ -58,6 +58,8 @@ const Download = (data: Downloads) => {
     secondLink,
     secondLinkText,
   } = data;
+
+  const srcImage = img && !isImgBroken ? img : "/images/broken-image.png";
   return (
     <div className="col-span-1">
       <div className={classNames("py-3 text-black md:px-6")}>
@@ -69,14 +71,14 @@ const Download = (data: Downloads) => {
           )}
           data-tina-field={tinaField(data, "img")}
         >
-          {img && !isImgBroken && (
+          {img && (
             <Image
               onError={() => setIsImgBroken(true)}
-              src={img}
+              src={srcImage}
               alt={header}
               height={400}
               width={210}
-              style={{ width: "auto", height: "100%" }}
+              style={{ width: "100%", height: "auto" }}
             />
           )}
         </div>
