3ac1c5c60fd276272c8acfa403d630fe302b7312
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-03-25 14:37:50 +1100

    Adding mobile browser for the Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -54,10 +54,10 @@ export default defineConfig({
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
     // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    {
+      name: "Mobile Safari",
+      use: { ...devices["iPhone 13"] },
+    },
 
     /* Test against branded browsers. */
     // {
