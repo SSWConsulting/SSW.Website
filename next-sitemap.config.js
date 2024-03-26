a41f7040ed7f380c8770bee9d71444529c2ab24e
Harry Ross <hazross@hotmail.com>
2024-03-27 10:54:44 +1100

    removed problematic URLs from sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -8,21 +8,7 @@ module.exports = {
   generateRobotsTxt: true,
   output: "standalone",
   additionalPaths: async () => {
-    const otherURLs = [
-      "https://www.ssw.com.au/rules/",
-      "https://sswdigital.com/",
-      "https://sswsophie.com/sophiebot/",
-      "https://sugarlearning.com/",
-      "https://sswtimepro.com/",
-      "https://codeauditor.com/",
-      "https://sswhealthcheck.com/",
-      "https://smashingbarrier.com/",
-      "https://fireusergroup.com/",
-      "https://bettersoftwaresuggestions.com/",
-      "https://blog.ssw.com.au/",
-      "https://adamcogan.com/",
-      "https://tv.ssw.com/",
-    ];
+    const otherURLs = ["https://blog.ssw.com.au/", "https://tv.ssw.com/"];
 
     return otherURLs.map((url) => ({
       loc: url,
