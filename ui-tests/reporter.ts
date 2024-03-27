f968b701942ccf148c60db21ee8cd323e4f133bd
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-03-27 12:06:14 +1100

    Removing custom reporter file

--- ui-tests/reporter.ts
+++
@@ -1,8 +0,0 @@
-import type { FullResult, Reporter } from "@playwright/test/reporter";
-
-class MyReporter implements Reporter {
-  onEnd(result: FullResult) {
-    console.log(`${result.status}`);
-  }
-}
-export default MyReporter;
