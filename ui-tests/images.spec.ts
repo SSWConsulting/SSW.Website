b78bc530fdd571360b733043b8c317d6237ac500
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-04-02 12:31:22 +1100

    Fixing homepage broken image (#2363)
    * Fixing homepage broken image
    
    Affected Route: `/`
    Fixed : #2362
    
    Adding device sizes array for SIZE props.
    https://nextjs.org/docs/pages/api-reference/components/image#devicesizes
    
    * Adding mobile browser for the Playwright
    
    * Adding browser for mobile safari
    
    * Modifying tests for the mobile screen
    
    * Running the template on pr trigger
    
    * Adding env for the template
    
    * Adding more attempts
    
    * Adding waituntil prop
    
    * Adding chromium browser for mobile viewport
    
    * Undo the changes in the template
    
    * Changing attempts to 2 from 4
    
    * Updating the device name
    
    * Adding new pipeline to check images daily
    
    * Fixing syntax for the daily run
    
    * Adding checkout step
    
    * Adding a new step to extract env
    
    * echoing echo in a step
    
    * Adding shell to pwsh
    
    * Adding a new step to create issue
    
    * Adding checkout step
    
    * echoing test result
    
    * Adding guard to create an issue and removing custom reporter for the artifact
    
    * Extracting the output without reporter
    
    * Echoing test result in the logs
    
    * Changing the echo into write-ouput
    
    * Converting output into a string
    
    * Running test for the creating issue with right artifact
    
    * Fixing indentation in the issue template
    
    * Removing project and status from md
    
    * Removing custom reporter file
    
    * Removing trigger for PR and adding input for dispatch
    
    * Removing env step
    
    * Removing pull request trigger
    
    * Commenting out size prop
    
    * Update components/blocks/clientLogos.tsx
    
    Co-authored-by: Matt Wicks [SSW] <wicksipedia@users.noreply.github.com>
    
    * Adding a new step to add testpass value
    
    * Removing PR trigger
    
    * Adding create an issue in daily image test
    
    * Removing the invalid props for steps
    
    * Adding PR trigger for testing
    
    * Removing the create an issue to test the output variables
    
    * Adding the default value for the testpassed
    
    * Adding the chain condition within the block
    
    * Adding create issue step back in the flow
    
    * Matt's feedback
    
    * Removing the test pass step and related variables
    
    * Test run
    
    * Adding PR trigger for test
    
    * Adding typo for test
    
    * Adding seo-pr for test
    
    * Adding typo to test
    
    * Adding typo to test
    
    * Adding failure step to get the outcome
    
    * Passing props on failure
    
    * Adding continue on error
    
    * Adding testing output job
    
    * Changing the step name
    
    * removing single quote
    
    * Adding the step to output variable value
    
    * Changing the run value
    
    * Wrapping the value before publishing it
    
    * Adding output
    
    * echoing create issue value
    
    * Changing the value type
    
    * adding env to get the default value
    
    * Changing the condition
    
    * Adding true check for the job
    
    * removing double quote for true
    
    * adding output value for create issue
    
    * Adding new false for consistency
    
    * Adding extra condition
    
    * Wrapping with the variable name
    
    * removing condition
    
    * Adding extra echo
    
    * adding env for the github action
    
    * removing echo
    
    * adding quote to create issue variable
    
    * removing continue on true
    
    * Adding variable for the continue on error
    
    * failure condition for the job
    
    * Removing redundant steps
    
    * Updating the variables and cleanup
    
    * Removing error
    
    * removing pull request trigger
    
    * Adding step to checkout repo for the template
    
    ---------
    
    Co-authored-by: Matt Wicks [SSW] <wicksipedia@users.noreply.github.com>

--- ui-tests/images.spec.ts
+++ ui-tests/images.spec.ts
@@ -7,7 +7,7 @@ test("Images load successfully on index", async ({ page }) => {
     }
   });
 
-  const response = await page.goto("/", { waitUntil: "networkidle" });
+  const response = await page.goto("/", { waitUntil: "commit" });
   expect(response.ok()).toBeTruthy();
 
   // Taken from https://github.com/microsoft/playwright/issues/19277
@@ -32,7 +32,7 @@ test("Images load successfully on consulting page", async ({ page }) => {
   });
 
   const response = await page.goto("/consulting/react", {
-    waitUntil: "networkidle",
+    waitUntil: "commit",
   });
   expect(response.ok()).toBeTruthy();
 
