6f576ccaa28d7d7ea7459028b01ab5c44c78c7fd
andrewwaltosssw <andrewwaltos@ssw.com.au>
2024-04-05 18:04:27 +1100

    adding title for domain

--- components/blocks/mdxComponentRenderer.tsx
+++ components/blocks/mdxComponentRenderer.tsx
@@ -16,6 +16,7 @@ import { BookingButton } from "../bookingButton/bookingButton";
 import { ColorPaletteProps } from "./colorPalette";
 import { Content, ContentType } from "./content";
 import { SectionHeaderProps } from "./sectionHeader";
+import { DomainFromQueryProps } from "./domainFromQuery";
 
 const UtilityButton = dynamic(() =>
   import("../button/utilityButton").then((mod) => mod.UtilityButton)
@@ -258,7 +259,7 @@ export const componentRenderer: Components<{
   ColorPalette: ColorPaletteProps;
   SectionHeader: SectionHeaderProps;
   Content: ContentType;
-  DomainFromQuery: { showDomain: boolean };
+  DomainFromQuery: DomainFromQueryProps;
 }> = {
   AgreementForm: (props) => <AgreementForm data={props} />,
   ClientLogos: () => <ClientLogos />,
