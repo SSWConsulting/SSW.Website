01339357f70e961c598df2ebffc39dfb29a66e48
Harry Ross <hazross@hotmail.com>
2024-04-05 17:45:24 +1100

    added next/third-parties for GTM

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import { draftMode } from "next/headers";
 import { cache } from "react";
 import { client } from "../../.tina/__generated__/client";
@@ -8,7 +9,8 @@ const getData = cache(async () => {
   return await client.queries.productsIndexQuery();
 });
 
-export default async function ProductsIndex() {
+export async function generateMetadata(): Promise<Metadata> {
+  // TODO: Replicate logic from seo.tsx, export it from a different file so this can be reused
   const tinaProps = await getData();
 
   const seo = tinaProps.data.productsIndex.seo;
@@ -16,10 +18,22 @@ export default async function ProductsIndex() {
     seo.canonical = `${tinaProps.data.global.header.url}/products`;
   }
 
+  return {
+    title: seo.title,
+    description: seo.description,
+    alternates: {
+      canonical: seo.canonical,
+    },
+  };
+}
+
+export default async function ProductsIndex() {
+  const tinaProps = await getData();
+
   const { isEnabled } = draftMode();
 
   return isEnabled ? (
-    <ClientPage props={{ ...tinaProps, seo }} />
+    <ClientPage props={{ ...tinaProps }} />
   ) : (
     <ServerPage data={tinaProps.data} />
   );
