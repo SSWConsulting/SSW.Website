8d4391e9a706c246b926d862225a940bb00a8345
Harry Ross <hazross@hotmail.com>
2024-04-05 17:09:34 +1100

    simplified client + server pages

--- app/products/ClientPage.tsx
+++ app/products/ClientPage.tsx
@@ -1,37 +1,14 @@
 "use client";
 
-import { PageCard } from "@/components/blocks/pageCards";
 import { useTina } from "tinacms/dist/react";
-// import { Breadcrumbs } from "../../components/blocks/breadcrumbs";
-import { Container } from "../../components/util/container";
-// import { SEO } from "../../components/util/seo";
+import ServerPage from "./ServerPage";
 
-export default function RealPage({ props }) {
+export default function ClientPage({ props }) {
   const { data } = useTina({
     query: props.query,
     variables: props.variables,
     data: props.data,
   });
 
-  return (
-    <>
-      {/* <SEO seo={props.seo} /> */}
-      <Container className="mb-10 flex-1 pt-2">
-        {/* <Breadcrumbs path={"/products"} suffix="" title={"Products"} /> */}
-        {data.productsIndex.title && (
-          <h1 className="mb-0 py-0 text-3xl">{data.productsIndex.title}</h1>
-        )}
-        {data.productsIndex.subTitle && (
-          <h2 className="mb-4 text-base">{data.productsIndex.subTitle}</h2>
-        )}
-        <div className="flex flex-col md:flex-row">
-          <div className="grid w-full grid-cols-1 gap-2 lg:grid-cols-2">
-            {data.productsIndex.productsList?.map((product, index) => (
-              <PageCard page={product} key={index} />
-            ))}
-          </div>
-        </div>
-      </Container>
-    </>
-  );
+  return <ServerPage data={data} />;
 }
