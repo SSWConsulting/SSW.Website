2776b38c492617ff5dc528de14779c7095fa56b7
andrewwaltosssw <andrewwaltos@ssw.com.au>
2024-04-05 16:40:36 +1100

    adding back domain block

--- components/blocks/index.ts
+++ components/blocks/index.ts
@@ -68,6 +68,7 @@ export const pageBlocks: Template[] = [
   contentCardBlockSchema,
   customDownloadButtonSchema,
   customImageBlockSchema,
+  domainFromQuerySchema,
   downloadBlockSchema,
   dynamicColumnsSchema,
   eventBookingSchema,
