13557e3c8d172b5c2fdf74e05c6f3cdf75abc3a3
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-04-08 16:48:59 +1000

    💄Awards - New design
    Affected Route: `/company/awards`
    Fixed: #2296
    
    Screenshot: TODO

--- components/blocks/tripleColumnImageBlock.tsx
+++ components/blocks/tripleColumnImageBlock.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Key } from "react";
 import type { Template } from "tinacms";
+import { CustomLink } from "../customLink";
 import { isEmpty } from "../training/eventBooking";
 
 export type TripleColumnImageBlockProps = {
@@ -10,17 +11,21 @@ export type TripleColumnImageBlockProps = {
 export type ImgCardProps = {
   title: string;
   subTitle?: string;
+  link?: {
+    url: string;
+    label: string;
+  };
   altText: string;
   imageSrc: string;
-  height?: number | undefined;
-  width?: number | undefined;
+  height?: number;
+  width?: number;
 };
 
 export const TripleColumnImageBlock: React.FC<TripleColumnImageBlockProps> = ({
   imageList,
 }) => {
   return (
-    <div className="grid grid-cols-12 px-4 py-1">
+    <div className="grid grid-cols-12 gap-5 py-1">
       {imageList?.map((imageblock, index: Key) => {
         return <ImgCard {...imageblock} key={index} />;
       })}
@@ -35,15 +40,12 @@ const ImgCard = ({
   altText,
   height,
   width,
+  link,
 }: ImgCardProps) => {
   return (
-    <div className="col-span-12 p-4 md:col-span-4">
-      <h3 className="font-normal">{title}</h3>
-      <div className="h-4 text-xs font-normal text-ssw-gray">
-        {subTitle ?? ""}
-      </div>
+    <div className="relative col-span-12 flex flex-col justify-between rounded border-1 text-center md:col-span-3">
       {imageSrc && (
-        <div className="py-4">
+        <div className="flex justify-center py-4">
           <Image
             src={imageSrc}
             alt={altText ?? ""}
@@ -52,6 +54,15 @@ const ImgCard = ({
           />
         </div>
       )}
+      <div className="not-prose bottom-0 w-full bg-gray-50 p-4 text-center">
+        <h3 className="font-bold">{title}</h3>
+        {subTitle && <span className="py-2 text-gray-400">{subTitle}</span>}
+        {link?.url && (
+          <CustomLink href={link.url} className="font-bold text-sswRed">
+            <h3>{link.label}</h3>
+          </CustomLink>
+        )}
+      </div>
     </div>
   );
 };
@@ -81,6 +92,25 @@ export const tripleColumnImageBlockSchema: Template = {
           type: "string",
           label: "Sub Title",
           name: "subTitle",
+        },
+        {
+          type: "object",
+          label: "Link",
+          name: "link",
+          fields: [
+            {
+              type: "string",
+              label: "URL",
+              name: "url",
+              required: true,
+            },
+            {
+              type: "string",
+              label: "Label",
+              name: "label",
+              required: true,
+            },
+          ],
         },
         {
           type: "string",
