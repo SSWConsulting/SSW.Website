a74767baa0d9044b9bedfbadd2370b1709f38e34
John Xu [SSW] <johnxu@ssw.com.au>
2024-04-15 18:07:28 +0800

    Remove unused constant and type

--- components/blocks/colorPalette.tsx
+++ components/blocks/colorPalette.tsx
@@ -1,31 +1,21 @@
 import classNames from "classnames";
 import { Template } from "tinacms";
-import { productColors } from "../util/constants";
 
 const colors = require("plugins/platform-color-palette/colors")
 
-export type ColorPaletteProps = {
-  colorBlocks: Block[];
-};
-
-type Block = {
-  color: keyof typeof productColors;
-  text: string;
-};
-
-export const ColorPalette = (props: ColorPaletteProps) => {
+export const ColorPalette = () => {
   return (
     <div className="flex min-h-24 w-full flex-wrap">
-      {Object.entries<{ text: string, color: string }>(colors ?? {}).map(([k, v], i) => (
+      {Object.entries<{ text: string, color: string }>(colors ?? {}).map(([platformName, { text, color }], i) => (
         <div
           className={classNames(
-            `bg-platform-${k}`,
+            `bg-platform-${platformName}`,
             "flex flex-grow flex-col items-center justify-center text-white"
           )}
           key={i}
         >
-          <div>{v.text}</div>
-          <div>{v.color}</div>
+          <div>{text}</div>
+          <div>{color}</div>
         </div>
       ))}
     </div>
@@ -37,27 +27,10 @@ export const colorPaletteSchema: Template = {
   label: "Color Palette",
   fields: [
     {
-      type: "object",
-      label: "Color Blocks",
-      name: "colorBlocks",
-      list: true,
-      ui: {
-        itemProps: (props) => ({ label: props?.text }),
-      },
-      fields: [
-        {
-          type: "string",
-          label: "Color",
-          name: "color",
-          options: Object.keys(productColors).map((color) => color),
-          // options: productColors.map((color) => color),
-        },
-        {
-          type: "string",
-          label: "Text",
-          name: "text",
-        },
-      ],
-    },
+      type: "string",
+      name: "dummy",
+      label: "Dummy",
+      required: false
+    }
   ],
 };
