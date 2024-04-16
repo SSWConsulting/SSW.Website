379a43ad9d9742218973d3c7a8c6a5ea07d85a1e
Harry Ross <hazross@hotmail.com>
2024-04-17 08:15:56 +1000

    changed data fetching logic

--- app/products/[filename]/page.tsx
+++ app/products/[filename]/page.tsx
@@ -5,21 +5,21 @@ import ServerPage from "./ServerPage";
 export const dynamicParams = false;
 
 export async function generateStaticParams() {
-  let PageListData = await client.queries.productsConnection();
-  const allPagesListData = PageListData;
+  const pagesData = await client.queries.productsConnection();
+  // const allPagesListData = PageListData;
 
-  while (PageListData.data.productsConnection.pageInfo.hasNextPage) {
-    const lastCursor = PageListData.data.productsConnection.pageInfo.endCursor;
-    PageListData = await client.queries.productsConnection({
-      after: lastCursor,
-    });
+  // while (PageListData.data.productsConnection.pageInfo.hasNextPage) {
+  //   const lastCursor = PageListData.data.productsConnection.pageInfo.endCursor;
+  //   PageListData = await client.queries.productsConnection({
+  //     after: lastCursor,
+  //   });
 
-    allPagesListData.data.productsConnection.edges.push(
-      ...PageListData.data.productsConnection.edges
-    );
-  }
+  //   allPagesListData.data.productsConnection.edges.push(
+  //     ...PageListData.data.productsConnection.edges
+  //   );
+  // }
 
-  const pages = allPagesListData.data.productsConnection.edges.map((page) => ({
+  const pages = pagesData.data.productsConnection.edges.map((page) => ({
     filename: page.node._sys.filename,
   }));
 
