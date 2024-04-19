ce95cff1498cec04fd24e1863f47d06b99a5cddf
Harry Ross <hazross@hotmail.com>
2024-04-19 15:01:13 +1000

    added similar behaviour of pages

--- app/products/ServerPage.tsx
+++ app/products/ServerPage.tsx
@@ -1,5 +1,6 @@
 import { PageCard } from "@/components/blocks/pageCards";
 import { Container } from "@/components/util/container";
+import { tinaField } from "tinacms/dist/react";
 
 export default function ServerPage({ data }) {
   return (
@@ -8,10 +9,20 @@ export default function ServerPage({ data }) {
       <Container className="mb-10 flex-1 pt-2">
         {/* <Breadcrumbs path={"/products"} suffix="" title={"Products"} /> */}
         {data.productsIndex.title && (
-          <h1 className="mb-0 py-0 text-3xl">{data.productsIndex.title}</h1>
+          <h1
+            data-tina-field={tinaField(data.productsIndex, "title")}
+            className="mb-0 py-0 text-3xl"
+          >
+            {data.productsIndex.title}
+          </h1>
         )}
         {data.productsIndex.subTitle && (
-          <h2 className="mb-4 text-base">{data.productsIndex.subTitle}</h2>
+          <h2
+            data-tina-field={tinaField(data.productsIndex, "subTitle")}
+            className="mb-4 text-base"
+          >
+            {data.productsIndex.subTitle}
+          </h2>
         )}
         <div className="flex flex-col md:flex-row">
           <div className="grid w-full grid-cols-1 gap-2 lg:grid-cols-2">
