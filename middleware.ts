2f61c0c2c785a625e591015104cce2e06d08a859
Calinator444 <caleb.williams5247@gmail.com>
2024-05-01 16:22:30 +1000

    added temporary redirect using next.js to forward all traffic to images/newsletter-uploads to /newsletter-uploads

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,15 @@
+import { NextURL } from "next/dist/server/web/next-url";
 import { NextRequest, NextResponse } from "next/server";
 import { addNoIndexHeaders } from "./middleware/noIndex";
 
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
-
+  const url: NextURL = request.nextUrl.clone();
+  let path = url.pathname.toString();
+  if (path.startsWith("/images/newsletter-uploads")) {
+    url.pathname = path.replace("/images", "");
+    return NextResponse.redirect(url, { status: 307 });
+  }
   addNoIndexHeaders(request, response);
-
   return response;
 }
