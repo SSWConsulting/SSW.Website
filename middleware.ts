90fd0b72f5ac38e704ea0e7a332f94cae5bd97f4
Caleb Williams [SSW] <65635198+Calinator444@users.noreply.github.com>
2024-05-02 12:37:21 +1000

    Update middleware.ts
    Co-authored-by: Matt Wicks [SSW] <wicksipedia@users.noreply.github.com>

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { addNoIndexHeaders } from "./middleware/noIndex";
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
   const url: NextURL = request.nextUrl.clone();
-  let path = url.pathname.toString();
+  const path = url.pathname.toString();
   if (path.startsWith("/images/newsletter-uploads")) {
     url.pathname = path.replace("/images", "");
     return NextResponse.redirect(url, { status: 307 });
