6ccfbf0018fdf7f8366e8b442eecf0d617108d88
Andrew Waltos [SSW] <87786621+andrewwaltosssw@users.noreply.github.com>
2024-05-24 10:10:08 +1000

    Logging | Adding user agent to requests metrics (#2670)
    adding user agent logging on requests

--- appInsight-api.js
+++ appInsight-api.js
@@ -9,6 +9,13 @@ if (!!process.env.NEXT_PUBLIC_APP_INSIGHT_CONNECTION_STRING) {
     .setAutoCollectRequests(true)
     .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
     .setSendLiveMetrics(true);
+  appInsights.defaultClient.addTelemetryProcessor((envelope, context) => {
+    if (envelope.data.baseType === "RequestData") {
+      envelope.data.baseData.properties ??= {};
+      envelope.data.baseData.properties.userAgent =
+        context["http.ServerRequest"].headers["user-agent"];
+    }
+  });
   appInsights.start();
 } else {
   console.log("🚨 App Insights - Server Side logging is not turned on!");
