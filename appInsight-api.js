cd87a08f1ef3846ed47e01e3e1eb339675832b22
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-05-27 12:01:52 +1000

    Adding guard for the telemetry

--- appInsight-api.js
+++ appInsight-api.js
@@ -1,8 +1,8 @@
-if (!!process.env.NEXT_PUBLIC_APP_INSIGHT_CONNECTION_STRING) {
+if (process.env.NEXT_PUBLIC_APP_INSIGHT_CONNECTION_STRING) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
   let appInsights = require("applicationinsights");
 
   appInsights
-    // eslint-disable-next-line no-undef
     .setup(process.env.NEXT_PUBLIC_APP_INSIGHT_CONNECTION_STRING)
     .setAutoCollectConsole(true)
     .setAutoCollectExceptions(true)
@@ -12,12 +12,13 @@ if (!!process.env.NEXT_PUBLIC_APP_INSIGHT_CONNECTION_STRING) {
   appInsights.defaultClient.addTelemetryProcessor((envelope, context) => {
     if (envelope.data.baseType === "RequestData") {
       envelope.data.baseData.properties ??= {};
-      if (context["http.ServerRequest"].headers["user-agent"])
+      if (context["http.ServerRequest"]?.headers["user-agent"])
         envelope.data.baseData.properties.userAgent =
           context["http.ServerRequest"].headers["user-agent"];
     }
   });
   appInsights.start();
 } else {
+  // eslint-disable-next-line no-console
   console.log("🚨 App Insights - Server Side logging is not turned on!");
 }
