b0c0f1b2da9a0993ac967ec8f4d3797599954e1e
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-05-29 11:46:22 +1000

    Removing the middleware changes after a discussion with Matt W

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,10 @@
-import { addNoIndexHeaders } from "middleware/noIndex";
 import { NextRequest, NextResponse } from "next/server";
-
-const IGNORED_PATH_PREFIXES = [
-  "/api/",
-  "/_next/static/",
-  "/images/",
-  "/pubic/",
-];
+import { addNoIndexHeaders } from "./middleware/noIndex";
 
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
+
   addNoIndexHeaders(request, response);
-  const url = request.nextUrl.clone();
-
-  // Skip middleware for API routes and static files
-  if (IGNORED_PATH_PREFIXES.some((prefix) => url.pathname.startsWith(prefix))) {
-    return response;
-  }
-
-  const pathname = url.pathname.toLowerCase();
-
-  if (url.pathname !== pathname) {
-    url.pathname = pathname;
-    const rewrittenResponse = NextResponse.rewrite(url);
-    addNoIndexHeaders(request, rewrittenResponse);
-    return rewrittenResponse;
-  }
 
   return response;
 }
