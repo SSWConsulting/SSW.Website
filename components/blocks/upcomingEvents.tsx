2440d75ace7eceb9decfc402d75181f6f70817ac
andrewwaltosssw <andrewwaltos@ssw.com.au>
2024-06-12 10:29:46 +1000

    updating upcoming events block to use Tina data

--- components/blocks/upcomingEvents.tsx
+++ components/blocks/upcomingEvents.tsx
@@ -1,31 +1,40 @@
 "use client";
 
-import axios from "axios";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import type { Template } from "tinacms";
 import { tinaField } from "tinacms/dist/react";
 
 import { useFormatDates } from "../../hooks/useFormatDates";
-import { EventInfo } from "../../services/server/events";
 import { CustomLink } from "../customLink";
 import { EventsRelativeBox } from "../events/eventsRelativeBox";
+import client from "../../tina/__generated__/client";
+import { EventTrimmed } from "../filter/events";
 
 export const UpcomingEvents = ({ data }) => {
-  const [events, setEvents] = useState<EventInfo[]>([]);
+  const [events, setEvents] = useState<EventTrimmed[]>([]);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const fetchEvents = async () => {
       setLoading(true);
-      const res = await axios.get<EventInfo[]>("/api/get-upcoming-events", {
-        params: { top: data.numberOfEvents },
+      const today = new Date();
+      today.setHours(0, 0, 0, 0);
+      const events = await client.queries.getFutureEventsQuery({
+        fromDate: today.toISOString(),
+        top: data.numberOfEvents,
       });
       setLoading(false);
 
-      if (res?.status !== 200 || !res.data.length) return;
-
-      setEvents(res.data);
+      if (!events.data) return;
+      const mappedEvents = events.data.eventsCalendarConnection.edges.map(
+        (event) => ({
+          ...event.node,
+          startDateTime: new Date(event.node.startDateTime),
+          endDateTime: new Date(event.node.endDateTime),
+        })
+      );
+      setEvents(mappedEvents);
     };
 
     fetchEvents();
@@ -63,7 +72,7 @@ export const UpcomingEvents = ({ data }) => {
 };
 
 type UpcomingEventProps = {
-  event: EventInfo;
+  event: EventTrimmed;
 };
 
 const UpcomingEvent = ({ event }: UpcomingEventProps) => {
@@ -73,23 +82,23 @@ const UpcomingEvent = ({ event }: UpcomingEventProps) => {
 
   return (
     <CustomLink
-      href={event.Url.Url}
+      href={event.url}
       className="unstyled no-underline"
       key={event.id}
     >
       <article className="my-2.5 grid grid-cols-4 rounded border-1 border-gray-300 bg-white p-2 shadow hover:border-ssw-black dark:border-gray-700 dark:bg-gray-800">
         <div className="col-span-3 justify-center px-3">
           <h2 className="m-0 py-1 text-sm font-bold text-black">
-            {event.Title}
+            {event.title}
           </h2>
           <EventsRelativeBox
             relativeDate={relativeDate}
             formattedDate={formattedDate}
             dateFontSize="text-xs"
           />
-          {!!event.Presenter && (
+          {!!event.presenter && (
             <span className="mt-1 inline-flex items-center text-xs text-black">
-              {event.Presenter}
+              {event.presenter}
             </span>
           )}
         </div>
@@ -97,8 +106,8 @@ const UpcomingEvent = ({ event }: UpcomingEventProps) => {
           <div className="col-span-1 flex items-center justify-center sm:mr-2 sm:justify-end">
             <Image
               className={"rounded-md"}
-              src={event.Thumbnail.Url}
-              alt={`${event.Title} logo`}
+              src={event.thumbnail.url}
+              alt={`${event.title} logo`}
               width={90}
               height={90}
               sizes="75w (max-width: 768px) 25vw"
