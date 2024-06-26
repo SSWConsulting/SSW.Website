5cd18f876852eecfed4cee09f9925576c25ca3fc
Calinator444 <caleb.williams5247@gmail.com>
2024-06-26 14:50:09 +1000

    🔧 - Updated interest form to use global setting for registration of interest jotform - Fixed interest form using formId as page url

--- components/events/interestForm.tsx
+++ components/events/interestForm.tsx
@@ -1,18 +1,16 @@
+import globals from "@/content/global/index.json";
 import { Template } from "tinacms";
-import globals from "../../content/global/index.json";
 import { JotFormEmbed } from "../blocks";
 
 export const InterestForm = (props) => {
   return (
     <JotFormEmbed
-      jotFormId={`${eoiFormId}?pageUrl=${globals.newsletterJotFormId}`}
+      jotFormId={`${globals.registrationOfInterestJotFormId}?pageUrl=${props.pageUrl}`}
       buttonText={props.buttonText || "I am interested"}
       containerClass=""
     />
   );
 };
-
-const eoiFormId: string = "241638083078865";
 
 export const interestFormSchema: Template = {
   label: "Interest Form",
