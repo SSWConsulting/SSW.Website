6116e4f4192bb9bde7219abd667799a76ac7792b
Calinator444 <caleb.williams5247@gmail.com>
2024-06-27 09:53:15 +1000

    ➖ removed page url from tina schema (the jotform captures it now)

--- components/events/interestForm.tsx
+++ components/events/interestForm.tsx
@@ -5,7 +5,7 @@ import { JotFormEmbed } from "../blocks";
 export const InterestForm = (props) => {
   return (
     <JotFormEmbed
-      jotFormId={`${globals.registrationOfInterestJotFormId}?pageUrl=${props.pageUrl}`}
+      jotFormId={`${globals.registrationOfInterestJotFormId}`}
       buttonText={props.buttonText || "I am interested"}
       containerClass=""
     />
@@ -16,12 +16,6 @@ export const interestFormSchema: Template = {
   label: "Interest Form",
   name: "InterestForm",
   fields: [
-    {
-      type: "string",
-      label: "Page URL",
-      name: "pageUrl",
-      required: true,
-    },
     {
       type: "string",
       label: "Button Text",
