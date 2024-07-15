8f139574034e445385bdaeffc22ae430e5a1e88d
Calinator444 <caleb.williams5247@gmail.com>
2024-07-15 11:37:48 +1000

    reworded note for top statement in past events query to be more descriptive

--- app/api/events/past/route.ts
+++ app/api/events/past/route.ts
@@ -8,7 +8,10 @@ export async function GET(req: NextRequest) {
     return new Response("presenterName is required", { status: 400 });
   const eventClient = await client.queries.getPastEventsQuery({
     fromDate: new Date().toISOString(),
-    top: 999, // GraphQL will limit the count to 50 by default, but were client side filtering 600 + events
+    top: 999 /* GraphQL will limit the count to 50 by default, 
+    but were doing the filtering outside of the query because GraphQL has no "like" filter
+    https://tina.io/docs/graphql/queries/advanced/filter-documents/#operator-types
+    */,
   });
   const events = await getEventsWithClient(eventClient, presenterName);
   return new Response(JSON.stringify(events), { status: 200 });
