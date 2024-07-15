caff346e7ee6a9298e4d3bf348c0bf26e678ea05
Calinator444 <caleb.williams5247@gmail.com>
2024-07-15 11:34:30 +1000

    added note explaining high event limit for past events

--- app/api/events/past/route.ts
+++ app/api/events/past/route.ts
@@ -8,7 +8,7 @@ export async function GET(req: NextRequest) {
     return new Response("presenterName is required", { status: 400 });
   const eventClient = await client.queries.getPastEventsQuery({
     fromDate: new Date().toISOString(),
-    top: 999,
+    top: 999, // GraphQL will limit the count to 50 by default, but were client side filtering 600 + events
   });
   const events = await getEventsWithClient(eventClient, presenterName);
   return new Response(JSON.stringify(events), { status: 200 });
