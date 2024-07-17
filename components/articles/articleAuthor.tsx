31376770db2461e43d8aec1d62a2f160ae1a7e52
Calinator444 <caleb.williams5247@gmail.com>
2024-07-17 10:39:55 +1000

    Update article author card to use next js images

--- components/articles/articleAuthor.tsx
+++ components/articles/articleAuthor.tsx
@@ -9,7 +9,13 @@ type ArticleAuthorProps = {
 const ArticleAuthor = ({ name, position, image }: ArticleAuthorProps) => {
   return (
     <div className="flex flex-row items-center gap-2 py-1">
-      <img src={image} alt="User Photo" className="h-10 w-10 rounded-full" />
+      <Image
+        src={image}
+        alt="User Photo"
+        width={40}
+        height={40}
+        className="rounded-full"
+      />
       <div className="font-semibold uppercase">{name}</div>
       <div className="font-semibold">|</div>
       <div className="sm uppercase text-gray-500">{position}</div>
