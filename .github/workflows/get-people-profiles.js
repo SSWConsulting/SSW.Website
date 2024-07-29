c583d78b00abe4e9bc430e0c7367ff2b2908e476
Tylah Kapa <tylahkapa@gmail.com>
2024-07-30 08:45:36 +1000

    Save progress

---
+++ .github/workflows/get-people-profiles.js
@@ -0,0 +1,7 @@
+const matter = require('gray-matter');
+const fs = require('fs');
+const GetPeopleProfiles = ( {github, context, peopleDirectory, presentersDirectory} ) => {
+  
+}
+
+module.exports = GetPeopleProfiles;
