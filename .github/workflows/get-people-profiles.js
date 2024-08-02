8689ba995a381d19a563f353061c645e01525b8b
Tylah Kapa <tylahkapa@gmail.com>
2024-08-02 11:00:48 +1000

    Update relative path

--- .github/workflows/get-people-profiles.js
+++ .github/workflows/get-people-profiles.js
@@ -37,7 +37,7 @@ const GetPeopleProfiles = ( {
 
     if (fs.existsSync(peopleProfileImagePath)) {
       fs.copyFileSync(peopleProfileImagePath, `${websitePublicPath}/${profileImageRelativePath}`);
-      presenterJson.profileImg = profileImageRelativePath;
+      presenterJson.profileImg = `/${profileImageRelativePath}`;
     }
 
     presenterJson.presenter = presenterJson.presenter ?? {
