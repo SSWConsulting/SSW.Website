335d69f14af306efcc875cb146466a0a79db71b6
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-08-15 15:26:18 +1000

    Fixing the TinaCMS experience
    We were previously using DraftMode() from Next.js to integrate TinaCMS into our website, which significantly slowed down the entire TinaCMS experience. However, since the Tina team has now implemented app routing on their end, we can safely remove the draft mode from our website. This change should result in a much-improved user experience.

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,8 +1,6 @@
 import client from "@/tina/client";
 import { useSEO } from "hooks/useSeo";
 import { Metadata } from "next";
-import { draftMode } from "next/headers";
-import ProductsIndexContent from "./products-index";
 import ProductsIndexPreview from "./products-index-preview";
 
 const getData = async () => {
@@ -25,12 +23,5 @@ export async function generateMetadata(): Promise<Metadata> {
 
 export default async function ProductsIndex() {
   const tinaProps = await getData();
-
-  const { isEnabled: isPreview } = draftMode();
-
-  return isPreview ? (
-    <ProductsIndexPreview props={{ ...tinaProps }} />
-  ) : (
-    <ProductsIndexContent props={tinaProps.data} />
-  );
+  return <ProductsIndexPreview props={{ ...tinaProps }} />;
 }
