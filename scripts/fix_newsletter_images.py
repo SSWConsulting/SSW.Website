d99988fd3159aa812ffce4685418611beef302e9
Calinator444 <caleb.williams5247@gmail.com>
2024-08-22 17:12:57 +1000

    📜 Attached improved script for fixing links

--- scripts/fix_newsletter_images.py
+++ scripts/fix_newsletter_images.py
@@ -21,9 +21,15 @@ def image_hosted_locally(image_path: str)-> bool:
     return image_path.startswith('/') or  image_path.startswith(ssw_url) or image_path.startswith(ssw_url_alt) or (not image_path.startswith('http')) 
 
 def url_to_file_path(url: str) -> str:
-    url = url.lstrip("/")
-    url = url.lstrip(ssw_url)
-    url = url.lstrip(ssw_url_alt)
+    url = url.replace("%20", " ")
+    if(url.startswith("../")):
+        url = url.replace("../", "", 1)
+    if(url.startswith("/")):
+        url = url.replace("/", "", 1)
+    if url.startswith(ssw_url):
+        url = url.replace(ssw_url, "", 1)
+    if url.startswith(ssw_url_alt):
+        url = url.replace(ssw_url_alt, "", 1)
     return os.path.join(images_path, url).replace("/", "\\")
 
 def path_to_file(path: str) -> str:
@@ -41,7 +47,22 @@ def find_path_in_directory(cwd: str, next_paths):
     for dir in os.listdir(cwd):
         if dir.lower() == next_paths[0].lower():
             return find_path_in_directory(os.path.join(cwd, dir), next_paths[1:])
-
+        
+def fix_elements(element: str, attribute: str, soup: BeautifulSoup, contents: str, html_file_path : str):
+    for element in soup.find_all(element):
+        if(element.has_attr(attribute)):
+            attr = element[attribute]
+            if image_hosted_locally(attr):
+                full_path = url_to_file_path(attr)
+                alt_path = find_path_case_insensitive(full_path)
+                if alt_path != full_path:
+                    if alt_path is not None:
+                        alt_path = path_to_relative_url(alt_path)
+                        contents = contents.replace(attr, alt_path)
+                    else:
+                        print(f"WARNING: no local image found for {attr}")
+                        print("Check file: ", html_file_path)
+    return contents
 def main():
     for root, _, files in os.walk(base_directory):
         for file in files:
@@ -52,18 +73,8 @@ def main():
                 with open(html_file_path, 'r', encoding=encoding) as html_file:
                     contents = html_file.read()
                     soup = BeautifulSoup(contents, 'html.parser')
-                    for img_tag in soup.find_all('img'):
-                        if(img_tag.has_attr('src')):
-                            img_src = img_tag['src']
-                            if image_hosted_locally(img_src):
-                                full_path = url_to_file_path(img_src)
-                                alt_path = find_path_case_insensitive(full_path)
-                                if alt_path != full_path:
-                                    if alt_path is not None:
-                                        alt_path = path_to_relative_url(alt_path)
-                                        contents = contents.replace(img_src, alt_path)
-                                        print(f"src: {img_src} alt: {alt_path}")
+                    contents = fix_elements('img', 'src', soup, contents, html_file_path)
+                    contents = fix_elements('td', 'background', soup, contents, html_file_path)
                 with open(html_file_path, 'w', encoding=encoding) as html_file:
-                    print(contents)
                     html_file.write(contents)
 main()
