a94270e92fdee01524327eb0e0a50382e0dcaa34
Caleb Williams [SSW] <65635198+Calinator444@users.noreply.github.com>
2024-09-05 11:03:21 +1000

    2823 add missing filters events (#3065)
    * 🐛 Populated events filters using static fetching
    
    * 🔥 Removed unused EventCategories import
    
    * 🐛 Fixed event filters not working
    
    * ✨ Enabled event category filtering at the database layer
    
    * ⚡ Fixed static site generation for events
    
    * 🐛 Fixed wrong image displaying when toggling event filters
    
    * 🐛 Fixed links to canberra UG thumbnail
    
    * Removed console logs
    
    * 🐛 Updated brisbane 2018 opening party to match use existing opening party field
    
    * ✨ Added similar category fetching for events & massaged events into fitting categories
    
    * 🐛 Updated 2013 Mobile Development webinar to fit into Mobile category
    
    * 🐛 Fixed uncategorized events
    
    * 🔇 Removed console log
    
    * 💄 Fixed pop in animation
    
    * 🐛 Added missing set hook for event title
    
    * ♻️ Updated event category fetch to use existing variable for overriding page size in Tina
    
    * ➕ Added import statement for event max override
    
    * ➖ removed unused import statements
    
    * 🔍 Removed redundant fuzzy search for "Mobile Development" (Mobile development events were renamed => Mobile)
    
    * 🎨 Updated getFilterState to const because it is never reassigned
    
    * 🎨 Added jsonLd prop back to event
    
    * 🚨 Fixed linting errors
    
    * 🚨 Fixed build and errors
    
    * 🔒Comitting updated tina lock file
    
    * removed empty useEffect in component filter
    
    * Added parentheses around if in useEffect
    
    * ⚡ Improved event transition render logic
    
    * Fixed events flickering while fetching more events
    
    * 🐛 Fixed flickering when loading events
    
    * 🔇 Removed console logging
    
    * 🚨 Fixed linting errors
    
    * 🚨 Fixed linting issues with useReducer
    
    * 🐛 Fixed events flickering when page first loads
    
    * 🚨 Fixed linting warnings
    
    * 🚨 Removed explicit any from array comparison function
    
    * 📒 Added TODO for cleaning up event list
    
    * 📒 Added TODO for improving event transition performance
    
    ---------
    
    Co-authored-by: Calinator444 <caleb.williams5247@gmail.com>

--- hooks/useFetchEvents.ts
+++ hooks/useFetchEvents.ts
@@ -1,3 +1,6 @@
+import { EventFilterAllCategories } from "@/components/filter/FilterBlock";
+import { EVENTS_MAX_SIZE_OVERRIDE } from "@/services/server/getEvents";
+import { GetPastEventsQueryQuery } from "@/tina/types";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import client from "../tina/__generated__/client";
 
@@ -6,30 +9,65 @@ const PAGE_LENGTH = 10;
 export const FUTURE_EVENTS_QUERY_KEY = "futureEvents";
 export const PAST_EVENTS_QUERY_KEY = "pastEvents";
 
+const getCategoriesForFilter = (category: string) => {
+  if (!category) return undefined;
+  const categories = {
+    "Angular and React": ["Angular and React", "Angular", "React"],
+    Other: ["Other", "Non-English Courses"],
+  };
+  const lookup = categories[category];
+
+  return lookup ? lookup : [category];
+};
+
+const formatCategory = (category: string): string => {
+  {
+    const categoryReplacements = {
+      "Non-English Courses": "Other",
+    };
+    const lookup = categoryReplacements[category];
+
+    return lookup ? lookup : category;
+  }
+};
+
 const TODAY = new Date();
 TODAY.setHours(0, 0, 0, 0);
 
-export const getFutureEvents = async ({ pageParam }) => {
+export const getFutureEvents = async (
+  pageParam?: string,
+  category?: string,
+  calendarType?: string
+) => {
+  const categories = getCategoriesForFilter(category);
   const res = await client.queries.getFutureEventsQuery({
     fromDate: TODAY.toISOString(),
     top: PAGE_LENGTH,
     after: pageParam,
+    categories,
+    calendarType,
   });
-
   return res.data;
 };
 
-export const useFetchFutureEvents = () => {
+export type SelectedCategories = {
+  technology: string;
+  category: string;
+};
+
+export const useFetchFutureEvents = (filters: SelectedCategories) => {
   const { data, fetchNextPage, isFetchingNextPage, error, isLoading } =
     useInfiniteQuery({
-      queryKey: [FUTURE_EVENTS_QUERY_KEY],
-      queryFn: getFutureEvents,
+      queryKey: [
+        FUTURE_EVENTS_QUERY_KEY + filters.technology + filters.category,
+      ],
+      queryFn: ({ pageParam }) =>
+        getFutureEvents(pageParam, filters.technology, filters.category),
       initialPageParam: undefined,
       getNextPageParam: (lastPage) => {
         return lastPage.eventsCalendarConnection.pageInfo.endCursor;
       },
     });
-
   return {
     futureEvents:
       data?.pages.flat().flatMap((item) =>
@@ -37,6 +75,7 @@ export const useFetchFutureEvents = () => {
           ...edge.node,
           startDateTime: new Date(edge.node.startDateTime),
           endDateTime: new Date(edge.node.endDateTime),
+          category: formatCategory(edge.node.category),
         }))
       ) || [],
     error,
@@ -49,26 +88,32 @@ export const useFetchFutureEvents = () => {
   };
 };
 
-const getPastEvents = async ({ pageParam }) => {
+const getPastEvents = async (
+  pageParam: string,
+  category: string = undefined,
+  calendarType: string = undefined
+) => {
+  const categories = getCategoriesForFilter(category);
   const res = await client.queries.getPastEventsQuery({
     fromDate: TODAY.toISOString(),
     top: PAGE_LENGTH,
     before: pageParam,
+    categories,
+    calendarType,
   });
-
   return res.data;
 };
 
-export const useFetchPastEvents = (enabled: boolean) => {
+export const useFetchPastEvents = (filters: SelectedCategories) => {
   const { data, isLoading, fetchNextPage, isFetchingNextPage, error } =
     useInfiniteQuery({
-      queryKey: [PAST_EVENTS_QUERY_KEY],
-      queryFn: getPastEvents,
+      queryKey: [PAST_EVENTS_QUERY_KEY + filters.technology + filters.category],
+      queryFn: ({ pageParam }) =>
+        getPastEvents(pageParam, filters.technology, filters.category),
       initialPageParam: undefined,
       getNextPageParam: (lastPage) => {
         return lastPage.eventsCalendarConnection.pageInfo.endCursor;
       },
-      enabled,
     });
 
   return {
@@ -78,6 +123,7 @@ export const useFetchPastEvents = (enabled: boolean) => {
           ...edge.node,
           startDateTime: new Date(edge.node.startDateTime),
           endDateTime: new Date(edge.node.endDateTime),
+          category: formatCategory(edge.node.category),
         }))
       ) || [],
     error,
@@ -88,4 +134,69 @@ export const useFetchPastEvents = (enabled: boolean) => {
       data?.pages[data?.pages.length - 1].eventsCalendarConnection.pageInfo
         .hasPreviousPage,
   };
+};
+
+export const getEventsCategories = async () => {
+  const today: string = new Date().toISOString();
+
+  const pastEvents = await client.queries.getPastEventsQuery({
+    fromDate: today,
+    top: EVENTS_MAX_SIZE_OVERRIDE,
+  });
+  const upcomingEvents = await client.queries.getFutureEventsQuery({
+    fromDate: today,
+    top: EVENTS_MAX_SIZE_OVERRIDE,
+  });
+  const upcomingEventsData = upcomingEvents.data;
+
+  const pastEventsData = pastEvents.data;
+
+  formatCategories(upcomingEventsData.eventsCalendarConnection.edges);
+  formatCategories(pastEventsData.eventsCalendarConnection.edges);
+  const category = "calendarType";
+
+  const technology = "category";
+
+  const filterCategories: EventFilterAllCategories = {
+    past: {
+      technologies: aggregateByCategory(pastEventsData, technology),
+      categories: aggregateByCategory(pastEvents.data, category),
+    },
+    upcoming: {
+      technologies: aggregateByCategory(upcomingEvents.data, technology),
+      categories: aggregateByCategory(upcomingEvents.data, category),
+    },
+  };
+  return filterCategories;
+};
+
+const aggregateByCategory = (
+  events: GetPastEventsQueryQuery,
+  targetCategory: string
+): EventCategories => {
+  return events.eventsCalendarConnection.edges.reduce((occurences, event) => {
+    const category = event.node[targetCategory];
+    if (occurences[category]) {
+      occurences[category]++;
+    } else {
+      occurences[category] = 1;
+    }
+    return occurences;
+  }, {});
+};
+
+export type EventCategories = {
+  [key: string]: number;
+};
+
+const formatCategories = (edges) => {
+  for (let i = 0; i < edges.length; i++) {
+    edges[i] = {
+      ...edges[i],
+      node: {
+        ...edges[i].node,
+        category: formatCategory(edges[i].node.category),
+      },
+    };
+  }
 };
