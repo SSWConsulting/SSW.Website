91534233cd6f47c3a3e205a9d49d4575e6da8767
Calinator444 <caleb.williams5247@gmail.com>
2024-09-20 11:25:05 +1000

    ⚡ Added bundling for page router dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const withNextPluginPreval = createNextPluginPreval();
 
 /** @type {import('next').NextConfig} */
 const config = {
+  bundlePagesRouterDependencies: true,
   poweredByHeader: false,
   images: {
     deviceSizes: [384, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
