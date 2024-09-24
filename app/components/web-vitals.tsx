02d3a0cfab4c71846f0be8b58363c0ed0c0b75f9
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-09-24 12:52:40 +1000

    Adding web vitals as client component

---
+++ app/components/web-vitals.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useAppInsightsContext } from "@microsoft/applicationinsights-react-js";
+import { usePathname } from "next/navigation";
+import { useReportWebVitals } from "next/web-vitals";
+
+export const WebVitals = () => {
+  const appInsights = useAppInsightsContext();
+  const pathname = usePathname();
+
+  useReportWebVitals((metric) => {
+    switch (metric.name) {
+      case "TTFB":
+      case "FCP":
+      case "LCP":
+      case "FID":
+      case "CLS":
+      case "INP":
+        appInsights.trackMetric(
+          { name: metric.name, average: metric.value },
+          { page: `${pathname}` }
+        );
+        break;
+    }
+  });
+  return <></>;
+};
