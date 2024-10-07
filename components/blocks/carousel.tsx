9d92f42cd77808ca6ccc54e6a8b643d29c2ec7ae
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-10-08 10:24:24 +1100

    mobile - reducing the size on mobile for banner image

--- components/blocks/carousel.tsx
+++ components/blocks/carousel.tsx
@@ -105,7 +105,7 @@ const CarouselItemImage = (props: CarouselItemImageProps) => {
         alt={label}
         height={388}
         width={1080}
-        sizes="100vw"
+        sizes="(max-width: 768px) 20vw, 100vw"
         priority={index === 0}
       />
       {/* `legend` required so that the carousel works properly */}
