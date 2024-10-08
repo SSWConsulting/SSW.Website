5a055390a59b455a2ba7690c7e8fa5b243173622
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-10-08 13:16:30 +1100

    Removing the comment

--- components/blocks/carousel.tsx
+++ components/blocks/carousel.tsx
@@ -105,7 +105,7 @@ const CarouselItemImage = (props: CarouselItemImageProps) => {
         alt={label}
         height={388}
         width={1080}
-        sizes="100vw" // for mobile, it will load 20% of the viewport width as it is not visible on mobile
+        sizes="100vw"
         priority={index === 0}
       />
       {/* `legend` required so that the carousel works properly */}
