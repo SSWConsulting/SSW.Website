406faed414e801f8e1bcc1ee08066bba0797e7c8
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-10-09 16:56:36 +1100

    Fixing layout shifting issue on Homepage (#3198)
    * Fixing layout shifting issue on Homepage
    
    Issue: #3156
    
    Screenshot:TODO
    
    * Adding max size for the mobile view
    
    * Adding memoized card logic to reduce the contentful paint matrics
    
    * Removing profiler from the component monitoring

--- components/blocks/carousel.tsx
+++ components/blocks/carousel.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import * as React from "react";
@@ -6,16 +5,13 @@ import { tinaField } from "tinacms/dist/react";
 
 import type { Template } from "tinacms";
 
-import dynamic from "next/dynamic";
 import useIsMobile from "../../hooks/useIsMobile";
 import { Container } from "../util/container";
 import { Section } from "../util/section";
 
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-const CarouselImplementation = dynamic(() =>
-  import("react-responsive-carousel").then((module) => module.Carousel)
-);
+import { Carousel as CarouselImplementation } from "react-responsive-carousel";
 
 export const Carousel = ({ data }) => {
   const router = useRouter();
@@ -51,7 +47,6 @@ export const Carousel = ({ data }) => {
         className={/* eslint-disable-line */ "aspect-carousel w-full"}
         data-tina-field={tinaField(data, carouselBlock.delay)}
       >
-        {/* @ts-expect-error next/dynamic */}
         <CarouselImplementation
           autoPlay={true}
           infiniteLoop={true}
@@ -105,7 +100,7 @@ const CarouselItemImage = (props: CarouselItemImageProps) => {
         alt={label}
         height={388}
         width={1080}
-        sizes="100vw"
+        sizes="(max-width: 640px) 50vw, 100vw"
         priority={index === 0}
       />
       {/* `legend` required so that the carousel works properly */}
--- components/blocks/serviceCards.tsx
+++ components/blocks/serviceCards.tsx
@@ -5,9 +5,11 @@ const Image = dynamic(() => import("next/image"), { ssr: false });
 import type { Template } from "tinacms";
 
 import dynamic from "next/dynamic";
+import React, { useMemo } from "react";
 import { CustomLink } from "../customLink";
 import { Container } from "../util/container";
 import { Section } from "../util/section";
+import BigCardContent from "./bigCardContent";
 
 const bgColor = {
   red: "bg-sswRed",
@@ -57,6 +59,8 @@ const Label = ({ text, schema, cardLabel }) => {
 };
 
 const BigCards = ({ title, cards, schema }) => {
+  const memoizedCards = useMemo(() => cards, [cards]);
+
   return (
     <>
       <Label
@@ -68,54 +72,14 @@ const BigCards = ({ title, cards, schema }) => {
         role="list"
         className="mt-5 grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-4"
       >
-        {cards.map((card, index) => (
-          <li
+        {memoizedCards.map((card, index) => (
+          <BigCardContent
             key={card.title}
-            className={`col-span-1 flex flex-col divide-y divide-gray-200 text-center shadow ${
-              bgColor[card.color]
-            } hover:opacity-80`}
-          >
-            <CustomLink
-              href={card.link ?? ""}
-              className="unstyled flex grow text-left text-white"
-            >
-              <div className="flex grow flex-col">
-                {card?.imgSrc && (
-                  <div className="absolute flex-1 self-end">
-                    <Image
-                      className="opacity-50"
-                      src={card.imgSrc ?? ""}
-                      width="100"
-                      height="100"
-                      sizes="20vw"
-                      alt={`Icon for ${card.title}`}
-                      loading="lazy"
-                    />
-                  </div>
-                )}
-                <div className="relative flex grow flex-col p-8">
-                  <h3
-                    className="flex pb-3 text-2xl font-thin lg:pt-8"
-                    data-tina-field={tinaField(
-                      schema.bigCards[index],
-                      serviceCards.bigCards.title
-                    )}
-                  >
-                    {card.title}
-                  </h3>
-                  <div className="grow"></div>
-                  <p
-                    data-tina-field={tinaField(
-                      schema.bigCards[index],
-                      serviceCards.bigCards.description
-                    )}
-                  >
-                    {card.description}
-                  </p>
-                </div>
-              </div>
-            </CustomLink>
-          </li>
+            card={card}
+            index={index}
+            schema={schema}
+            bgColor={bgColor}
+          />
         ))}
       </ul>
     </>
