9bcbd38a65b457096b994d37644e5566f37997b0
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-10-15 16:37:24 +1100

    fixing filepath for the static pages

--- app/consulting/video-production/[filename]/page.tsx
+++ app/consulting/video-production/[filename]/page.tsx
@@ -26,7 +26,7 @@ export async function generateStaticParams() {
 
   const pages = allPagesListData.data.videoProductionConnection.edges.map(
     (page) => ({
-      params: { filename: page.node._sys.filename },
+      filename: page.node._sys.filename,
     })
   );
 
