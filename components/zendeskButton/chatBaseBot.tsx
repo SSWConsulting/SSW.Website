919709564194616a0b338a89e76c7a0af4098e60
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-10-17 10:56:19 +1100

    loading the chatbot on the pure client side

--- components/zendeskButton/chatBaseBot.tsx
+++ components/zendeskButton/chatBaseBot.tsx
@@ -1,19 +1,38 @@
-import Script from "next/script";
+"use client";
+import { useEffect } from "react";
 
 const chatBaseBotId = process.env.NEXT_PUBLIC_CHATBASE_BOT_ID;
 
 const ChatBaseBot = () => {
-  return (
-    chatBaseBotId && (
-      <Script
-        src="https://www.chatbase.co/embed.min.js"
-        id={chatBaseBotId}
-        async
-        defer
-        strategy="lazyOnload"
-      />
-    )
-  );
+  useEffect(() => {
+    const loadChatBase = () => {
+      if (chatBaseBotId) {
+        const script = document.createElement("script");
+        script.src = "https://www.chatbase.co/embed.min.js";
+        script.id = chatBaseBotId;
+        script.async = true;
+        script.defer = true;
+        document.body.appendChild(script);
+      }
+    };
+
+    if ("requestIdleCallback" in window) {
+      requestIdleCallback(() => {
+        loadChatBase();
+      });
+    } else {
+      setTimeout(loadChatBase, 3000);
+    }
+
+    return () => {
+      const existingScript = document.getElementById(chatBaseBotId);
+      if (existingScript) {
+        existingScript.remove();
+      }
+    };
+  }, []);
+
+  return null;
 };
 
 export default ChatBaseBot;
