1478eb207c998fd54dee86c00c6477b2dc383bbf
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-10-22 16:28:28 +1100

    fixing the min height for the breadcrumb

--- app/company/clients/[filename]/index.tsx
+++ app/company/clients/[filename]/index.tsx
@@ -17,7 +17,7 @@ export default function CompanyPage({ tinaProps, props }) {
     <>
       {data.caseStudy.seo?.showBreadcrumb === null ||
         (data.caseStudy.seo?.showBreadcrumb && (
-          <Section className="mx-auto w-full max-w-9xl px-8 py-0">
+          <Section className="mx-auto min-h-24 w-full max-w-9xl px-8 py-5 md:min-h-16">
             <Breadcrumbs
               path={removeExtension(props.variables.relativePath)}
               suffix={data.global.breadcrumbSuffix}
