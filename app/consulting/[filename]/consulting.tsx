44e7722b3ed07362815fe8ff1616ec65be74c620
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-10-23 15:25:06 +1100

    Adding breadcrumb

--- app/consulting/[filename]/consulting.tsx
+++ app/consulting/[filename]/consulting.tsx
@@ -1,5 +1,21 @@
 "use client";
 
-export default function Consulting() {
-  return <></>;
+import { Section } from "@/components/util/section";
+import { removeExtension } from "@/services/client/utils.service";
+import { Breadcrumbs } from "app/components/breadcrumb";
+
+export default function Consulting({ props, tinaProps }) {
+  const { data } = tinaProps;
+  return (
+    <>
+      <Section className="mx-auto min-h-24 w-full max-w-9xl px-8 py-5 md:min-h-16">
+        <Breadcrumbs
+          path={removeExtension(props.variables.relativePath)}
+          suffix={data.global.breadcrumbSuffix}
+          title={data.consulting.seo?.title}
+          seoSchema={data.consulting.seo}
+        />
+      </Section>
+    </>
+  );
 }
