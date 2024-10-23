05f61d55e1e1f50e61bdccea66a07e8f19b10ef9
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-10-23 11:44:26 +1100

    removing everything from the page

--- app/consulting/[filename]/consulting.tsx
+++ app/consulting/[filename]/consulting.tsx
@@ -1,32 +1,5 @@
 "use client";
 
-import { BookingButton, BuiltOnAzure } from "@/components/blocks";
-import { Booking } from "@/components/blocks/booking";
-import { Section } from "@/components/util/section";
-import { removeExtension } from "@/services/client/utils.service";
-import { Breadcrumbs } from "app/components/breadcrumb";
-
-export default function Consulting({ tinaProps, props }) {
-  const { data } = tinaProps;
-  return (
-    <>
-      <Section className="mx-auto min-h-24 w-full max-w-9xl px-8 py-5 md:min-h-16">
-        <Breadcrumbs
-          path={removeExtension(props.variables.relativePath)}
-          suffix={data.global.breadcrumbSuffix}
-          title={data.consulting.seo?.title}
-          seoSchema={data.consulting.seo}
-        />
-      </Section>
-      <Section className="w-full" color="black">
-        <Booking {...data.consulting.booking}>
-          <BookingButton />
-        </Booking>
-      </Section>
-
-      <Section>
-        <BuiltOnAzure data={{ backgroundColor: "default" }} />
-      </Section>
-    </>
-  );
+export default function Consulting() {
+  return <></>;
 }
