746710fc88e520d14ef4a82365a1a2b61e511256
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-10-29 11:04:40 +1100

    Adding react query hydration

--- app/providers/query-provider.tsx
+++ app/providers/query-provider.tsx
@@ -1,19 +1,36 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactNode, useState } from "react";
+import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
+import { ReactNode } from "react";
 
 const FIVE_MINS = 1000 * 60 * 5;
 
-export function QueryProvider({ children }: { children: ReactNode }) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: { queries: { staleTime: FIVE_MINS } },
-      })
-  );
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: { queries: { staleTime: FIVE_MINS } },
+  });
+}
+
+let clientQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (typeof window === "undefined") {
+    // Server: always make a new query client
+    return makeQueryClient();
+  } else {
+    // Browser: make a new query client if we don't already have one
+    if (!clientQueryClient) clientQueryClient = makeQueryClient();
+    return clientQueryClient;
+  }
+}
+
+export const QueryProvider = ({ children }: { children: ReactNode }) => {
+  const queryClient = getQueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
+    </QueryClientProvider>
   );
-}
+};
