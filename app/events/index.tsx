90047b86cc74e95478f9d5323a9f80a87284d4fc
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-10-30 12:04:03 +1100

    removing hydration boundary to fix the delayed display

--- app/events/index.tsx
+++ app/events/index.tsx
@@ -4,7 +4,6 @@ import { Blocks } from "@/components/blocks-renderer";
 import { componentRenderer } from "@/components/blocks/mdxComponentRenderer";
 import { EventsFilter } from "@/components/filter/events";
 import { Container } from "@/components/util/container";
-import { HydrationBoundary } from "@tanstack/react-query";
 import { QueryProvider } from "app/providers/query-provider";
 import { useSearchParams } from "next/navigation";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
@@ -19,30 +18,28 @@ export default function EventsIndexPage({ props, tinaProps }) {
 
   return (
     <QueryProvider>
-      <HydrationBoundary state={props.dehydratedState}>
-        <Container size="small">
-          <div className="md:flex md:flex-row">
-            <h1 className="md:mr-12 md:shrink-0 md:basis-64">SSW Events</h1>
-            <div className="mt-5 min-w-0 max-w-full shrink grow overflow-auto whitespace-normal break-all pb-1 pt-15 md:mr-12 md:shrink-0 md:basis-64">
-              <TinaMarkdown
-                content={data.eventsIndex.preface}
-                components={componentRenderer}
-              />
-            </div>
+      <Container size="small">
+        <div className="md:flex md:flex-row">
+          <h1 className="md:mr-12 md:shrink-0 md:basis-64">SSW Events</h1>
+          <div className="mt-5 min-w-0 max-w-full shrink grow overflow-auto whitespace-normal break-all pb-1 pt-15 md:mr-12 md:shrink-0 md:basis-64">
+            <TinaMarkdown
+              content={data.eventsIndex.preface}
+              components={componentRenderer}
+            />
           </div>
-          <EventsFilter
-            filterCategories={filterCategories}
-            sidebarBody={data.eventsIndex.sidebarBody}
-            defaultToPastTab={defaultToPastTab}
-            futureEvents={futureEventsData}
-            pastEvents={pastEventsData}
-          />
-        </Container>
-        <Blocks
-          prefix="EventsIndexAfterEvents"
-          blocks={data.eventsIndex.afterEvents}
+        </div>
+        <EventsFilter
+          filterCategories={filterCategories}
+          sidebarBody={data.eventsIndex.sidebarBody}
+          defaultToPastTab={defaultToPastTab}
+          futureEvents={futureEventsData}
+          pastEvents={pastEventsData}
         />
-      </HydrationBoundary>
+      </Container>
+      <Blocks
+        prefix="EventsIndexAfterEvents"
+        blocks={data.eventsIndex.afterEvents}
+      />
     </QueryProvider>
   );
 }
