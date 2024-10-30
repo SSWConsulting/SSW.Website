7f0b3cd38bc26004bf6537d77471acb74024763b
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-10-30 13:15:02 +1100

    removing the query client from being rendered on client side

--- app/providers/query-provider.tsx
+++ app/providers/query-provider.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ReactNode, useMemo } from "react";
