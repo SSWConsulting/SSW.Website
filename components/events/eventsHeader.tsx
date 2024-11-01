6218f5d8bc887a94b0abf418e7f161a0093325ad
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2024-11-01 15:43:07 +1100

    Events page - Largest Contentful Paint
    Affected route: `/events/*`

--- components/events/eventsHeader.tsx
+++ components/events/eventsHeader.tsx
@@ -1,27 +1,19 @@
-import Image, { getImageProps } from "next/image";
-import { getBackgroundImage } from "../../helpers/images";
+import Image from "next/image";
 import { Section } from "../util/section";
 
 const EventsHeader = ({ data }) => {
-  const {
-    props: { srcSet },
-  } = getImageProps({
-    src: data?.heroBackground || "/images/polygonBackground.png",
-    alt: "Events header background",
-    height: 724,
-    width: 1728,
-  });
-  const backgroundImage = getBackgroundImage(srcSet);
-
   return (
-    <Section
-      className="flex h-102 items-center justify-center border-b-8 border-sswRed bg-white bg-cover bg-no-repeat"
-      style={{
-        backgroundImage,
-      }}
-    >
+    <Section className="relative flex h-102 items-center justify-center border-b-8 border-sswRed">
+      <Image
+        src={data?.heroBackground || "/images/polygonBackground.png"}
+        alt={data?.altText || "Events header background"}
+        layout="fill"
+        className="bg-contain bg-no-repeat"
+        sizes="(max-width: 768px) 50vw, 1728px"
+        priority
+      />
       {data?.imgOverlay && (
-        <div className="flex max-w-2xl lg:max-w-3xl">
+        <div className="z-10 flex max-w-2xl lg:max-w-3xl">
           <Image
             src={data.imgOverlay}
             alt={data?.altText}
