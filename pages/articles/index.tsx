c066a2f2c9f1314a69c6c8ec77348d64117aab77
Calinator444 <caleb.williams5247@gmail.com>
2024-11-11 15:44:00 +1100

    disable eslint rules

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -25,10 +25,10 @@ export const ARTICLES_QUERY_KEY = "articlesKey";
 export default function ArticlesIndexPage(props) {
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const prop: any = {};
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
+    // eslint-disable-next-line no-console
     console.log(prop.data.nonexistent);
   }, []);
   const { data } = useTina({
