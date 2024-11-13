47ab90522edab9e78e33bcff7f8acfba3367dd11
Calinator444 <calinator666@gmail.com>
2024-11-14 10:59:57 +1100

    🚨 Fixed linting warning

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { ErrorPage } from "@/components/util/error/error";
-import LayoutWrapper from "./layout-wrapper";
+
+import { useEffect } from "react";
 
 // Error boundaries must be Client Components
 
@@ -10,6 +10,9 @@ export default function GlobalError({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  useEffect(() => {
+    console.error(error);
+  });
   return (
     <html>
       <body>
