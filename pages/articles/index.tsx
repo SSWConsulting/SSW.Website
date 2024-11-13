eaeb39926d0d7077b1a110fbc9f29fc8c0d0ad36
Calinator444 <calinator666@gmail.com>
2024-11-13 15:13:51 +1100

    🧪 throwing error inside of articles base page

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -8,6 +8,7 @@ import {
 } from "@tanstack/react-query";
 import classNames from "classnames";
 import { TODAY } from "hooks/useFetchEvents";
+import { useEffect } from "react";
 import { tinaField, useTina } from "tinacms/dist/react";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
 import ArticlesHeader from "../../components/articles/articlesHeader";
@@ -18,7 +19,6 @@ import { Section } from "../../components/util/section";
 import { SEO } from "../../components/util/seo";
 import { getArticles } from "../../hooks/useFetchArticles";
 import { removeExtension } from "../../services/client/utils.service";
-
 export const ARTICLES_QUERY_KEY = "articlesKey";
 
 export default function ArticlesIndexPage(props) {
@@ -26,6 +26,10 @@ export default function ArticlesIndexPage(props) {
     data: props.data,
     query: props.query,
     variables: props.variables,
+  });
+
+  useEffect(() => {
+    throw new Error("Error boundary test");
   });
   const { dehydratedState } = props;
   return (
