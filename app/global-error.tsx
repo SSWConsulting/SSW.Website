2b288f6763d6c9541a7eec946e76cb656535a2a1
Calinator444 <calinator666@gmail.com>
2024-11-14 12:56:18 +1100

    📈 added app insights logging for errors in root layout

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,26 +1,35 @@
 "use client";
-
+import { ErrorPage } from "@/components/util/error-page";
+import { AppInsightsProvider } from "@/context/app-insight-client";
+import { useAppInsightsContext } from "@microsoft/applicationinsights-react-js";
 import { useEffect } from "react";
-
+import LayoutWrapper from "./layout-wrapper";
 // Error boundaries must be Client Components
+const appInsights = useAppInsightsContext();
 
-export default function GlobalError({
-  error,
-}: {
-  error: Error & { digest?: string };
-  reset: () => void;
-}) {
+export default function GlobalError({ error }: { error: Error }) {
+  const errorDetails = error.stack || error.message;
   useEffect(() => {
     // eslint-disable-next-line no-console
     console.error(error);
+    appInsights.trackException({
+      exception: error,
+      properties: {
+        Request: `GET /${window?.location?.pathname || "unknown"}`,
+        Type: "ErrorBoundary",
+        ErrorInfo: errorDetails,
+      },
+    });
   });
+
   return (
     <html>
       <body>
-        <h1>Error occurred in layout</h1>
-        {/* <LayoutWrapper>
-          <ErrorPage details={error.message} />
-        </LayoutWrapper> */}
+        <AppInsightsProvider>
+          <LayoutWrapper>
+            <ErrorPage details={errorDetails}></ErrorPage>
+          </LayoutWrapper>
+        </AppInsightsProvider>
       </body>
     </html>
   );
