12b7e3663ce51f7ad6cf51b5d507d1bca8a60756
Calinator444 <calinator666@gmail.com>
2024-11-14 17:39:23 +1100

    removed unused imports in global-error

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,8 +1,5 @@
 "use client";
 import { ErrorPage } from "@/components/util/error-page";
-import { AppInsightsProvider } from "@/context/app-insight-client";
-import GlobalErrorHandler from "./components/global-error-handler";
-import LayoutWrapper from "./layout-wrapper";
 // Error boundaries must be Client Components
 
 export default function GlobalError({ error }: { error: Error }) {
