ccf451937d6a437eae7febc958aab636286f8764
Calinator444 <calinator666@gmail.com>
2024-11-15 18:43:46 +1100

    Revert "🧪 Testing throwing an error in layout.tsx"
    This reverts commit 6f20e362eac40e974b9ecacf561a68f60a9e7d3c.

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,6 @@ import client from "../tina/__generated__/client";
 import { MenuWrapper } from "./components/MenuWrapper";
 import PageLayout from "./components/page-layout";
 import { WebVitals } from "./components/web-vitals";
-import ErrorThrower from "./error-thrower";
 import { LiveStream } from "./live-steam-banner/live-stream";
 import { DEFAULT } from "./meta-data/default";
 import { getMegamenu, MegaMenuProps } from "./utils/get-mega-menu";
@@ -79,7 +78,6 @@ export default async function RootLayout({
           <AppInsightsProvider>
             <WebVitals />
             {children}
-            <ErrorThrower />
           </AppInsightsProvider>
           {/* </Theme> */}
         </PageLayout>
