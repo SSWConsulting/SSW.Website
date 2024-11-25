0d76ed4460e55a544ae2e4022115996912ae3564
Calinator444 <calinator666@gmail.com>
2024-11-25 15:49:57 +1100

    ✨ Made breadcrumb default to title of the current page

--- components/blocks/breadcrumbs/breadcrumbs.schema.tsx
+++ components/blocks/breadcrumbs/breadcrumbs.schema.tsx
@@ -1,4 +1,5 @@
-import { Template } from "tinacms";
+import React from "react";
+import { Button, Input, Template, wrapFieldsWithMeta } from "tinacms";
 
 export const BreadcrumbSchema: Template = {
   name: "breadcrumbs",
@@ -11,6 +12,29 @@ export const BreadcrumbSchema: Template = {
       type: "string",
       label: "Final Breadcrumb",
       name: "finalBreadcrumb",
+      ui: {
+        component: wrapFieldsWithMeta(({ form, input }) => {
+          const formState = form.getState();
+          return (
+            <div>
+              <div className="flex gap-4">
+                <Input {...input} />
+                <Button
+                  variant="primary"
+                  onClick={() => {
+                    input.onChange(formState.values.seo.title);
+                  }}
+                >
+                  Reset
+                </Button>
+              </div>
+              <span className="mt-2 block whitespace-normal font-sans text-xs font-bold text-gray-400">
+                Note: this field defaults to the title of the current page
+              </span>
+            </div>
+          );
+        }),
+      },
       description: "The final breadcrumb in the list",
     },
   ],
