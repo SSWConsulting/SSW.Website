83d59db4605fa863f490300990902c4ad0759089
Calinator444 <calinator666@gmail.com>
2024-11-25 12:38:54 +1100

    🎨 Added new background color type for new consulting pages

--- components/util/constants/styles.tsx
+++ components/util/constants/styles.tsx
@@ -1,6 +1,7 @@
 export const sectionColors = {
   default: "bg-white text-black",
   lightgray: "bg-gray-100 text-black",
+  darkgray: "bg-gray-950 text-gray-300",
   red: "bg-sswRed text-white",
   black: "bg-black text-white",
 };
@@ -8,6 +9,7 @@ export const sectionColors = {
 export enum SectionColor {
   Default = "default",
   LightGray = "lightgray",
+  DarkGray = "darkgray",
   Red = "red",
   Black = "black",
 }
