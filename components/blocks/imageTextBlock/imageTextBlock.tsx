6a2a9049a35536f4794e6472915b8d09487d1b6b
isaaclombardssw <isaaclombard@ssw.com.au>
2024-11-29 12:53:33 +1100

    adding in some more visual editing

--- components/blocks/imageTextBlock/imageTextBlock.tsx
+++ components/blocks/imageTextBlock/imageTextBlock.tsx
@@ -1,5 +1,7 @@
 import { Container } from "@/components/util/container";
 import Image from "next/image";
+import { tinaField } from "tinacms/dist/react";
+import { TinaMarkdown } from "tinacms/dist/rich-text";
 import { backgroundOptions } from "../sharedTinaFields/colourOptions/blockBackgroundOptions";
 import { IconLabel } from "./iconLabel";
 import { ListItem } from "./listItem";
@@ -17,8 +19,15 @@ export const ImageTextBlock = ({ data }) => {
       <Container className="mx-auto flex align-top">
         <div className="w-full">
           {data.topLabel && <IconLabel data={data.topLabel} />}
-          {data.isH1 ? <h1>{data.heading}</h1> : <h2>{data.heading}</h2>}
-          <p>{data.description}</p>
+          {data.isH1 ? (
+            <h1 data-tina-field={tinaField(data, "heading")}>{data.heading}</h1>
+          ) : (
+            <h2 data-tina-field={tinaField(data, "heading")}>{data.heading}</h2>
+          )}
+          <TinaMarkdown
+            content={data.description}
+            data-tina-field={tinaField(data, "description")}
+          />
           {data.chips && <PillGroup data={data.chips} />}
           <div
             className={`grid ${data.featureColumns?.twoColumns ? "grid-cols-2" : "grid-cols-1"}`}
@@ -28,8 +37,16 @@ export const ImageTextBlock = ({ data }) => {
             })}
           </div>
         </div>
-        <div className="w-full">
-          <Image src={data.image} alt={data.imageAlt} />
+        <div className="relative w-full">
+          {data.mediaConfuguration?.imageSource && (
+            <Image
+              src={data.mediaConfuguration?.imageSource}
+              alt={data.mediaConfuguration?.altText ?? "image"}
+              fill={true}
+              objectFit="contain"
+              data-tina-field={tinaField(data, "mediaConfiguration")}
+            />
+          )}
         </div>
       </Container>
     </section>
