04f3819461ea37a9a07e2e2626819896083d171e
Baba Kamyljanov <babakamyljanov@ssw.com.au>
2024-12-05 13:37:56 +0800

    implemented office page.tsx

---
+++ app/offices/index.tsx
@@ -0,0 +1,3 @@
+export default function OfficeIndexPage({ props, tinaProps }) {
+  return <></>;
+}
---
+++ app/offices/page.tsx
@@ -0,0 +1,43 @@
+import { TODAY } from "@/hooks/useFetchEvents";
+import { useSEO } from "@/hooks/useSeo";
+import client from "@/tina/client";
+import { Metadata } from "next";
+import OfficeIndexPage from ".";
+import { TinaClient } from "../tina-client";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const tinaProps = await getData();
+  const seo = tinaProps.props.seo;
+
+  if (seo && !seo.canonical) {
+    seo.canonical = `${tinaProps.props.data.global.header.url}offices`;
+  }
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { seoProps } = useSEO(seo);
+  return { ...seoProps };
+}
+
+const getData = async () => {
+  const tinaProps = await client.queries.officeIndexQuery({
+    relativePath: "officesIndex.json",
+    date: TODAY.toISOString(),
+  });
+
+  return {
+    props: {
+      data: tinaProps.data,
+      query: tinaProps.query,
+      variables: tinaProps.variables,
+      seo: tinaProps.data.officeIndex.seo,
+      header: {
+        url: tinaProps.data.global.header.url,
+      },
+    },
+  };
+};
+
+export default async function OfficePage() {
+  const { props } = await getData();
+  return <TinaClient props={props} Component={OfficeIndexPage} />;
+}
