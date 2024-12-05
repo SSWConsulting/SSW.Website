fa5d471a46229950284273808acf47905230bb8c
Calinator444 <calinator666@gmail.com>
2024-12-05 14:20:34 +1100

    Removed empty space when list items do not have icons

--- components/blocks/imageComponents/imageTextBlock/listItem.tsx
+++ components/blocks/imageComponents/imageTextBlock/listItem.tsx
@@ -4,15 +4,15 @@ import { Icon } from "../../sharedTinaFields/icon";
 export const ListItem = ({ data }) => {
   return (
     <div className="flex gap-1 align-top">
-      <div className="h-full p-1">
-        {data.icon && (
+      {data.icon && (
+        <div className="h-full p-1">
           <Icon
             data={{ name: data.icon }}
             tinaField={tinaField(data, "icon")}
             className="size-6 text-white"
           />
-        )}
-      </div>
+        </div>
+      )}
       <div className="flex flex-col">
         <h6
           className="w-full p-1 text-base font-semibold dark:text-white"
