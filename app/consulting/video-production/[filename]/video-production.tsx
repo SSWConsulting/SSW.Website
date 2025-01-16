1368d343b62c05769196013f7e97cbed7eb14834
Calinator444 <calinator666@gmail.com>
2025-01-16 14:00:17 +1100

    cleaned imports for video procuction page

--- app/consulting/video-production/[filename]/video-production.tsx
+++ app/consulting/video-production/[filename]/video-production.tsx
@@ -4,19 +4,15 @@ import { TinaMarkdown } from "tinacms/dist/rich-text";
 
 import { Blocks } from "@/components/blocks-renderer";
 import { Booking } from "@/components/blocks/booking";
+import { BuiltOnAzure } from "@/components/blocks/builtOnAzure";
 import { componentRenderer } from "@/components/blocks/mdxComponentRenderer";
 import { BookingButton } from "@/components/bookingButton/bookingButton";
 import { Container } from "@/components/util/container";
 import { Section } from "@/components/util/section";
-import { Breadcrumbs } from "app/components/breadcrumb";
-import { ReactElement } from "react";
-
-import {
-  azureFooterColors,
-  BuiltOnAzure,
-} from "@/components/blocks/builtOnAzure";
 import { sanitiseXSS, spanWhitelist } from "@/helpers/validator";
 import { removeExtension } from "@/services/client/utils.service";
+import { Breadcrumbs } from "app/components/breadcrumb";
+import { ReactElement } from "react";
 import ReactDOMServer from "react-dom/server";
 
 export default function VideoProductionPage({ props, tinaProps }) {
