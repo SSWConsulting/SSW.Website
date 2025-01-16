82a65304a1b0eb1de5173c4eb4d5d4358f8524c2
Calinator444 <calinator666@gmail.com>
2025-01-16 11:15:07 +1100

    🔥 Removed unused imports

--- components/layout/footer/pre-footer.tsx
+++ components/layout/footer/pre-footer.tsx
@@ -1,10 +1,9 @@
-import { BuiltOnAzure, BuiltOnAzureProps } from "@/components/blocks/builtOnAzure";
-import { SectionColor } from "@/components/util/constants/styles";
+import {
+  BuiltOnAzure,
+  BuiltOnAzureProps,
+} from "@/components/blocks/builtOnAzure";
 import { Section } from "@/components/util/section";
-import { headers } from "next/headers";
-export const PreFooter = ({
-  data
-}: BuiltOnAzureProps) => {
+export const PreFooter = ({ data }: BuiltOnAzureProps) => {
   return (
     <Section className="w-full flex-none">
       <BuiltOnAzure data={data} />
