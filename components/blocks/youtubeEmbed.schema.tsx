2e0c4f352c9334a9b9f019fb076fafad09eb8e68
Caleb Williams [SSW] <65635198+Calinator444@users.noreply.github.com>
2025-01-28 08:59:04 +1100

    💄Card Component - Updated video embed aspect ratio (#3482)
    * Added option to embed youtube videos inside the card carousel
    
    * 🚨
    
    * rebuilt tina lock file
    
    * made card images 16:9
    
    * hiding youtube controls and irrelevant video previews for card component embeds
    
    * removed white space
    
    * Added youtube embed using urls for card component
    
    * rounded embed corners
    
    * fixed linting errors

---
+++ components/blocks/youtubeEmbed.schema.tsx
@@ -0,0 +1,7 @@
+export const youtubeEmbedField = {
+  type: "string",
+  label: "YouTube URL",
+  name: "youtubeUrl",
+  description:
+    "Enter the YouTube video URL (only used if Media Type is set to youtube)",
+};
