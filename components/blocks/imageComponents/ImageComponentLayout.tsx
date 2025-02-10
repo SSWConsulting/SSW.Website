89eb70d08fe16e611171461a6baee083d23e0516
Calinator444 <calinator666@gmail.com>
2025-02-10 15:36:22 +1100

    updated image text block to break at xl size

--- components/blocks/imageComponents/ImageComponentLayout.tsx
+++ components/blocks/imageComponents/ImageComponentLayout.tsx
@@ -46,7 +46,7 @@ export const ImageComponentLayout = ({ data, children }) => {
         size="custom"
         padding="px-4 sm:px-8"
         className={classNames(
-          "mx-auto flex flex-col gap-8 py-4 align-middle sm:py-12 md:grid md:gap-16",
+          "mx-auto flex flex-col gap-8 py-4 align-middle sm:py-12 md:gap-16 xl:grid",
           data.mediaConfiguration?.imageSource || youtubeVideoId
             ? "md:grid-cols-2"
             : "md:grid-cols-1",
--- components/blocks/imageComponents/imageTextBlock/imageTextBlock.tsx
+++ components/blocks/imageComponents/imageTextBlock/imageTextBlock.tsx
@@ -48,7 +48,7 @@ export const ImageTextBlock = ({ data }) => {
 
       {data.chips && <PillGroup data={data.chips} />}
       <div
-        className={`grid ${data.featureColumns?.twoColumns ? "sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2" : "grid-cols-1"}`}
+        className={`grid gap-x-3 ${data.featureColumns?.twoColumns ? "sm:grid-cols-2" : "grid-cols-1"}`}
       >
         {data.featureColumns?.features?.map((item, index) => {
           return <ListItem key={index} data={item} />;
