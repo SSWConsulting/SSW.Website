9011bc95da64c56e099334a01afdcb7e18d9c365
Baba Kamyljanov [SSW] <138548013+babakamyljanovssw@users.noreply.github.com>
2025-02-17 10:49:19 +0800

    Chips - Allow adding up to 6 chips (#3603)
    * changed chip field to have 6 chips
    
    * added validation, improved descriptions
    
    * removed v2
    
    * added description
    
    * clean console.log
    
    * update tina-lock

--- components/blocksSubtemplates/pillGroup.tsx
+++ components/blocksSubtemplates/pillGroup.tsx
@@ -2,22 +2,22 @@ import { tinaField } from "tinacms/dist/react";
 
 export const PillGroup = ({ data }) => {
   return (
-    <div className="flex gap-2 py-2">
-      {data.filledChipText && (
-        <span
-          data-tina-field={tinaField(data, "filledChipText")}
-          className="rounded-md bg-neutral-600 px-2 py-1 text-xs font-light text-white"
-        >
-          {data.filledChipText}
-        </span>
-      )}
-      {data.clearChipText && (
-        <span
-          data-tina-field={tinaField(data, "clearChipText")}
-          className="px-2 py-1 text-xs font-light text-white"
-        >
-          {data.clearChipText}
-        </span>
+    <div className="flex flex-wrap gap-2 py-2">
+      {data?.map(
+        (chip, index) =>
+          chip?.chipText && (
+            <span
+              key={index}
+              data-tina-field={tinaField(chip, "chipText")}
+              className={
+                chip.chipType === "filledChip"
+                  ? "rounded-md bg-neutral-600 px-2 py-1 text-xs font-light text-white"
+                  : "px-2 py-1 text-xs font-light text-white"
+              }
+            >
+              {chip.chipText}
+            </span>
+          )
       )}
     </div>
   );
@@ -26,14 +26,34 @@ export const PillGroup = ({ data }) => {
 export const pillGroupSchema = [
   {
     type: "string",
-    label: "Filled Chip",
-    name: "filledChipText",
-    description: "Text for the filled chip.",
+    label: "Chip text",
+    name: "chipText",
+    description: "Max 30 characters.",
+    ui: {
+      validate: (value: string) => {
+        const lenghtOfText = value?.length || 0;
+        if (lenghtOfText > 30) {
+          return "Chip text should be less than 30 characters.";
+        }
+      },
+    },
   },
   {
     type: "string",
-    label: "Clear Chip",
-    name: "clearChipText",
-    description: "Text for the clear chip.",
+    label: "Select chip type",
+    name: "chipType",
+    ui: {
+      component: "select",
+      options: [
+        {
+          label: "Filled Chip",
+          value: "filledChip",
+        },
+        {
+          label: "Clear Chip",
+          value: "clearChip",
+        },
+      ],
+    },
   },
 ];
