31e7fa0bc43dadb519a0f00782c468370f03aca3
Baba Kamyljanov [SSW] <138548013+babakamyljanovssw@users.noreply.github.com>
2025-02-27 11:59:45 +0800

    Update app/logo/[[...filename]]/page.tsx
    Co-authored-by: Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>

--- app/logo/[[...filename]]/page.tsx
+++ app/logo/[[...filename]]/page.tsx
@@ -19,7 +19,7 @@ export async function generateMetadata({
   const seo = tinaProps.props.seo;
 
   if (seo && !seo.canonical) {
-    seo.canonical = `${tinaProps.props.header.url}logo/${params.filename}`;
+    seo.canonical = `${tinaProps.props.header.url}logo${params.filename ? `/${params.filename}`: ""}`;
   }
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
