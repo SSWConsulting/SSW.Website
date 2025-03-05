69cf2dc70e22e6e6481fdec66b99197a61c9a511
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-03-06 10:18:11 +1100

    Adding exception handling for appInsight initializer

--- appInsight-api.js
+++ appInsight-api.js
@@ -1,25 +1,36 @@
 import * as appInsights from "applicationinsights";
 
 if (process.env.NEXT_PUBLIC_APP_INSIGHT_CONNECTION_STRING) {
-  appInsights
-    .setup(process.env.NEXT_PUBLIC_APP_INSIGHT_CONNECTION_STRING)
-    .setAutoCollectConsole(true)
-    .setAutoCollectExceptions(true)
-    .setAutoCollectRequests(true)
-    .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
-    .setSendLiveMetrics(true);
-  appInsights.defaultClient?.addTelemetryProcessor((envelope, context) => {
-    if (envelope.data.baseType === "RequestData") {
-      envelope.data.baseData.properties ??= {};
-      if (context["http.ServerRequest"]?.headers["user-agent"])
-        envelope.data.baseData.properties.userAgent =
-          context["http.ServerRequest"].headers["user-agent"];
+  try {
+    appInsights
+      .setup(process.env.NEXT_PUBLIC_APP_INSIGHT_CONNECTION_STRING)
+      .setAutoCollectConsole(true)
+      .setAutoCollectExceptions(true)
+      .setAutoCollectRequests(true)
+      .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
+      .setSendLiveMetrics(true);
+    appInsights.defaultClient?.addTelemetryProcessor((envelope, context) => {
+      if (envelope.data.baseType === "RequestData") {
+        envelope.data.baseData.properties ??= {};
+        if (context["http.ServerRequest"]?.headers["user-agent"])
+          envelope.data.baseData.properties.userAgent =
+            context["http.ServerRequest"].headers["user-agent"];
+      }
+    });
+    appInsights.start();
+    if (appInsights.defaultClient) {
+      console.log("✅ App Insights - Server Side logging is turned on!");
+    } else {
+      console.error("❌ App Insights - Failed to initialize!");
     }
-  });
-  appInsights.start();
+  } catch (error) {
+    console.error(
+      "🚨 App Insights - An error occurred while initializing:",
+      error
+    );
+  }
   console.log("✅ App Insights - Server Side logging is turned on!");
 } else {
-  // eslint-disable-next-line no-console
   console.log(
     "🚨 App Insights - Server Side logging is not turned on, Please add NEXT_PUBLIC_APP_INSIGHT_CONNECTION_STRING variable!"
   );
