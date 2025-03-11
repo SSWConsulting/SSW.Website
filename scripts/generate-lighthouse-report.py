cd020d12b7e76da0fabca70fe7b4c272ac290ae5
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-03-11 17:28:11 +1100

    Updating the path

--- scripts/generate-lighthouse-report.py
+++ scripts/generate-lighthouse-report.py
@@ -3,7 +3,7 @@ import os
 import glob
 
 # Define paths
-TREEMAP_FOLDER = "./lighthouseci"
+TREEMAP_FOLDER = "./.lighthouseci"
 OUTPUT_FILE_PATH = "lighthouse-report.mdx"  # The MDX file to be created
 from urllib.parse import urlparse
 
