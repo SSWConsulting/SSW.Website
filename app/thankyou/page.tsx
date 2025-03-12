97c626299e6968333845a700b48607db65b29fef
Baba Kamyljanov <babakamyljanov@ssw.com.au>
2025-03-12 15:15:03 +0800

    added title to thankyou page

--- app/thankyou/page.tsx
+++ app/thankyou/page.tsx
@@ -1,6 +1,11 @@
+import { Metadata } from "next";
 import ThankyouPage from ".";
 
 export const dynamicParams = false;
+
+export const metadata: Metadata = {
+  title: "Thank you for consulting with SSW",
+};
 
 export default async function Thankyou() {
   return <ThankyouPage />;
