6353d986f46c25a480f957021f1d4a789aa03259
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-03-13 09:37:46 +1100

    Adding guards for pages with minimum information

--- app/company/clients/[filename]/page.tsx
+++ app/company/clients/[filename]/page.tsx
@@ -65,7 +65,7 @@ export async function generateMetadata({
   }
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const { seoProps } = useSEO(seo);
+  const { seoProps } = useSEO(seo || {});
 
   return { ...seoProps };
 }
