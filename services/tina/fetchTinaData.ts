311f09265ece428faa083b995eef630de328d5d7
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-03-18 17:41:11 +1100

    Adding service for Fetching Tina Data

---
+++ services/tina/fetchTinaData.ts
@@ -0,0 +1,33 @@
+import { TODAY } from "@/hooks/useFetchEvents";
+import { notFound } from "next/navigation";
+
+export enum FileType {
+  MDX = "mdx",
+  JSON = "json",
+}
+export async function fetchTinaData<T, V>(
+  queryFunction: (
+    variables?: V,
+    options?: unknown
+  ) => Promise<{
+    data: T;
+    variables: V;
+    query: string;
+  }>,
+  filename?: string,
+  type: FileType = FileType.MDX
+): Promise<{ data: T; variables: V; query: string }> {
+  try {
+    const variables: V = {
+      relativePath: filename ? `${filename}.${type}` : "",
+      date: TODAY.toISOString(),
+    } as V;
+
+    const response = await queryFunction(variables);
+
+    return response;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    notFound();
+  }
+}
