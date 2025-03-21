12d2c0b0ae8ba54a77fb97f04d392454587686a9
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-03-21 17:31:16 +1100

    caching based of the filename

--- services/tina/fetchTinaData.ts
+++ services/tina/fetchTinaData.ts
@@ -8,11 +8,11 @@ export enum FileType {
 
 const cache = new Map();
 
-async function checkCache(cacheKey) {
+async function checkCache(cacheKey: string) {
   return cache.has(cacheKey) ? cache.get(cacheKey) : null;
 }
 
-async function cacheData(cacheKey, response) {
+async function cacheData(cacheKey: string, response: unknown) {
   cache.set(cacheKey, response);
 }
 
@@ -35,23 +35,28 @@ export async function fetchTinaData<T, V>(
     } as V;
 
     // Check if we're in the pipeline (CI environment)
-    const isCI = true; // Custom environment variable for CI builds
+    const isCI = true; // Assuming you have a way to check this, e.g., process.env.IS_CI_BUILD === "true"
+
+    // Generate a unique cache key based on queryFunction name and the filename
+    const cacheKey = `${queryFunction.name}-${filename}-${JSON.stringify(variables)}`;
 
     if (isCI) {
       // Perform caching only in the CI pipeline (build time)
-      const cacheKey = `${filename}-${TODAY.toISOString()}`;
-      const cachedData = await checkCache(cacheKey); // Implement cache check here (e.g., using a memory cache)
+      const cachedData = await checkCache(cacheKey); // Check if data is already cached
       if (cachedData) {
+        // eslint-disable-next-line no-console
+        console.log("🚀 ~ ooho! I'm served from Cache:");
+
         return cachedData;
       }
     }
 
+    // Execute the query function and get the response
     const response = await queryFunction(variables);
 
     if (isCI) {
       // Cache the response for the build time if in CI pipeline
-      const cacheKey = `${filename}-${TODAY.toISOString()}`;
-      await cacheData(cacheKey, response); // Implement caching here (e.g., using a memory cache)
+      await cacheData(cacheKey, response); // Cache the response based on the function name, filename, and variables
     }
 
     return response;
