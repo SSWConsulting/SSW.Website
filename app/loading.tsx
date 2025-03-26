fed91e562b13677ae76447c349b286c3c0979f6c
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-03-26 16:40:14 +1100

    testing - removing loader

--- app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "@/components/util/container";
-
-export default function Loading() {
-  return (
-    <Container className="flex justify-center" width="large" size="custom">
-      <h1>Loading...</h1>
-    </Container>
-  );
-}
