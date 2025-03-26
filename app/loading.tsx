2db062e46f1f2ae440b419900c7725a82c2b0a84
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-03-26 16:58:32 +1100

    Revert "testing - removing loader"
    This reverts commit fed91e562b13677ae76447c349b286c3c0979f6c.

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import { Container } from "@/components/util/container";
+
+export default function Loading() {
+  return (
+    <Container className="flex justify-center" width="large" size="custom">
+      <h1>Loading...</h1>
+    </Container>
+  );
+}
