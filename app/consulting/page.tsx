f1b9dd2f5f9e9120894f6eea17c10ce1cd892095
Calinator444 <calinator666@gmail.com>
2025-03-28 08:49:46 +1100

    Fixed canonical header on consulting index page

--- app/consulting/page.tsx
+++ app/consulting/page.tsx
@@ -10,9 +10,6 @@ export async function generateMetadata(): Promise<Metadata> {
   const tinaProps = await getData();
 
   const seo = tinaProps.props.seo;
-  if (seo && !seo.canonical) {
-    seo.canonical = `${tinaProps.props.header.url}consulting`;
-  }
 
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const { seoProps } = useSEO(seo);
@@ -25,7 +22,10 @@ const getData = async () => {
 
   const seo = tinaProps.data.consultingIndex.seo;
   if (seo && !seo.canonical) {
-    seo.canonical = `${tinaProps.data.global.header.url}/consulting`;
+    seo.canonical = new URL(
+      "/consulting",
+      tinaProps.data.global.header.url
+    ).toString();
   }
 
   return {
