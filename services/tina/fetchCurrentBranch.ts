d6a5a2080f822f14ac9f7f4f71b52c73d0512ab3
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-04-09 16:22:26 +1000

    fixing the issue with fetching branch

--- services/tina/fetchCurrentBranch.ts
+++ services/tina/fetchCurrentBranch.ts
@@ -1,9 +1,14 @@
 export const fetchCurrentBranch = async (): Promise<string | undefined> => {
+  if (process.env.NEXT_PHASE === "phase-production-build") {
+    // During build, return a default value
+    return process.env.NEXT_PUBLIC_TINA_BRANCH;
+  }
+
   try {
     const res = await fetch("/api/branch");
     const data = await res.json();
     return data.branch;
   } catch {
-    return undefined;
+    return process.env.NEXT_PUBLIC_TINA_BRANCH;
   }
 };
