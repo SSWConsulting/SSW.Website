03e4e1e2ab2da3c40581644a6e46c194a15d9f4b
Calinator444 <calebwilliams@ssw.com.au>
2025-10-29 17:37:37 +1100

    fixed typo in React import

--- components/consulting/index/category.tsx
+++ components/consulting/index/category.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import Reactfrom "react";
+import React from "react";
 import { tinaField } from "tinacms/dist/react";
 import { PageCard } from "./pageCard";
 
