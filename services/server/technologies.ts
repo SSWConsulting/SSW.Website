9355f7282e17fded01411ebd589c0250de83e493
Aman Kumar [SSW] <71385247+amankumarrr@users.noreply.github.com>
2025-12-12 10:18:59 +1100

    moving technology card carousel

---
+++ services/server/technologies.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import client from "../../tina/__generated__/client";
+
+/**
+ * Fetches technologies v2 data from TinaCMS filtered by technology group names
+ * @param technologyGroupNames Array of technology group names to filter by
+ * @returns The technologies v2 connection data
+ */
+export const getTechnologiesByGroup = async (technologyGroupNames: string[]) => {
+  const response = await client.queries.technologiesv2Connection({
+    filter: {
+      associatedGroup: {
+        technologyGroupsv2: {
+          name: {
+            in: technologyGroupNames,
+          },
+        },
+      },
+    },
+  });
+
+  return response;
+};
+
