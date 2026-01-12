0e508eb4d5b3cb4eba1b10c6582df98fd7658deb
Calinator444 <calebwilliams@ssw.com.au>
2026-01-12 13:19:31 +1100

    removing functions from mege conflict

--- services/server/events.ts
+++ services/server/events.ts
@@ -1,11 +1,11 @@
 "use server";
-import { EventFilterAllCategories } from "@/components/filter/FilterBlock";
-import { formatCategory } from "@/helpers/getTrimmedEvents";
-
 import { EventTrimmed } from "@/components/filter/events";
+import { EventFilterAllCategories } from "@/components/filter/FilterBlock";
 import { VideoCardType } from "@/components/util/videoCards";
+import { formatCategory } from "@/helpers/getTrimmedEvents";
 import client from "@/tina/client";
 import { GetPastEventsQueryQuery } from "@/tina/types";
+import { EventInfo } from "./events-types";
 import { EVENTS_MAX_SIZE_OVERRIDE } from "./getEvents";
 
 const PAGE_LENGTH = 10;
@@ -185,72 +185,6 @@ const formatCategories = (edges) => {
   }
 };
 
-/**
- * Converts properties of type Date so the object can be passed as static props
- *
- * @param event The event with Date objects
- * @returns The event Object but with strings for date properties
- */
-export const convertEventDatesToStrings = (event: EventInfo) => {
-  if (!event) return null;
-  return {
-    ...event,
-    startDateTime: event?.startDateTime.toISOString(),
-    endDateTime: event?.endDateTime.toISOString(),
-    startShowBannerDateTime: event?.startShowBannerDateTime.toISOString(),
-    endShowBannerDateTime: event?.endShowBannerDateTime.toISOString(),
-  };
-};
-
-export type BookingFormSubmissionData = {
-  Name: string;
-  Topic: string;
-  Company: string;
-  Country: string;
-  State: string;
-  Email: string;
-  Phone: string;
-  Recaptcha: string;
-  SourceWebPageURL: string;
-  EmailSubject: string;
-  EmailBody: string;
-  Note?: string | null;
-};
-
-export type AddContactToNewslettersData = {
-  Email: string;
-  FullName: string;
-};
-
-export interface EventInfo extends EventTrimmed {
-  youTubeId?: string;
-  abstract?: string;
-  delayedLiveStreamStart?: boolean;
-  liveStreamDelayMinutes?: number;
-  startShowBannerDateTime?: Date;
-  endShowBannerDateTime?: Date;
-  trailerUrl?: string;
-  presenterList?: {
-    presenter?: {
-      profileImg?: string;
-      torsoImg?: string;
-      presenter?: {
-        name?: string;
-        peopleProfileURL?: string;
-      };
-      about?: TinaMarkdownContent;
-      tip?: string;
-    };
-  }[];
-}
-
-type EventDates = {
-  startShowBannerDateTime: Date;
-  endShowBannerDateTime: Date;
-  startDateTime: Date;
-  endDateTime: Date;
-};
-
 type EventsRespose = Awaited<
   ReturnType<typeof client.queries.eventsContentQuery>
 >;
@@ -273,32 +207,3 @@ export const getVideoCardProps = (events: EventsData): VideoCardType[] => {
     })) || []
   );
 };
-
-export const formatDates = (eventInfo: EventInfoStatic): EventDates => {
-  const {
-    startShowBannerDateTime,
-    endShowBannerDateTime,
-    startDateTime,
-    endDateTime,
-  } = eventInfo;
-  return {
-    startShowBannerDateTime: new Date(startShowBannerDateTime),
-    endShowBannerDateTime: new Date(endShowBannerDateTime),
-    startDateTime: new Date(startDateTime),
-    endDateTime: new Date(endDateTime),
-  };
-};
-
-export interface EventInfoStatic
-  extends Omit<
-    EventInfo,
-    | "startShowBannerDateTime"
-    | "endShowBannerDateTime"
-    | "startDateTime"
-    | "endDateTime"
-  > {
-  startShowBannerDateTime?: string;
-  endShowBannerDateTime?: string;
-  startDateTime?: string;
-  endDateTime?: string;
-}
