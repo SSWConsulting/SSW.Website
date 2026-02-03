29c95df30abfc29f9e772f84bf1b70d4b6675575
Calinator444 <calebwilliams@ssw.com.au>
2026-02-03 14:57:39 +1100

    Updated megamenu to hide contact us button on contact us page

--- components/server/MegaMenuWrapper.tsx
+++ components/server/MegaMenuWrapper.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import classNames from "classnames";
+import { usePathname } from "next/navigation";
 import { MegaMenuLayout } from "ssw.megamenu";
 import { CustomLink } from "../customLink";
 
 export function MegaMenuWrapper(props) {
+  const pathName = usePathname();
+
   return (
     <MegaMenuLayout
+      hidePhone={pathName === "/company/contact-us"}
       menuBarItems={props.menu}
+      isFlagVisible={true}
       tagline="Enterprise Software Development"
       linkComponent={(props) => (
         <CustomLink
