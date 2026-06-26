b2d0fb1a1f2dd1fdef3b67203f83fb2b9dba88f9
Josh Berman <joshberman@ssw.com.au>
2026-06-26 13:55:32 +1000

    fix scoop +  heading width of second component

--- components/blocks/v3/imageCards/imageCards.tsx
+++ components/blocks/v3/imageCards/imageCards.tsx
@@ -42,7 +42,7 @@ export function V3ImageCards({ data }) {
         {data?.heading && (
           <h2
             data-tina-field={tinaField(data, "heading")}
-            className="my-4 text-3xl text-white lg:text-4xl"
+            className="my-4 text-3xl text-white lg:text-4xl max-w-lg"
           >
             <AlternatingText text={data.heading} />
           </h2>
