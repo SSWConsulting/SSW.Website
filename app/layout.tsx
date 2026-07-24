45fccdde1c90299af1264d4c0bc32f8f7841ef7b
Josh Berman <joshberman@ssw.com.au>
2026-07-24 10:29:59 +1000

    cleanup unused code

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ import isBetween from "dayjs/plugin/isBetween";
 import relativeTime from "dayjs/plugin/relativeTime";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
-import { Metadata, Viewport } from "next";
 import dynamic from "next/dynamic";
 import { inter } from "@/lib/fonts";
 import "styles.css";
@@ -19,7 +18,6 @@ import { MenuWrapper } from "./components/MenuWrapper";
 import PageLayout from "./components/page-layout";
 import { WebVitals } from "./components/web-vitals";
 import { LiveStream } from "./live-steam-banner/live-stream";
-import { DEFAULT } from "./meta-data/default";
 import { QueryProvider } from "./providers/query-provider";
 import { getMegamenu, MegaMenuProps } from "./utils/get-mega-menu";
 import { getPhishingBanner } from "./utils/get-phishing-banner";
@@ -33,18 +31,6 @@ dayjs.extend(isBetween);
 const ChatBaseBot = dynamic(
   () => import("@/components/zendeskButton/chatBaseBot")
 );
-
-export const metadata: Metadata = {
-  ...DEFAULT,
-};
-
-export const viewport: Viewport = {
-  themeColor: "#cc4141",
-  width: "device-width",
-  initialScale: 1,
-};
-
-export const revalidate = 1800; // 30 minutes
 
 export default async function RootLayout({
   children,
